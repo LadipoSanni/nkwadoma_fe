6e5b9a8f02372b8686d333c95eb5414dea25d674
MOYIN-21 <olawamidemoyinoluwamary@gmail.com>
2024-09-24 17:35:51 +0100

    I Wrote test to check if the warning message still exist when test is run

--- test/page.test.tsx
+++ test/page.test.tsx
@@ -1,6 +1,7 @@
 import "@testing-library/react"
 import Home from "@/app/page";
 import {render, screen} from "@testing-library/react";
+import '@testing-library/jest-dom';
 
 
 describe("testing home component", ()=> {
@@ -9,4 +10,26 @@ describe("testing home component", ()=> {
         const button = screen.getAllByText("Deploy now");
         expect(button);
     })
+
+    it(`test that the img with the incorrect id will not render`, ()=> {
+        render(<Home/>);
+        const deployButton = screen.queryByTestId('deployButton');
+
+        expect(deployButton).not.toBeInTheDocument();
+    });
+
+    it(`test that the img with the correct property is rendered`, ()=>{
+        render(<Home/>)
+        const deployButton = screen.getByTestId('deploy-button');
+
+        expect(deployButton).toBeInTheDocument();
+
+        const parentAnchor = deployButton.closest('a');
+        expect(parentAnchor).toHaveAttribute('href', 'https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app');
+
+        expect(deployButton).toHaveAttribute('alt', 'Vercel logomark');
+
+        expect(deployButton).toHaveAttribute('width', '20');
+        expect(deployButton).toHaveAttribute('height', '20');
+    });
 })
