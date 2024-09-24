39f0bee2ba5d2d18dfea72a0b8d066a8bdde9e9c
ikjames5454 <ikennajames03@gmail.com>
2024-09-24 19:03:25 +0100

    test

--- test/page.test.tsx
+++ test/page.test.tsx
@@ -2,34 +2,26 @@ import "@testing-library/react"
 import Home from "@/app/page";
 import {render, screen} from "@testing-library/react";
 import '@testing-library/jest-dom';
+import { Providers } from "@/app/provider";
 
 
 describe("testing home component", ()=> {
-    it('test that button renders', ()=> {
-        render(<Home />);
-        const button = screen.getAllByText("Deploy now");
-        expect(button);
-    })
+
+    const renderComponent = () => {
+     return render(
+            <Providers>
+                <Home />
+            </Providers>
+        )
+    }
+
 
     it(`test that the img with the incorrect id will not render`, ()=> {
-        render(<Home/>);
+        renderComponent()
         const deployButton = screen.queryByTestId('deployButton');
 
         expect(deployButton).not.toBeInTheDocument();
     });
 
-    it(`test that the img with the correct property is rendered`, ()=>{
-        render(<Home/>)
-        const deployButton = screen.getByTestId('deploy-button');
-
-        expect(deployButton).toBeInTheDocument();
-
-        const parentAnchor = deployButton.closest('a');
-        expect(parentAnchor).toHaveAttribute('href', 'https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app');
-
-        expect(deployButton).toHaveAttribute('alt', 'Vercel logomark');
-
-        expect(deployButton).toHaveAttribute('width', '20');
-        expect(deployButton).toHaveAttribute('height', '20');
-    });
+    
 })
