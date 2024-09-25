25ca4888b31d76e8b9586efdb3a846b0ea1ff2e1
a-mariam <mariiam22222@gmail.com>
2024-09-25 16:46:25 +0100

    working project folder structure and admin layout

--- src/component/AdminLayout/AdminLayout.tsx
+++ src/component/AdminLayout/AdminLayout.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import SideBar from "@/component/AdminLayout/SideBar";
 import TopBar from "@/component/AdminLayout/TopBar";
+
 type props = {
     children: React.ReactNode;
 }
 
-const Layout = ({children}: props) => {
+const AdminLayout = ({children}: props) => {
     return (
         <div id={'layout'} className={`flex h-screen  `}
         >
-            <SideBar/>
+            <SideBar />
             <div id={'TopNavComponent  '} className={`flex flex-col gap-3 flex-grow `}>
-                <TopBar/>
+                <TopBar />
                 <div id={`children`}
                      className={` fixed  bg-[#f0f2f4] h-[92%] md:h-[90%] w-full md:w-[84%] bottom-0 right-0 `}>
                     {children}
@@ -21,4 +22,4 @@ const Layout = ({children}: props) => {
     );
 };
 
-export default Layout;
+export default AdminLayout;
