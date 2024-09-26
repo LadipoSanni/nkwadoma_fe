c3271c6dd14340e2bdc4e36de2abbabac9733b34
a-mariam <mariiam22222@gmail.com>
2024-09-26 13:42:38 +0100

    WORKING On institution change and filter pushing cause co-worker need the layout"

--- src/component/selectedLoan/SelectedLoanHeader.tsx
+++ src/component/selectedLoan/SelectedLoanHeader.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SelectLoanTab from "@/component/selectedLoan/SelectLoanTab";
+import InstitutionAndFilter from "@/component/selectedLoan/InstitutionAndFilter";
 
 const SelectedLoanHeader = () => {
     return (
         <div className={`grid mt-4 md:mr-auto md:ml-auto ml-1 w-[98%] h-[20%] md:w-[98%] md:h-[22%]`}>
           <SelectLoanTab/>
+          <InstitutionAndFilter/>
         </div>
     );
 };
