e7140802c3c7f3b1ced2f7ad168c676fd3afad7f
a-mariam <mariiam22222@gmail.com>
2024-09-26 04:31:54 +0100

    working on select loan page

---
+++ src/component/selectedLoan/SelectedLoanHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectedLoanHeader = () => {
+    return (
+        <div className={` grid bg-red-200 mt-4 mr-auto ml-auto w-[98%] h-[22%]`}>
+
+        </div>
+    );
+};
+
+export default SelectedLoanHeader;
