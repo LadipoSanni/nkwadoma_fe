0b8f04e66da79c8c7516358ce29612156a33a773
ikjames5454 <ikennajames03@gmail.com>
2024-09-26 09:00:14 +0100

    test

--- src/pages/portFolioManager/loanProduct/LoanProductPage.tsx
+++ src/pages/portFolioManager/loanProduct/LoanProductPage.tsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React from "react";
+import { InputAdornment, TextField } from "@mui/material";
+import { SearchOutlined } from "@mui/icons-material";
 
-type Props = {
-    sx?:string
-}
 
-function LoanProductPage({sx}: Props) {
+
+function LoanProductPage() {
   return (
-    <div className={`${sx}`}>LoanProductPage</div>
-  )
+    <div className={`grid grid-cols-1  place-items-center  h-full`}>
+      <div className="grid rounded bg-white w-[97%] h-[85vh] ">
+        <div className={` mt-[1rem]  `}>
+          <TextField
+            size="small"
+            placeholder="Search"
+            tabIndex={2}
+            // onChange={(e) => handleSearch(e.target.value)}
+            id={"searchLoanProduct"}
+            fullWidth
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchOutlined fontSize="small" />
+                  </InputAdornment>
+                ),
+              },
+            }}
+            variant="outlined"
+          />
+        </div>
+        LoanProductPage
+      </div>
+    </div>
+  );
 }
 
-export default LoanProductPage
+export default LoanProductPage;
