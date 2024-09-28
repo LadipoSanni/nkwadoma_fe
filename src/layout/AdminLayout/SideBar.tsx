3cd578711848f910bcf293e391488c58d70dffbc
a-mariam <mariiam22222@gmail.com>
2024-09-28 11:04:39 +0100

    started  on side bar mobile view

--- src/layout/AdminLayout/SideBar.tsx
+++ src/layout/AdminLayout/SideBar.tsx
@@ -1,21 +1,23 @@
 "use client"
 import React, {useState} from 'react';
 import {useRouter} from "next/navigation";
+import {store, useAppSelector} from "@/redux/store";
+import {setShowMobileSideBar} from "@/redux/slice/layout/adminLayout";
 
 
 const SideBar = () => {
     const router = useRouter();
-    const [showSide] = useState(false)
+    const showMobileSideBar = useAppSelector(state => state.adminLayout.showMobileSideBar)
     return (
         <>
             <>
-                {showSide &&
+                {showMobileSideBar &&
                     <aside
-                        id={'mobileSideBar'}
+                        id={'adminMobileSideBar'}
                         className={` w-[100vw] h-[100vh]  border-r-2 border-r-grey-200  flex z-10 md:hidden`}
                     >
                         <div
-                            className={` w-[70vw]  py-8 px-5 border border-r-grey-200 z-10 h-[100%] bg-learnSpaceWhite `}
+                            className={` w-[70vw] bg-white py-8 px-5 border border-r-grey-200 z-10 h-[100%] bg-learnSpaceWhite `}
                         >
                             <div className={`h-[100%] w-[96%] `}>
                                 <div id={'fund'} onClick={() => router.push('')} className={``}>
@@ -38,8 +40,8 @@ const SideBar = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className={` h-[100vh] w-[40vw] backdrop-blur-sm bg-[grey/30] `}
-                            // onClick={() => { store.dispatch(layOutActions.setHamburger(false))}}
+                        <div id="sideBarblurBackground" className={` h-[100vh] w-[40vw] backdrop-blur-sm bg-[grey/30] `}
+                            onClick={() => { store.dispatch(setShowMobileSideBar(false))}}
                         ></div>
 
                     </aside>
