147bd381bac790eca28e6cc0cbf149ba72632dbf
Moyinoluwa <olawamidemoyinoluwamary@gmail.com>
2024-09-30 17:03:43 +0100

    loan request mock data

---
+++ src/app/(Admin)/loan/(selectedLoan)/loanRequest/LoanRequestMockData.tsx
@@ -0,0 +1,212 @@
+export const LoanRequestTable = [
+    {
+        id: 1,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    },
+    {
+        id: 2,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 3,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 4,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 5,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 6,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 6,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 7,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 8,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 9,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 10,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 11,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 12,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 13,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 14,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 15,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 16,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 17,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 18,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 19,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 20,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 21,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    }, {
+        id: 22,
+        traineeName: 'Anoluwapo Ogungbemi',
+        program: 'Software Engineering',
+        cohort: 'Gems',
+        startDate: '12 Dec, 2021',
+        requestDate: '12 Jan, 2022',
+        initialDeposit: '#3,000,0000',
+        amountRequested: '#3,000,0000'
+    },
+
+]
