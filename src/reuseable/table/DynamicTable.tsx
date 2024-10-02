a3ec82c043abd2846b3007fa9b1187d85bba884c
Moyinoluwa <olawamidemoyinoluwamary@gmail.com>
2024-10-02 12:50:16 +0100

    I added ids

--- src/reuseable/table/DynamicTable.tsx
+++ src/reuseable/table/DynamicTable.tsx
@@ -45,21 +45,21 @@ const DynamicTable = <T extends { [key: string]: string }>({
     const paginatedData = tableData.slice((page - 1) * rowsPerPage, page * rowsPerPage);
 
     return (
-        <div data-testid="dynamicTableContainer" className={`w-[100%]`}>
-            <Paper data-testid="dynamicTablePaper" sx={{width: '100%', overflow: 'hidden'}} elevation={0}>
-                <div data-testid="dynamicTableBorder"
+        <div data-testid="dynamicTableContainer" id={`dynamicTableContainer`} className={`w-[100%]`}>
+            <Paper data-testid="dynamicTablePaper" id={`dynamicTablePaper`} sx={{width: '100%', overflow: 'hidden'}} elevation={0}>
+                <div data-testid="dynamicTableBorder" id={`dynamicTableBorder`}
                      className='border-[1px] border-learnSpaceGray border-solid rounded'>
-                    <TableContainer data-testid="dynamicTableScrollbar" className='scrollbar-custom'
+                    <TableContainer data-testid="dynamicTableScrollbar" id={`dynamicTableScrollbar`} className='scrollbar-custom'
                                     sx={{height: `${tableHeight}vh`}}>
-                        <Table data-testid="dynamicTable" stickyHeader
+                        <Table data-testid="dynamicTable" id={`dynamicTable`} stickyHeader
                                sx={{width: '100%', border: '#d0d5dd', height: 'auto', backgroundColor: '#ffffff'}}>
-                            <TableHead data-testid="dynamicTableHead" sx={{backgroundColor: '#e7e7e7'}}>
-                                <TableRow data-testid="dynamicTableHeadRow"
+                            <TableHead data-testid="dynamicTableHead" id={`dynamicTableHead`} sx={{backgroundColor: '#e7e7e7'}}>
+                                <TableRow data-testid="dynamicTableHeadRow" id={`dynamicTableHeadRow`}
                                           style={{position: "sticky", top: 0, backgroundColor: "#fafbfc", zIndex: 1}}>
                                     {tableHeader.map((column) => (
-                                        <TableCell data-testid={`dynamicTableHeadCell${column.id}`}
+                                        <TableCell data-testid={`dynamicTableHeadCell${column.id}`} id={`dynamicTableHeadCell`}
                                                    sx={{backgroundColor: '#fafbfc'}} key={column.id}>
-                                            <div data-testid={`dynamicTableHeadCellDiv${column.id}`}
+                                            <div data-testid={`dynamicTableHeadCellDiv${column.id}`} id={`dynamicTableHeadCellDiv`}
                                                  className={`${styles.tableHeader}`}>
                                                 {column.title}
                                             </div>
@@ -67,9 +67,10 @@ const DynamicTable = <T extends { [key: string]: string }>({
                                     ))}
                                 </TableRow>
                             </TableHead>
-                            <TableBody data-testid="dynamicTableBody">
+                            <TableBody data-testid="dynamicTableBody" id={`dynamicTableBody`}>
                                 {paginatedData.map((row, rowIndex) => (
                                     <TableRow
+                                        id={`dynamicTableBodyRow`}
                                         data-testid={`dynamicTableBodyRow${rowIndex}`}
                                         hover={true}
                                         key={rowIndex}
@@ -77,9 +78,9 @@ const DynamicTable = <T extends { [key: string]: string }>({
                                         onClick={() => handleRowClick(row)}
                                     >
                                         {tableHeader.map((column) => (
-                                            <TableCell data-testid={`dynamicTableBodyCell${rowIndex}${column.id}`}
+                                            <TableCell id={`dynamicTableBodyCell`} data-testid={`dynamicTableBodyCell${rowIndex}${column.id}`}
                                                        key={column.id}>
-                                                <div data-testid={`dynamicTableBodyCellDiv${rowIndex}${column.id}`}
+                                                <div data-testid={`dynamicTableBodyCellDiv${rowIndex}${column.id}`} id={`dynamicTableBodyCellDiv`}
                                                      className={`${styles.tableRow}`}>
                                                     {column.selector ? column.selector(row) : String(row[column.id] ?? '')}
                                                 </div>
