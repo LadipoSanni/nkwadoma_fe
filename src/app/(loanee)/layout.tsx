44f8429feecbd97077ae7d9533fd3b30573ba209
Koleaje Olayinka OluwaTOBI <koleajeolayinka@gmail.com>
2024-10-02 17:27:42 +0100

    Refactor LoaneeLayout and cleanup unused files
                   - Renamed and moved LoaneeLayout component to Loaneelayout/LoaneeLayoutComponent.tsx to resolve naming conflicts.
                   - Deleted unused icons and index files.
                   - Updated layout.tsx to use the new LoaneeLayoutComponent.

--- src/app/(loanee)/layout.tsx
+++ src/app/(loanee)/layout.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode } from 'react';
+import React, {ReactNode} from 'react';
+import LoaneeLayoutComponent from '@/layout/Loaneelayout/LoaneeLayoutComponent'
 
 interface LoaneeLayoutProps {
     children: ReactNode;
@@ -6,17 +7,10 @@ interface LoaneeLayoutProps {
 
 const LoaneeLayout: React.FC<LoaneeLayoutProps> = ({ children }) => {
     return (
-        <main data-testid="AcceptedTraineeDashboardMain" id="AccepTrMain"
-              className="flex h-screen bg-LearnSpaceAthensGray">
-            {/*<NavigationSidebar/>*/}
-            <div data-testid={"AcceptedTraineeDashboardContainer"} id={"AcceptTrContainer"}
-                 className="flex flex-col flex-grow">
-                {/*<DashboardHeader/>*/}
-                <div data-testid="AcceptedTraineeDashboardContent" id="AcceptTrContent" className="px-5 py-8 h-full">
-                    {children}
-                </div>
-            </div>
-        </main>
+        <LoaneeLayoutComponent>
+            {children}
+        </LoaneeLayoutComponent>
+
     );
 };
 
---
+++ src/layout/Loaneelayout/LoaneeLayoutComponent.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react';
+
+interface LoaneeLayoutProps {
+    children: ReactNode;
+}
+
+const LoaneeLayoutComponent: React.FC<LoaneeLayoutProps> = ({ children }) => {
+    return (
+        <main data-testid="AcceptedTraineeDashboardMain" id="AccepTrMain"
+              className="flex h-screen bg-LearnSpaceAthensGray">
+            {/*<NavigationSidebar/>*/}
+            <div data-testid={"AcceptedTraineeDashboardContainer"} id={"AcceptTrContainer"}
+                 className="flex flex-col flex-grow">
+                {/*<DashboardHeader/>*/}
+                <div data-testid="AcceptedTraineeDashboardContent" id="AcceptTrContent" className="px-5 py-8 h-full">
+                    {children}
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default LoaneeLayoutComponent;
