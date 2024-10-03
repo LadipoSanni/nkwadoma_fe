b32fddd8ee41888e5acc9990d45711ef2cc87541
Moyinoluwa <olawamidemoyinoluwamary@gmail.com>
2024-10-03 16:24:29 +0100

    login test

--- test/auth/Login.test.tsx
+++ test/auth/Login.test.tsx
@@ -1,6 +1,8 @@
-import {render} from '@testing-library/react';
+import {render, screen, fireEvent, queryByAttribute} from '@testing-library/react';
 import '@testing-library/react';
-// import Login from '@/features/auth/login/Login';
+import Login from '@/features/auth/login/Login';
+import {userEvent} from "@testing-library/user-event";
+import * as React from "react";
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const {expect, describe, it} = require("@jest/globals");
@@ -14,73 +16,54 @@ describe('Login Component', () => {
         expect(login).not.toBeInTheDocument();
     })
 
-    // it('should est that LoanEmptyState component exist', () => {
-    //     const {getByTestId} = render(<Login/>);
-    //     expect(getByTestId('parentDivId')).toBeInTheDocument();
-    // });
-    //
-    //
-    // it('should render login component correctly', () => {
-    //     render(<Login/>);
-    //
-    //     expect(screen.getByTestId('parentDivId')).toBeInTheDocument();
-    //     expect(screen.getByTestId('childDivId')).toBeInTheDocument();
-    //     expect(screen.getByTestId('loginDivId')).toBeInTheDocument();
-    //     expect(screen.getByTestId('emailAndPasswordId')).toBeInTheDocument();
-    //     expect(screen.getByTestId('emailId')).toBeInTheDocument();
-    //     expect(screen.getByTestId('passwordId')).toBeInTheDocument();
-    //     expect(screen.getByTestId('submitButton')).toBeInTheDocument();
-    // });
-    //
-    // it('should enable login button when email and password are filled', () => {
-    //     render(<Login/>);
-    //
-    //     const emailInput = screen.getByTestId('email');
-    //     const passwordInput = screen.getByTestId('password');
-    //     const loginButton = screen.getByTestId('loginButton');
-    //
-    //     expect(loginButton).toBeDisabled();
-    //     expect(loginButton).toHaveClass('bg-[#90D8AE] cursor-not-allowed');
-    //
-    //     fireEvent.change(emailInput, {target: {value: 'tolu@gmail.com'}});
-    //     fireEvent.change(passwordInput, {target: {value: 'password123'}});
-    //
-    //     expect(loginButton).not.toBeDisabled();
-    //     expect(loginButton).toHaveClass('bg-[#0D9B48] cursor-pointer');
-    // });
-    //
-    // it('should show and hide the password when the visibility toggle is clicked', () => {
-    //     render(<Login/>);
-    //
-    //     const passwordInput = screen.getByTestId('password');
-    //     const toggleButton = screen.getByRole('button', {name: /toggle password visibility/i});
-    //
-    //     expect(passwordInput).toHaveAttribute('type', 'password');
-    //
-    //     fireEvent.click(toggleButton);
-    //
-    //     expect(passwordInput).toHaveAttribute('type', 'text');
-    //
-    //     fireEvent.click(toggleButton);
-    //     expect(passwordInput).toHaveAttribute('type', 'password');
-    // });
-    //
-    // it('should test that login button is disabled when forms are not filled', () => {
-    //     render(<Login/>);
-    //     const loginButton = screen.getByTestId('loginButton');
-    //     fireEvent.click(loginButton);
-    //     expect(loginButton).toBeDisabled();
-    // });
-    //
-    // it('should test that login button is disabled when forms are filled', () => {
-    //     render(<Login/>);
-    //     const emailInput = screen.getByTestId('email');
-    //     const passwordInput = screen.getByTestId('password');
-    //     const loginButton = screen.getByTestId('loginButton');
-    //
-    //     fireEvent.change(emailInput, {target: {value: 'tolu@gmail.com'}});
-    //     fireEvent.change(passwordInput, {target: {value: 'password123'}});
-    //     fireEvent.click(loginButton);
-    //     expect(loginButton).not.toBeDisabled();
-    // });
+    it('should est that LoanEmptyState component exist', () => {
+        const {getByTestId} = render(<Login/>);
+        expect(getByTestId('parentDivId')).toBeInTheDocument();
+    });
+
+
+    it('should render login component correctly', () => {
+
+        const getById = queryByAttribute.bind(null, "id")
+       const buttonComponent = render(<Login/>);
+
+        expect(screen.getByTestId('parentDivId')).toBeInTheDocument();
+        expect(screen.getByTestId(`writeUpAndLogin`)).toBeInTheDocument();
+        expect(screen.getByTestId('childDivId')).toBeInTheDocument();
+        expect(screen.getByTestId('loginDivId')).toBeInTheDocument();
+        expect(screen.getByTestId('emailAndPasswordId')).toBeInTheDocument();
+        expect(screen.getByTestId('emailId')).toBeInTheDocument();
+        expect(screen.getByTestId('passwordId')).toBeInTheDocument();
+        expect(getById(buttonComponent.container, "loginButton")).toBeInTheDocument()
+    });
+
+    it('should test that login button is not disabled after email and password has been entered', async () => {
+        render(<Login />);
+
+        const emailInput = screen.getByPlaceholderText('Enter email address');
+        const passwordInput = screen.getByPlaceholderText('Enter password');
+        const loginButton = screen.getByRole('button', { name: /login/i });
+
+        expect(loginButton).toBeDisabled();
+
+        await userEvent.type(emailInput, 'ola@gmail.com');
+        await userEvent.type(passwordInput, 'glory123');
+
+        expect(loginButton).not.toBeDisabled();
+    });
+
+    it('enables the login button after email and password have been entered', () => {
+        render(<Login />);
+
+        const emailInput = screen.getByPlaceholderText('Enter email address');
+        const passwordInput = screen.getByPlaceholderText('Enter password');
+        const loginButton = screen.getByRole('button', { name: /login/i });
+
+        expect(loginButton).toBeDisabled();
+
+        fireEvent.change(emailInput, { target: { value: 'user@example.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'password123' } });
+
+        expect(loginButton).toBeEnabled();
+    });
 });
