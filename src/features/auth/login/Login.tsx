a57d1e3d1e4bf5468e726449404752db6b0e683e
Moyinoluwa <olawamidemoyinoluwamary@gmail.com>
2024-10-03 16:25:02 +0100

    Added mobile responsiveness

--- src/features/auth/login/Login.tsx
+++ src/features/auth/login/Login.tsx
@@ -7,7 +7,6 @@ import AuthInputField from "@/reuseable/Input/AuthInputField";
 const Login: React.FC = () => {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
-    const [showPassword] = React.useState(false);
 
     const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
         setEmail(e.target.value)
@@ -24,9 +23,9 @@ const Login: React.FC = () => {
 
     return (
         <section data-testid={`parentDivId`} id={`parentDivId`}
-                 className="flex flex-col bg-[#fafbfc] min-h-screen px-20 py-16">
-            <p className={`text-[#0D9B48] text-xl font-DM-Mono font-medium leading-10`}> Middl</p>
-            <div className={`flex flex-row items-center justify-between pt-8`}>
+                 className="flex flex-col bg-[#fafbfc] min-h-screen md:px-20 md:py-16 px-5 py-10 justify-evenly md:justify-center">
+            <p className={`text-[#0D9B48] text-3xl font-DM-Mono font-medium leading-10`}> Meedl</p>
+            <div data-testid={`writeUpAndLogin`} id={`writeUpAndLogin`} className={`flex flex-row items-center justify-between `}>
                 <div className={`hidden md:block`}>
                     <p className={`text-[#101828] text-5xl font-bold font-sans`}>Revolutionizing financing <br/> and
                         access to loans</p>
@@ -35,43 +34,48 @@ const Login: React.FC = () => {
                         investment avenues with the <br/> pressing need for accessible loans.</p>
                 </div>
 
-                <div data-testid={`childDivId`} id={`childDivId`}
-                     className="w-full bg-[#FFFFFF] rounded-lg md:shadow md:max-w-xl max-w-sm">
-                    <div data-testid={`loginDivId`} id={`loginDivId`}
-                         className="p-1 space-y-2 md:space-y-1 sm:p-8">
-                        <h1 className=" font-sans text-[#1A1A1A] text-3xl leading-5 font-medium ">Log in to your
-                            account</h1>
-                        {/* eslint-disable-next-line react/no-unescaped-entities */}
-                        <div data-testid={`emailAndPasswordId`} id={`emailAndPasswordId`}
-                             className="pt-12 space-y-5">
-                            <div data-testid={`emailId`} id={`emailId`}>
-                                <AuthInputField label={"Email"} id={`email`}
-                                                placeholder={`Enter email address`}
-                                                type="email"
-                                                value={email}
-                                                onChange={handleEmail}
-                                />
+                <div data-testid={`childDivId`} id={`childDivId`} className={` rounded-xl md:shadow md:max-w-xl w-full flex items-center justify-center`}>
+                    <div
+                         className="w-full bg-[#FFFFFF] rounded-xl md:border border-2 border-gray-100">
+                        <div data-testid={`loginDivId`} id={`loginDivId`}
+                             className="px-4 py-10">
+                            <h1 className=" font-sans text-[#1A1A1A] md:text-3xl text-2xl leading-5 ">Log in to your
+                                account</h1>
+                            {/* eslint-disable-next-line react/no-unescaped-entities */}
+                            <div data-testid={`emailAndPasswordId`} id={`emailAndPasswordId`}
+                                 className="pt-10 space-y-5">
+                                <div data-testid={`emailId`} id={`emailId`}>
+                                    <AuthInputField label={"Email"} id={`email`}
+                                                    data-testid={`loginEmailId`}
+                                                    placeholder={`Enter email address`}
+                                                    type="email"
+                                                    value={email}
+                                                    onChange={handleEmail}
+                                    />
+                                </div>
+                                <div data-testid={`passwordId`} id={`passwordId`}>
+                                    <AuthInputField label={`Password`} id={'password'}
+                                                    data-testid={`password`}
+                                                    type={'password'}
+                                                    placeholder={`Enter password`}
+                                                    endAdornment={`show`}
+                                                    value={password}
+                                                    onChange={handlePassword}>
+                                    </AuthInputField>
+                                </div>
+                                <div id={"authButtonContainer"} className={`w-[100%]`}>
+                                    <AuthButton disable={!isFormValid} backgroundColor={'#0d9b48'} textColor={"white"}
+                                                id={"loginButton"}
+                                                buttonText={"Login"} width={"inherit"}
+                                                handleClick={handleReset}>
+                                    </AuthButton>
+                                </div>
+                                <p className="flex items-center justify-center text-sm text-[#101828] leading-4">
+                                    Forgot Password? <a href="/auth/reset-password"
+                                                        className="font-medium text-[#0D9B48] underline">Reset it
+                                    here</a>
+                                </p>
                             </div>
-                            <div data-testid={`passwordId`} id={`passwordId`}>
-                                <AuthInputField label={`Password`} id={'password'}
-                                                type={'password'}
-                                                placeholder={`Enter password`}
-                                                endAdornment={`show`}
-                                                value={password}
-                                                onChange={handlePassword}>
-                                </AuthInputField>
-                            </div>
-                            <div id={"authButtonContainer"} className={`w-[100%]`}>
-                                <AuthButton disable={!isFormValid} backgroundColor={'#0d9b48'} textColor={"white"}
-                                            id={"resetPasswordButton"}
-                                            buttonText={"Login"} width={"inherit"}
-                                            handleClick={handleReset}>
-                                </AuthButton>
-                            </div>
-                            <p className="flex items-center justify-center text-sm text-[#101828] leading-4">
-                                Forgot Password? <a href="/auth/reset-password"
-                                                    className="font-medium text-[#0D9B48] underline">Reset it here</a>
-                            </p>
                         </div>
                     </div>
                 </div>
