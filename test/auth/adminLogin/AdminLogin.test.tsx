286f57bba5542076718d4d64461263293feb4372
Moyinoluwa <olawamidemoyinoluwamary@gmail.com>
2024-10-04 16:32:15 +0100

    super Admin login

---
+++ test/auth/adminLogin/AdminLogin.test.tsx
@@ -0,0 +1,76 @@
+import {render, screen, fireEvent, queryByAttribute} from '@testing-library/react';
+import '@testing-library/react';
+import {userEvent} from "@testing-library/user-event";
+import * as React from "react";
+import AdminLogin from "@/features/auth/adminLogin/AdminLogin";
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const {expect, describe, it} = require("@jest/globals");
+
+describe('Login Component', () => {
+    it("should test that login component does not exist", () => {
+        const {queryByTestId} = render(
+            <div></div>
+        );
+        const login = queryByTestId('parentDivId');
+        expect(login).not.toBeInTheDocument();
+    })
+
+    it('should have the email input field', () => {
+        render(<AdminLogin />);
+
+        const emailInput = screen.getByTestId("loginEmailId");
+        expect(emailInput).toBeInTheDocument();
+    });
+
+
+    it('should have the password input field', () => {
+        render(<AdminLogin />);
+
+        const password = screen.getByTestId("password");
+        expect(password).toBeInTheDocument();
+    });
+
+
+    it('should render login component correctly', () => {
+
+        const getById = queryByAttribute.bind(null, "id")
+        const buttonComponent = render(<AdminLogin/>);
+
+        expect(screen.getByTestId('loginDivId')).toBeInTheDocument();
+        expect(screen.getByTestId('emailAndPasswordId')).toBeInTheDocument();
+        expect(screen.getByTestId('emailId')).toBeInTheDocument();
+        expect(screen.getByTestId('passwordId')).toBeInTheDocument();
+        expect(getById(buttonComponent.container, "loginButton")).toBeInTheDocument()
+    });
+
+    it('should test that login button is not disabled after email and password has been entered', async () => {
+        render(<AdminLogin />);
+
+        const emailInput = screen.getByPlaceholderText('Enter email address');
+        const passwordInput = screen.getByPlaceholderText('Enter password');
+        const loginButton = screen.getByRole('button', { name: /login/i });
+
+        expect(loginButton).toBeDisabled();
+
+        await userEvent.type(emailInput, 'ola@gmail.com');
+        await userEvent.type(passwordInput, 'glory123');
+
+        expect(loginButton).not.toBeDisabled();
+    });
+
+    it('enables the login button after email and password have been entered', () => {
+        render(<AdminLogin />);
+
+        const emailInput = screen.getByPlaceholderText('Enter email address');
+        const passwordInput = screen.getByPlaceholderText('Enter password');
+        const loginButton = screen.getByRole('button', { name: /login/i });
+
+        expect(loginButton).toBeDisabled();
+
+        fireEvent.change(emailInput, { target: { value: 'user@example.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'password123' } });
+
+        expect(loginButton).toBeEnabled();
+    });
+});
