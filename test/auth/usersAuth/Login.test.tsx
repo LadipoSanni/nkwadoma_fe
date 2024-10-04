a8ea4492676178174037debb6312a5e7ee981183
Moyinoluwa <olawamidemoyinoluwamary@gmail.com>
2024-10-04 16:36:34 +0100

    I created an userAuth test directory under auth folder and moved all auth tests inside

--- test/auth/usersAuth/Login.test.tsx
+++ test/auth/usersAuth/Login.test.tsx
@@ -1,6 +1,6 @@
 import {render, screen, fireEvent, queryByAttribute} from '@testing-library/react';
 import '@testing-library/react';
-import Login from '@/features/auth/login/Login';
+import Login from '@/features/auth/usersAuth/login/Login';
 import {userEvent} from "@testing-library/user-event";
 import * as React from "react";
 
