f3d3a7b2c0ee5b8637eb42ad2fd78a82874aa533
Moyinoluwa <olawamidemoyinoluwamary@gmail.com>
2024-10-04 16:39:39 +0100

    Super admin login page

---
+++ src/app/auth/adminLogin/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AdminLogin from "@/features/auth/adminLogin/AdminLogin";
+
+const Page = () => {
+    return (
+        <AdminLogin/>
+    );
+};
+
+export default Page;
