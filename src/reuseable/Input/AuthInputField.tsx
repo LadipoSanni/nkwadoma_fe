168cec88024fbc5249708cf5380f0cd654c6a28d
koleajeolayinka <koleajeolayinka@gmail.com>
2024-10-07 17:50:40 +0100

    Add error message handling to AuthInputField and CreatePassword components
                   - Modified src/features/auth/create-password/CreatePassword.tsx to pass error messages based on password criteria.
                   - Updated src/reuseable/Input/AuthInputField.tsx to display error messages conditionally.
                   - Added tests in 	est/reuseable/Inputs/AuthInputField.test.tsx to verify error message rendering.

--- src/reuseable/Input/AuthInputField.tsx
+++ src/reuseable/Input/AuthInputField.tsx
@@ -8,9 +8,10 @@ interface ReusableInputProps extends InputHTMLAttributes<HTMLInputElement> {
     label: string;
     id: string;
     endAdornment?: React.ReactNode | string;
+    errorMessage?: string;
 }
 
-const AuthInputField: React.FC<ReusableInputProps> = ({ label, id, endAdornment, type, ...props }) => {
+const AuthInputField: React.FC<ReusableInputProps> = ({label, id, endAdornment, type, errorMessage, ...props}) => {
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
     const handleToggleVisibility = () => {
@@ -20,33 +21,39 @@ const AuthInputField: React.FC<ReusableInputProps> = ({ label, id, endAdornment,
     const renderEndAdornment = () => {
         if (typeof endAdornment === 'string') {
             return (
-                <span id={`end-adornment-${id}`} className={'text-[#BDC2C9] cursor-pointer text-[14px] font-normal leading-[22px]'} onClick={handleToggleVisibility}>
+                <span id={`end-adornment-${id}`}
+                      className={'text-[#475467] cursor-pointer text-[14px] font-normal leading-[22px]'}
+                      onClick={handleToggleVisibility}>
                     {isPasswordVisible ? 'Hide' : 'Show'}
                 </span>
             );
         } else {
             return (
-                <span id={`end-adornment-${id}`} style={{ cursor: 'pointer' }} onClick={handleToggleVisibility}>
-                    {isPasswordVisible ? <VisibilityIcon style={{ color: '#000000', width: '16px', height: '16px' }} /> : endAdornment}
+                <span id={`end-adornment-${id}`} style={{cursor: 'pointer'}} onClick={handleToggleVisibility}>
+                    {isPasswordVisible ?
+                        <VisibilityIcon style={{color: '#000000', width: '16px', height: '16px'}}/> : endAdornment}
                 </span>
             );
         }
     };
 
     return (
-        <div id={`custom-input-field-${id}`} className={`${inter.className} flex flex-col gap-[6px]`}>
+        <div id={`custom-input-field-${id}`} className={`${inter.className} flex flex-col gap-[8px]`}>
             <label id={`label-${id}`} htmlFor={id} className="text-[#101828] text-[14px] font-normal leading-[22px]">
                 {label}
             </label>
-            <div id={`input-container-${id}`} className="flex p-[10px_14px] items-center w-full gap-2 rounded-[var(--radius-xs,_4px)] border border-[#D0D5DD] bg-[#FFF] ">
+            <div id={`input-container-${id}`}
+                 className="flex px-[14px] items-center h-[3.375rem] w-full gap-2 rounded-[var(--radius-xs,_4px)] border border-[#D0D5DD] bg-[#FFF]">
                 <input
                     id={id}
                     type={isPasswordVisible ? 'text' : type}
-                    className={`${inter.className} w-full bg-transparent text-[#101828] text-[14px] font-normal leading-[22px] focus:outline-none focus:border-none`}
+                    className={`${inter.className} w-full h-full text-[#101828] text-[14px] font-normal leading-[22px] focus:outline-none`}
                     {...props}
                 />
                 {renderEndAdornment()}
             </div>
+            {errorMessage && <div className={'flex px-4 items-center'}><p id={`error-message-${id}`} className="text-[#E80000] text-[14px] font-normal">{errorMessage}</p>
+            </div>}
         </div>
     );
 };
