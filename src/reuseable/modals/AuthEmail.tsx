b7e7fc07675140094156438121ca608b6176f8e6
a-mariam <mariiam22222@gmail.com>
2024-10-10 08:57:38 +0100

    reformate files

--- src/reuseable/modals/AuthEmail.tsx
+++ src/reuseable/modals/AuthEmail.tsx
@@ -1,11 +1,10 @@
 'use client'
 import React from 'react';
 import Image from 'next/legacy/image'
-import { useRouter } from 'next/navigation'
+import {useRouter} from 'next/navigation'
 
 interface props {
     header?: string,
-    text?: string,
     email?: string | null,
 }
 
@@ -20,12 +19,12 @@ const AuthEmail = ({header, email}: props) => {
         >
             <div id={'successIconContainer'} className={``} data-testid={'successIconContainer'}>
                 <Image
-                 id={'successIcon'}
-                 data-testid={'successIcon'}
-                 width={50}
-                 height={50}
-                 src={'/Icon - Success (1).svg'}
-                 alt={'success icon'}
+                    id={'successIcon'}
+                    data-testid={'successIcon'}
+                    width={50}
+                    height={50}
+                    src={'/Icon - Success (1).svg'}
+                    alt={'success icon'}
                 />
             </div>
             <div className={`grid gap-3`}>
@@ -37,7 +36,7 @@ const AuthEmail = ({header, email}: props) => {
                 <div className={`text-xs  text-[#6c7685] grid gap-1 leading-3 w-[96%]`}>
                     <div>We’ve sent a link to create a new password to</div>
                     <div className={`flex w-[100%] break-words`}>
-                        <div  className={`text-[#6c7685] font-bold `}>{email}.</div>
+                        <div className={`text-[#6c7685] font-bold `}>{email}.</div>
                         <div className={`break-words `}>If it’s not in your inbox, check</div>
                     </div>
                     <div> your spam folder.</div>
@@ -45,10 +44,9 @@ const AuthEmail = ({header, email}: props) => {
             </div>
             <div className={`h-[70%] flex justify-normal relative`} id={'backToLoginFromResetPasswordStep2'}>
                 <div className={`w-fit absolute bottom-0`} onClick={() => router.push("/auth/login")}>
-                    <div  className={`text-[#0d9b48] text-sm underline `}>Back to Log in</div>
+                    <div className={`text-[#0d9b48] text-sm underline `}>Back to Log in</div>
                 </div>
             </div>
-
         </div>
     );
 };
