27a33a7c509b29957456fef3eddd39dfbf4c49e1
moyinoluw <olawamidemoyinoluwamary@gmail.com>
2024-10-15 13:09:00 +0100

    Program page

---
+++ src/app/(Admin)/program/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Program from "@/features/adminFeatures/program/Program";
+
+function page() {
+    return (
+        <div>
+            <Program/>
+        </div>
+    )
+}
+
+export default page
+
