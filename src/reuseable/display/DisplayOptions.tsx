5396a369b0b41650d40db8067c4e88086c8c6149
koleajeolayinka <koleajeolayinka@gmail.com>
2024-10-16 08:59:39 +0100

    feat: update program view and display options
    - Enhanced the program view in Index.tsx to support grid and list views.
    - Updated DisplayOptions.tsx to handle view switching between grid and list.

--- src/reuseable/display/DisplayOptions.tsx
+++ src/reuseable/display/DisplayOptions.tsx
@@ -2,19 +2,25 @@ import React from 'react';
 import {MdGridView, MdOutlineViewList} from 'react-icons/md';
 import {inter} from "@/app/fonts";
 
-const DisplayOptions = () => {
+const DisplayOptions = ({ setView }: { setView: (view: 'grid' | 'list') => void, activeView: 'grid' | 'list' }) => {
     return (
-        <div className={`${inter.className} flex p-0.5 items-center cursor-pointer gap-1 rounded-md bg-neutral100 w-[8.75rem] h-[2.0625rem]`}>
-            <div
-                className={"flex py-1 px-2 gap-1 items-center rounded-md bg-white shadow-custom text-meedlBlue h-[1.8125rem] w-[4.25rem]"}>
+        <section id="displayOptions"
+            className={`${inter.className} flex p-0.5 items-center cursor-pointer gap-1 rounded-md bg-neutral100 w-[8.75rem] h-[2.0625rem]`}>
+            <div id={'gridDisplay'}
+                className={"flex py-1 px-2 gap-1 items-center rounded-md bg-white shadow-custom text-meedlBlue h-[1.8125rem] w-[4.25rem]"}
+                onClick={() => setView('grid')}
+            >
                 <MdGridView className="w-5 h-5 text-meedlBlue"/>
-                <span className={'text-[14px] font-normal leading-[150%]'}>Grid</span>
+                <span id={'GridText'} className={'text-[14px] font-normal leading-[150%]'}>Grid</span>
             </div>
-            <div className="flex py-1 px-2 gap-1 items-center rounded-md text-grey450 h-[1.8125rem] w-[4.25rem]">
+            <div id={'listDisplay'}
+                className="flex py-1 px-2 gap-1 items-center rounded-md text-grey450 h-[1.8125rem] w-[4.25rem]"
+                onClick={() => setView('list')}
+            >
                 <MdOutlineViewList className="w-5 h-5 text-neutral950"/>
-                <span className={'text-[14px] font-normal leading-[150%]'}>List</span>
+                <span id={'listDisplay'} className={'text-[14px] font-normal leading-[150%]'}>List</span>
             </div>
-        </div>
+        </section>
     );
 };
 
