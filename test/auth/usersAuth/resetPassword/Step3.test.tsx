971eacd3485fc773c0764ba7b13d3c71fb85adeb
a-mariam <mariiam22222@gmail.com>
2024-10-24 16:48:51 +0100

    fixed build failure

--- test/auth/usersAuth/resetPassword/Step3.test.tsx
+++ test/auth/usersAuth/resetPassword/Step3.test.tsx
@@ -2,6 +2,14 @@ import "@testing-library/react"
 import {render, screen} from "@testing-library/react";
 import Index from "@/features/auth/usersAuth/reset-password/step3";
 
+jest.mock("next/navigation", () => ({
+    useRouter() {
+        return {
+            prefetch: () => null
+        };
+    }
+}));
+
 describe('test step 3', () => {
 
     it('should contain parent container', () => {
