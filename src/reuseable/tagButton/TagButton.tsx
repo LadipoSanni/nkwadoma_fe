0b22f7553c437b85cd0f4fc765f71f2905b5325f
moyinoluw <olawamidemoyinoluwamary@gmail.com>
2024-10-28 19:55:46 +0100

    Tag button reusable

--- src/reuseable/tagButton/TagButton.tsx
+++ src/reuseable/tagButton/TagButton.tsx
@@ -14,7 +14,7 @@ export const TagButton: React.FC<TagButtonProps> = ({tagCount, tagIcon:Icon, tag
         <div id="tagButton" data-testid="tagButton">
             <span
                 id={`content`}
-                className={`${inter.className} ${tagButtonStyle}  py-1 px-2 text-sm font-medium text-lightBlue300 w-full rounded-full border border-slate-200 flex items-center space-x-2`}>
+                className={`${inter.className} ${tagButtonStyle}  py-1 px-2 text-sm font-medium text-lightBlue300 w-full rounded-full flex items-center space-x-2`}>
                 <Icon className="w-4 h-4 text-meedlBlue"/>
                 <span className={`${inter.className} text-meedlBlue`}>
                     {tagCount} {tagText}
