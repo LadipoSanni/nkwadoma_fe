93e2183856c39d9454f3728e2d752a276b57654a
moyinoluw <olawamidemoyinoluwamary@gmail.com>
2024-11-01 13:14:10 +0100

    deleteId

--- src/reuseable/details/DeleteCohort.tsx
+++ src/reuseable/details/DeleteCohort.tsx
@@ -27,25 +27,25 @@ export const DeleteCohort: React.FC<deleteCohortProps> = ({setIsOpen}) => {
     return (
         <div>
 
-            <Image src={DeleteIcon} alt={"deleteIcon"} className={`md:w-[5rem] w-[4rem] md:h-[5rem] h-[4rem]`}
+            <Image src={DeleteIcon} alt={"deleteIcon"} className={`md:w-[5rem] w-[4rem] md:h-[5rem] h-[4rem]`}  id="deleteIcon" data-testid="deleteIcon"
                    width={200}
                    height={200}
             />
-            <div className={`pt-3`}>
-                <h1 className={`${cabinetGrotesk.className} text-2xl font-medium leading-7 text-meedlBlack`}>Delete
+            <div className={`pt-3`} id={"deleteTitle"} data-testid="deleteTitle">
+                <h1 className={`${cabinetGrotesk.className} text-2xl font-medium leading-7 text-meedlBlack`} id="title" data-testid="title">Delete
                     cohort <br/></h1>
 
-                <p className={`pt-3 text-sm font-normal leading-6 text-grey450`}>Are you sure you want to delete this
+                <p className={`pt-3 text-sm font-normal leading-6 text-grey450`} id={`questionTag`} data-testid={"questionTag"}>Are you sure you want to delete this
                     cohort? This action can&#39;t be undone </p>
             </div>
 
-            <div className={`flex flex-row justify-end gap-3 py-5`}>
+            <div className={`flex flex-row justify-end gap-3 py-5`} id={'buttons'} data-testid={'buttons'}>
                 <Button size={'lg'} variant={'outline'}
                         className={`${inter.className} h-12 text-sm font-semibold leading-6 text-meedlBlack`}
-                        onClick={handleCanCelCohort}>Cancel</Button>
+                        onClick={handleCanCelCohort} id={"cancelButton"} data-testid={"cancelButton"}>Cancel</Button>
                 <Button size={'lg'} variant={'secondary'}
                         className={`${inter.className} h-12 bg-error450 text-sm font-semibold leading-6 text-meedlWhite`}
-                        onClick={handleDeleteCohort}>Delete</Button>
+                        onClick={handleDeleteCohort} id={"deleteButton"} data-testid={"deleteButton"}>Delete</Button>
             </div>
         </div>
     )
