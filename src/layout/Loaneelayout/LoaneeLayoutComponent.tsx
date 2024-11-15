4ed6e09a6060af0069dc16bf8fd4f446ff3888f4
koleajeolayinka <koleajeolayinka@gmail.com>
2024-11-15 14:26:48 +0100

    update implement loan application

--- src/layout/Loaneelayout/LoaneeLayoutComponent.tsx
+++ src/layout/Loaneelayout/LoaneeLayoutComponent.tsx
@@ -9,13 +9,13 @@ interface LoaneeLayoutProps {
 const LoaneeLayoutComponent: React.FC<LoaneeLayoutProps> = ({children}) => {
     return (
         <main data-testid="AcceptedTraineeDashboardMain" id="AccepTrMain"
-              className="flex bg-LearnSpaceAthensGray">
+              className="flex bg-gray">
             <SideBar/>
             <div data-testid={"AcceptedTraineeDashboardContainer"} id={"AcceptTrContainer"}
                  className="flex flex-col flex-grow bg-gray">
                 <TopBar/>
                 <div id={'TopBarAndCenterComponent'}
-                     className={` w-full absolute bottom-0 px-5 md:pt-7 py-4 pb-6  h-[92vh]  grid  md:w-[84vw] md:h-[90vh] `}>
+                     className={` w-full absolute bottom-0 px-5 bg-gray md:pt-7 py-4 pb-6  h-[92vh]  grid  md:w-[84vw] md:h-[90vh] `}>
                     <div data-testid="AcceptedTraineeDashboardContent" id="AcceptTrContent"
                          className="md:px-10 px-3 md:py-5 pt-3 h-full rounded-md bg-meedlWhite">
                         {children}
