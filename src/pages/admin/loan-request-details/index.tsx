88cfc39df21f171094abd6545fa6fe31394db379
a-mariam <mariiam22222@gmail.com>
2024-11-22 15:52:21 +0100

    working on mobile details scrollbar

--- src/pages/admin/loan-request-details/index.tsx
+++ src/pages/admin/loan-request-details/index.tsx
@@ -57,49 +57,49 @@ const LoanDetails = () => {
             <div
                 id={`loanRequestDetailsComponent`}
                 data-testid={'loanRequestDetailsComponent'}
-                className={` ${styles.componentDivision}  grid grid-rows-2 bg-purple-100 w-full h-full md:flex  md:justify-between  pt-8 md:pt-4  md:w-full md:h-full  `}
+                className={`  overflow-y-auto md:overflow-y-hidden  grid grid-rows-2 w-full h-lvh md:flex  md:justify-between  pt-8 md:pt-4  md:w-full md:h-full  `}
                 // className={` w-full h-full md:w-full md:h-full rounded bg-lime-400 md:bg-lime-400 `}
             >
                 <div
                     id={`ImageComponentOnLoanRequestDetails`}
                     data-testid={`ImageComponentOnLoanRequestDetails`}
-                    className={` bg-red-300 grid gap-3 md:grid md:gap-6 md:w-fit md:h-fit   `}
+                    className={` bg-red-300 grid gap-3 h-fit md:grid md:gap-6 md:w-fit md:h-fit   `}
                  >
 
-                    {/*<Avatar id={'loaneeImageOnLoanRequestDetails'} data-testid={'loaneeImageOnLoanRequestDetails'} className={`h-[8rem] w-[8rem] md:w-[8rem] md:h-[8rem] `}>*/}
-                    {/*    <AvatarImage  src={`/234d70b3-ec71-4d68-8696-5f427a617fb7.jpeg`} alt="@shadcn" />*/}
-                    {/*    <AvatarFallback>CN</AvatarFallback>*/}
-                    {/*</Avatar>*/}
+                    <Avatar id={'loaneeImageOnLoanRequestDetails'} data-testid={'loaneeImageOnLoanRequestDetails'} className={`h-[8rem] w-[8rem] md:w-[8rem] md:h-[8rem] `}>
+                        <AvatarImage  src={`/234d70b3-ec71-4d68-8696-5f427a617fb7.jpeg`} alt="@shadcn" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
 
-                    {/*<div*/}
-                    {/*    className={`grid gap-2`}*/}
-                    {/*>*/}
-                    {/*    <div id={'loaneeNameOnLoanRequestDetails'} data-testid={'loaneeNameOnLoanRequestDetails'} className={`${cabinetGroteskRegular.className} text-black text-xl md:text-3xl  `}>Sarah Akinyemi</div>*/}
-                    {/*    <div*/}
-                    {/*        className={`flex gap-2  `}*/}
-                    {/*    >*/}
-                    {/*        <span id={'loaneeProgramOnLoanRequestDetails'} data-testid={'loaneeProgramOnLoanRequestDetails'} className={`${inter.className} text-sm text-black400`}>Product Design</span>*/}
-                    {/*        <span className={`${inter.className} text-sm text-black400 mt-auto mb-auto md:mt-auto md:mb-auto `}>.</span>*/}
-                    {/*        <span id={'loaneeCohortOnLoanRequestDetails'} data-testid={'loaneeCohortOnLoanRequestDetails'} className={`${inter.className} text-sm text-black400`}>Luminary</span>*/}
-                    {/*    </div>*/}
-                    {/*    <Button*/}
-                    {/*        id={'loaneeCheckCreditScoreOnLoanRequestDetails'}*/}
-                    {/*        data-testid={'loaneeCheckCreditScoreOnLoanRequestDetails'}*/}
-                    {/*        className={`${inter.className} w-fit px-4 md:mt-2 text-sm font-semibold text-meedlBlue border border-meedlBlue`}*/}
-                    {/*    >*/}
-                    {/*        Check credit score*/}
-                    {/*    </Button>*/}
-                    {/*</div>*/}
+                    <div
+                        className={`grid gap-2`}
+                    >
+                        <div id={'loaneeNameOnLoanRequestDetails'} data-testid={'loaneeNameOnLoanRequestDetails'} className={`${cabinetGroteskRegular.className} text-black text-xl md:text-3xl  `}>Sarah Akinyemi</div>
+                        <div
+                            className={`flex gap-2  `}
+                        >
+                            <span id={'loaneeProgramOnLoanRequestDetails'} data-testid={'loaneeProgramOnLoanRequestDetails'} className={`${inter.className} text-sm text-black400`}>Product Design</span>
+                            <span className={`${inter.className} text-sm text-black400 mt-auto mb-auto md:mt-auto md:mb-auto `}>.</span>
+                            <span id={'loaneeCohortOnLoanRequestDetails'} data-testid={'loaneeCohortOnLoanRequestDetails'} className={`${inter.className} text-sm text-black400`}>Luminary</span>
+                        </div>
+                        <Button
+                            id={'loaneeCheckCreditScoreOnLoanRequestDetails'}
+                            data-testid={'loaneeCheckCreditScoreOnLoanRequestDetails'}
+                            className={`${inter.className} w-fit px-4 md:mt-2 text-sm font-semibold text-meedlBlue border border-meedlBlue`}
+                        >
+                            Check credit score
+                        </Button>
+                    </div>
                 </div>
                 <div
-                    className={`  overflow-x-hidden overflow-y-auto md:w-fit bg-lime-400  w-full md:h-fit border border-gray500 rounded-md md:px-4 md:py-4 px-3 py-3 grid gap-1 md:grid `}
+                    className={`  overflow-x-hidden overflow-y-auto md:w-fit  w-full md:h-fit border border-gray500 rounded-md md:px-4 md:py-4 px-3 py-3 grid gap-1 md:grid `}
                 >
-                   {/*<div*/}
-                   {/*    className={` overflow-x-auto  scrollbar scrollbar-thumb-purple-900 scrollbar-track-purple-100 md:w-fit w-full h-[10px] md:h-fit  flex md:flex `}*/}
-                   {/*>*/}
-                   {/*   <TabConnector tabNames={loanRequestDetailsTab} currentTab={currentTab}/>*/}
-                   {/*</div>*/}
-                   {/* <LoanDetailsCard dataList={dataList} id={"basicDetailsOnLoanDetails"} showNextButton={true} backButtonId={"continueToLoan"} nextButtonTittle={'continue'} />*/}
+                   <div
+                       className={` overflow-x-auto md:w-fit w-full h-[10px] md:h-fit  flex md:flex `}
+                   >
+                      <TabConnector tabNames={loanRequestDetailsTab} currentTab={currentTab}/>
+                   </div>
+                    <LoanDetailsCard dataList={dataList} id={"basicDetailsOnLoanDetails"} showNextButton={true} backButtonId={"continueToLoan"} nextButtonTittle={'continue'} />
 
                 </div>
             </div>
