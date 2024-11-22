75cded599d7d417f4f629b845d0cbcbe407aeeaf
a-mariam <mariiam22222@gmail.com>
2024-11-22 13:35:45 +0100

    almost done with details mobile view

--- src/pages/admin/loan-request-details/index.tsx
+++ src/pages/admin/loan-request-details/index.tsx
@@ -40,9 +40,9 @@ const LoanDetails = () => {
     ];
 
     const component = [
-        {"basicDetails": <LoanDetailsCard dataList={dataList}  />},
-        {"AdditionalDetails": <LoanDetailsCard/>},
-        {"LoanDetails": <LoanDetailsCard/>}
+        {"basicDetails": <LoanDetailsCard dataList={dataList} id={"basicDetailsOnLoanDetails"} showNextButton={true} backButtonId={"continueToLoan"} nextButtonTittle={'continue'} />},
+        {"AdditionalDetails": <LoanDetailsCard id={"basicDetailsOnLoanDetails"} />},
+        {"LoanDetails": <LoanDetailsCard id={"basicDetailsOnLoanDetails"}/>}
 
     ]
 
@@ -50,56 +50,56 @@ const LoanDetails = () => {
         <div
             id={"loanRequestDetails"}
             data-testid={"loanRequestDetails"}
-            className={` ${styles.container}  md:px-8 w-full h-full overflow-y-scroll md:overflow-y-hidden px-4 pt-4 md:pt-4 `}
+            className={` ${styles.container}  md:px-8 w-full h-full   px-4 pt-4 md:pt-4 `}
             // className={`w-full h-full md:grid grid gap-2 md:px-8 px-4 pt-4 md:pt-4  md:w-full md:h-full  `}
         >
             <BackButton handleClick={backToLoanRequest} iconRight={true} text={"Back to loan request"} id={"loanRequestDetailsBackButton"} textColor={'#142854'}  />
             <div
                 id={`loanRequestDetailsComponent`}
                 data-testid={'loanRequestDetailsComponent'}
-                className={`w-full h-full md:flex  md:justify-between grid gap-5 pt-8 md:pt-4  md:w-full md:h-full  `}
+                className={` ${styles.componentDivision}  grid grid-rows-2 bg-purple-100 w-full h-full md:flex  md:justify-between  pt-8 md:pt-4  md:w-full md:h-full  `}
                 // className={` w-full h-full md:w-full md:h-full rounded bg-lime-400 md:bg-lime-400 `}
             >
                 <div
                     id={`ImageComponentOnLoanRequestDetails`}
                     data-testid={`ImageComponentOnLoanRequestDetails`}
-                    className={` grid gap-3 md:grid md:gap-6 md:w-fit md:h-fit   `}
+                    className={` bg-red-300 grid gap-3 md:grid md:gap-6 md:w-fit md:h-fit   `}
                  >
 
-                    <Avatar id={'loaneeImageOnLoanRequestDetails'} data-testid={'loaneeImageOnLoanRequestDetails'} className={`h-[8rem] w-[8rem] md:w-[8rem] md:h-[8rem] `}>
-                        <AvatarImage  src={`/234d70b3-ec71-4d68-8696-5f427a617fb7.jpeg`} alt="@shadcn" />
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
+                    {/*<Avatar id={'loaneeImageOnLoanRequestDetails'} data-testid={'loaneeImageOnLoanRequestDetails'} className={`h-[8rem] w-[8rem] md:w-[8rem] md:h-[8rem] `}>*/}
+                    {/*    <AvatarImage  src={`/234d70b3-ec71-4d68-8696-5f427a617fb7.jpeg`} alt="@shadcn" />*/}
+                    {/*    <AvatarFallback>CN</AvatarFallback>*/}
+                    {/*</Avatar>*/}
 
-                    <div
-                        className={`grid gap-2`}
-                    >
-                        <div id={'loaneeNameOnLoanRequestDetails'} data-testid={'loaneeNameOnLoanRequestDetails'} className={`${cabinetGroteskRegular.className} text-black text-xl md:text-3xl  `}>Sarah Akinyemi</div>
-                        <div
-                            className={`flex gap-2  `}
-                        >
-                            <span id={'loaneeProgramOnLoanRequestDetails'} data-testid={'loaneeProgramOnLoanRequestDetails'} className={`${inter.className} text-sm text-black400`}>Product Design</span>
-                            <span className={`${inter.className} text-sm text-black400 mt-auto mb-auto md:mt-auto md:mb-auto `}>.</span>
-                            <span id={'loaneeCohortOnLoanRequestDetails'} data-testid={'loaneeCohortOnLoanRequestDetails'} className={`${inter.className} text-sm text-black400`}>Luminary</span>
-                        </div>
-                        <Button
-                            id={'loaneeCheckCreditScoreOnLoanRequestDetails'}
-                            data-testid={'loaneeCheckCreditScoreOnLoanRequestDetails'}
-                            className={`${inter.className} w-fit px-4 md:mt-2 text-sm font-semibold text-meedlBlue border border-meedlBlue`}
-                        >
-                            Check credit score
-                        </Button>
-                    </div>
+                    {/*<div*/}
+                    {/*    className={`grid gap-2`}*/}
+                    {/*>*/}
+                    {/*    <div id={'loaneeNameOnLoanRequestDetails'} data-testid={'loaneeNameOnLoanRequestDetails'} className={`${cabinetGroteskRegular.className} text-black text-xl md:text-3xl  `}>Sarah Akinyemi</div>*/}
+                    {/*    <div*/}
+                    {/*        className={`flex gap-2  `}*/}
+                    {/*    >*/}
+                    {/*        <span id={'loaneeProgramOnLoanRequestDetails'} data-testid={'loaneeProgramOnLoanRequestDetails'} className={`${inter.className} text-sm text-black400`}>Product Design</span>*/}
+                    {/*        <span className={`${inter.className} text-sm text-black400 mt-auto mb-auto md:mt-auto md:mb-auto `}>.</span>*/}
+                    {/*        <span id={'loaneeCohortOnLoanRequestDetails'} data-testid={'loaneeCohortOnLoanRequestDetails'} className={`${inter.className} text-sm text-black400`}>Luminary</span>*/}
+                    {/*    </div>*/}
+                    {/*    <Button*/}
+                    {/*        id={'loaneeCheckCreditScoreOnLoanRequestDetails'}*/}
+                    {/*        data-testid={'loaneeCheckCreditScoreOnLoanRequestDetails'}*/}
+                    {/*        className={`${inter.className} w-fit px-4 md:mt-2 text-sm font-semibold text-meedlBlue border border-meedlBlue`}*/}
+                    {/*    >*/}
+                    {/*        Check credit score*/}
+                    {/*    </Button>*/}
+                    {/*</div>*/}
                 </div>
                 <div
-                    className={`  overflow-x-hidden overflow-y-auto md:w-fit w-full md:h-fit border border-gray500 rounded-md md:px-4 md:py-4 px-3 py-3 grid gap-1 md:grid `}
+                    className={`  overflow-x-hidden overflow-y-auto md:w-fit bg-lime-400  w-full md:h-fit border border-gray500 rounded-md md:px-4 md:py-4 px-3 py-3 grid gap-1 md:grid `}
                 >
-                   <div
-                       className={` overflow-x-auto  scrollbar scrollbar-thumb-purple-900 scrollbar-track-purple-100 md:w-fit w-full h-[10px] md:h-fit  flex md:flex `}
-                   >
-                      <TabConnector tabNames={loanRequestDetailsTab} currentTab={currentTab}/>
-                   </div>
-                    {/*<LoanDetailsCard dataList={dataList}  />*/}
+                   {/*<div*/}
+                   {/*    className={` overflow-x-auto  scrollbar scrollbar-thumb-purple-900 scrollbar-track-purple-100 md:w-fit w-full h-[10px] md:h-fit  flex md:flex `}*/}
+                   {/*>*/}
+                   {/*   <TabConnector tabNames={loanRequestDetailsTab} currentTab={currentTab}/>*/}
+                   {/*</div>*/}
+                   {/* <LoanDetailsCard dataList={dataList} id={"basicDetailsOnLoanDetails"} showNextButton={true} backButtonId={"continueToLoan"} nextButtonTittle={'continue'} />*/}
 
                 </div>
             </div>
--- src/reuseable/cards/loan-details-card/index.tsx
+++ src/reuseable/cards/loan-details-card/index.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
+import {Button} from "@/components/ui/button";
 
 
 interface props {
     dataList?: { label: string; value: string; }[];
+    showBackButton?: boolean,
+    showNextButton?: boolean,
+    backButtonTittle?: string,
+    nextButtonTittle?: string,
+    handleNextButton?: ()=> void,
+    handleBackButton?: ()=> void,
+    nextButtonId?: string,
+    backButtonId?: string,
+    id: string,
 }
 
-const LoanDetailsCard = ({dataList}: props) => {
+const LoanDetailsCard = ({dataList, showBackButton, showNextButton, nextButtonTittle, backButtonTittle}: props) => {
     return (
         <div
-            id={'loanDetailsCardMainComponent'}
-            data-testid={'loanDetailsCardMainComponent'}
-            className={` bg-[#f9f9f9] rounded-md w-full grid gap-10 h-fit px-3 py-3   `}
+            className={`w-full h-fit md:w-full grid gap-4 md:grid md:gap-4 md:h-fit px-2 py-2 md:px-2 md:py-2 `}
         >
-            {dataList?.map((data, index) => (
-                <div
-                    key={index}
-                    className={`md:w-full md:h-fit w-full h-fit md:flex md:justify-between grid gap-2 `}
-                >
-                    <div className={`text-sm text-black300 `}>{data.label}</div>
-                    <div className={`text-sm text-[#101828]`}>{data.value}</div>
-                </div>
-            ))}
+            <div
+                id={'loanDetailsCardMainComponent'}
+                data-testid={'loanDetailsCardMainComponent'}
+                className={` bg-[#f9f9f9] rounded-md w-full grid gap-10 h-fit px-3 py-3   `}
+            >
+                {dataList?.map((data, index) => (
+                    <div
+                        key={index}
+                        className={`md:w-full md:h-fit w-full h-fit md:flex md:justify-between grid gap-2 `}
+                    >
+                        <div className={`text-sm text-black300 `}>{data.label}</div>
+                        <div className={`text-sm text-[#101828]`}>{data.value}</div>
+                    </div>
+                ))}
+            </div>
+            <div
+                id={'buttonComponent'}
+                className={`w-full h-fit grid gap-3 md:flex md:justify-end  `}
+            >
+                {showBackButton &&
+                    <Button
+
+                    >
+                        {backButtonTittle}
+                    </Button>
+                }
+                {showNextButton &&
+                    <Button
+                        className={` bg-meedlBlue text-white hover:bg-meedleBlue hover:text-white py-6 px-5 `}
+                    >
+                        {nextButtonTittle}
+                    </Button>
+                }
+            </div>
 
         </div>
     );
