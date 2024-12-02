6b1261732f16b21594b76aff6480be92d6a8863c
Moyinoluwa <olawamidemoyinoluwamary@gmail.com>
2024-12-02 13:26:30 +0100

    card height

--- src/reuseable/cards/AllProgramsList.tsx
+++ src/reuseable/cards/AllProgramsList.tsx
@@ -37,7 +37,7 @@ const AllProgramsCard: React.FC<ProgramList> = ({id, title, description, tagButt
 
 
     return (
-        <Card  id={`allProgramsCard-${id}`} data-testid="allProgramsCard"  className="w-full md:max-w-lg  border border-grey50 rounded-lg cursor-pointer" >
+        <Card  id={`allProgramsCard-${id}`} data-testid="allProgramsCard"  className="w-full md:max-w-lg h-52  border border-grey50 rounded-lg cursor-pointer" >
             <CardHeader id={`header-${id}`} data-testid="header" className="flex flex-row justify-between items-center">
                 <CardTitle id={`title-${id}`} data-testid="title" className={`${inter.className} text-lg font-medium text-[#101828]`}>{title}</CardTitle>
                     <Kebab kebabOptions={dropdownOption} icon={FiMoreVertical} handleDropDownClick={handleCardDropDownClick}/>
