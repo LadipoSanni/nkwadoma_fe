2d53e3b71548e64d7492122e4b0b3afa1688f51c
koleajeolayinka <koleajeolayinka@gmail.com>
2024-12-03 16:49:06 +0100

    feat: create loan offer dialog - update AuthInputField Component padding - update Organiation details IDs

--- src/features/portfolio-manager/organizationDetails/Index.tsx
+++ src/features/portfolio-manager/organizationDetails/Index.tsx
@@ -40,7 +40,6 @@ const OrganizationDetails = () => {
     };
 
     const setAdminView = (data: typeof adminData) => {
-        // Define the function logic here
         console.log(data);
     };
 
@@ -50,7 +49,7 @@ const OrganizationDetails = () => {
         { label: "Phone number", value: "+2349000021111" },
         {
             label: "Status",
-            value: <span className={`rounded-[32px] h-[21px] w-[58px] flex items-center justify-center ${details.cohortStatus === "Active" ? "bg-[#E7F5EC] text-[#063F1A]" : "bg-[#FEF6E8] text-[#66440A]"}`}>
+            value: <span id="status" className={`rounded-[32px] h-[21px] w-[58px] flex items-center justify-center ${details.cohortStatus === "Active" ? "bg-[#E7F5EC] text-[#063F1A]" : "bg-[#FEF6E8] text-[#66440A]"}`}>
                 {details.cohortStatus}
             </span>
         },
@@ -76,11 +75,11 @@ const OrganizationDetails = () => {
         { title: 'Full name', sortable: true, id: 'name', selector: (row: TableRowData) => row.name },
         { title: 'Email', sortable: true, id: 'email', selector: (row: TableRowData) => row.email },
         {
-            title: <div className='relative md:right-4 md:left-4'>Status</div>,
+            title: <div id="adminStatusHeader" className='relative md:right-4 md:left-4'>Status</div>,
             sortable: true,
             id: 'adminStatus',
             selector: (row: TableRowData) => (
-                <span className={`pt-1 pb-1 pr-3 pl-3 rounded-xl ${row.adminStatus === "Active" ? "text-[#063F1A] bg-[#E7F5EC]" : row.adminStatus === "Invited" ? "text-[#142854] bg-[#F3F8FF]" : "text-[#59100D] bg-[#FBE9E9]"}`}>
+                <span id="adminStatus" className={`pt-1 pb-1 pr-3 pl-3 rounded-xl ${row.adminStatus === "Active" ? "text-[#063F1A] bg-[#E7F5EC]" : row.adminStatus === "Invited" ? "text-[#142854] bg-[#F3F8FF]" : "text-[#59100D] bg-[#FBE9E9]"}`}>
                     {row.adminStatus ?? "Deactivated"}
                 </span>
             ),
@@ -94,43 +93,43 @@ const OrganizationDetails = () => {
     }, [adminData]);
 
     return (
-        <main className={`${inter.className} grid gap-7 py-6 px-3 md:px-10`}>
-            <div onClick={handleBackClick} className={'inline-flex  w-[10.375rem] cursor-pointer gap-2 items-center'}>
+        <main id="organizationDetailsMain" className={`${inter.className} grid gap-7 py-6 px-3 md:px-10`}>
+            <div id="backButton" onClick={handleBackClick} className={'inline-flex  w-[10.375rem] cursor-pointer gap-2 items-center'}>
                 <MdOutlineArrowBack className={'h-5 w-5 text-meedlBlue'} />
                 <p className={'text-meedlBlue font-medium text-[14px] leading-[150%]'}>Back to organization</p>
             </div>
-            <Tabs defaultValue="details">
-                <TabsList className={'p-0.5 gap-1 h-[2.0625rem] items-center cursor-pointer rounded-md bg-neutral100'}>
-                    <TabsTrigger value="details" className={'py-1 px-2 gap-1 items-center rounded-md h-[1.8125rem] w-[3.875rem] data-[state=active]:shadow-custom'}>Details</TabsTrigger>
-                    <TabsTrigger value="admins" className={'py-1 px-2 gap-1 items-center rounded-md h-[1.8125rem] data-[state=active]:shadow-custom'}>Admins</TabsTrigger>
+            <Tabs id="organizationTabs" defaultValue="details">
+                <TabsList id="tabsList" className={'p-0.5 gap-1 h-[2.0625rem] items-center cursor-pointer rounded-md bg-neutral100'}>
+                    <TabsTrigger id="detailsTab" value="details" className={'py-1 px-2 gap-1 items-center rounded-md h-[1.8125rem] w-[3.875rem] data-[state=active]:shadow-custom'}>Details</TabsTrigger>
+                    <TabsTrigger id="adminsTab" value="admins" className={'py-1 px-2 gap-1 items-center rounded-md h-[1.8125rem] data-[state=active]:shadow-custom'}>Admins</TabsTrigger>
                 </TabsList>
-                <TabsContent value="details">
-                    <div className={'mt-10 grid gap-9 md:gap-0 md:flex  md:justify-between'}>
-                        <section className={'relative'}>
-                            <Image src={'/asset/Image/Banner.svg'} alt={'banner'} height={134} width={351} />
-                            <div className={'flex items-center justify-center absolute top-[70px] left-[14px] w-[140px] h-[140px] bg-greyBase200 rounded-full border-[10px] border-meedlWhite'}>
-                                <Image src={'/asset/Image/semicolonLogo.svg'} alt={'organization logo'} height={70} width={70} className={''} />
+                <TabsContent id="detailsContent" value="details">
+                    <div id="detailsContainer" className={'mt-10 grid gap-9 md:gap-0 md:flex  md:justify-between'}>
+                        <section id="bannerSection" className={'relative'}>
+                            <Image id="bannerImage" src={'/asset/Image/Banner.svg'} alt={'banner'} height={134} width={351} />
+                            <div id="logoContainer" className={'flex items-center justify-center absolute top-[70px] left-[14px] w-[140px] h-[140px] bg-greyBase200 rounded-full border-[10px] border-meedlWhite'}>
+                                <Image id="organizationLogo" src={'/asset/Image/semicolonLogo.svg'} alt={'organization logo'} height={70} width={70} className={''} />
                             </div>
-                            <div className={'grid mt-24 gap-3 '}>
-                                <h1 className={`${cabinetGrotesk.className} text-black500 font-medium text-[24px] leading-[120%]`}>Semicolon africa</h1>
-                                <div className={'flex items-center gap-2'}>
+                            <div id="organizationInfo" className={'grid mt-24 gap-3 '}>
+                                <h1 id="organizationName" className={`${cabinetGrotesk.className} text-black500 font-medium text-[24px] leading-[120%]`}>Semicolon africa</h1>
+                                <div id="organizationWebsite" className={'flex items-center gap-2'}>
                                     <IoGlobeOutline className={'h-5 w-5 text-meedlBlue'} />
                                     <p className={'text-meedlBlue text-[14px] font-medium leading-[150%]'}>www.semicolon.africa</p>
                                 </div>
                             </div>
                         </section>
-                        <div className={`relative md:w-6/12 md:pt-0 pt-0`}>
+                        <div id="detailsTabContainer" className={`relative md:w-6/12 md:pt-0 pt-0`}>
                             <DetailsTabContainer isTable={false} isNotTableDataList={loanDetail} dataList={dataList} tabTitle1={"Organization details"} tabTitle2={"Loan details"} />
                         </div>
                     </div>
                 </TabsContent>
 
-                <TabsContent value="admins" className={'mt-4'}>
-                    <section className={'md:flex md:justify-between grid  gap-5 items-center mt-10'}>
-                        <SearchInput id={'programCohortSearch'} value="search" onChange={() => { }} />
-                        <Button className={'h-[2.8125rem] md:w-[10.375rem] w-full rounded-md bg-meedlBlue hover:bg-meedlBlue text-meedlWhite text-[0.875rem] font-semibold leading-[150%] '} onClick={handleInviteClick}>Invite admin</Button>
+                <TabsContent id="adminsContent" value="admins" className={'mt-4'}>
+                    <section id="adminActions" className={'md:flex md:justify-between grid  gap-5 items-center mt-10'}>
+                        <SearchInput id={'organizationSearch'} value="search" onChange={() => { }} />
+                        <Button id="inviteAdminButton" className={'h-[2.8125rem] md:w-[10.375rem] w-full rounded-md bg-meedlBlue hover:bg-meedlBlue text-meedlWhite text-[0.875rem] font-semibold leading-[150%] '} onClick={handleInviteClick}>Invite admin</Button>
                     </section>
-                    <div id="programListView" className={'grid mt-7'} style={{ height: '62vh', gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))' }}>
+                    <div id="adminListView" className={'grid mt-7'} style={{ height: '62vh', gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))' }}>
                         <LoanProductTable
                             tableData={adminData?.data.body}
                             tableHeader={adminsHeader}
