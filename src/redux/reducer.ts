25af286f651a5a15b8d2a86ce218c87fe3239e32
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 <ned@workmail.com>
2024-12-04 07:59:54 +0100

    fixed to a certain extent

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -7,18 +7,15 @@ import { programApi } from "@/service/admin/program_query";
 import { cohortApi } from "@/service/admin/cohort_query";
 import { userApi } from "@/service/users/api";
 import {organizationApi} from "@/service/admin/organization";
-import {loaneeSlice} from "@/redux/slice/loanee/Loanee_slice";
-// import {loaneeApi} from "@/service/users/Loanee_query";
+
 
 const appReducer = combineReducers({
 
     adminLayout: adminLayoutReducer,
     selectedLoan: selectedLoanReducer,
     authSlice: authSliceReducer,
-    loaneeReducer: loaneeSlice,
     [authApi.reducerPath]: authApi.reducer,
     [programApi.reducerPath]: programApi.reducer,
-    // [loaneeApi.reducerPath]: loaneeApi.reducer,
     [cohortApi.reducerPath]: cohortApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
     [organizationApi.reducerPath] : organizationApi.reducer,
