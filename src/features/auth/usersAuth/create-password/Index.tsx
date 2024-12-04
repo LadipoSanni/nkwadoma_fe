c2ee5a050e1ff7c73c4a4563f2d7a66fd518ed52
Moyinoluwa <olawamidemoyinoluwamary@gmail.com>
2024-12-04 16:50:13 +0100

    removed duplicated constant

--- src/features/auth/usersAuth/create-password/Index.tsx
+++ src/features/auth/usersAuth/create-password/Index.tsx
@@ -50,7 +50,7 @@ const CreatePassword = () => {
 
     const handleConfirmPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setConfirmPassword(e.target.value);
-    };    const searchParams = useSearchParams()
+    };
 
 
     const remainingCriteria = criteriaMessages.filter((_, index) => !criteriaStatus[index]);
