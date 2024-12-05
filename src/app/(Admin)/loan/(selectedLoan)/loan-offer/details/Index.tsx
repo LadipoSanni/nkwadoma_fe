e71dc8a4804c7bf3d43db75deb4b38e8852ee645
koleajeolayinka <koleajeolayinka@gmail.com>
2024-12-05 13:26:08 +0100

    update loan offer details

---
+++ src/app/(Admin)/loan/(selectedLoan)/loan-offer/details/Index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoanOfferDetails from "@/pages/admin/loanOfferDetails/Index";
+
+const Index = () => {
+    return (
+        <LoanOfferDetails/>
+    );
+};
+
+export default Index;
---
+++ src/pages/admin/loanOfferDetails/Index.tsx
@@ -0,0 +1,106 @@
+"use client"
+import React, {useState} from 'react';
+import BackButton from "@/components/back-button";
+import {useRouter} from "next/navigation";
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+import {cabinetGroteskRegular, inter} from "@/app/fonts";
+import {Button} from "@/components/ui/button";
+import TabConnector from "@/reuseable/details/tab-connector";
+import styles from "./index.module.css"
+import LoanDetailsCard from "@/reuseable/cards/loan-details-card";
+
+
+const LoanOfferDetails = () => {
+    const router = useRouter()
+    const [currentTab] = useState(0)
+
+    const backToLoanRequest = () => {
+        router.push("/loan/loan-request")
+    }
+
+    const loanRequestDetailsTab = [
+        "Basic details",
+        "Additional details",
+        "Loan details"
+    ]
+    const dataList = [
+        {label: "Alternate email address", value: "mariiam@gmail.com"},
+        {label: "Alternate phone number", value: "+2347039393309"},
+        {label: "Alternative residential address", value: "300, Herbert Macaulay Way, Alagomeji, Sabo, Yaba"},
+        {label: "Next of kin", value: "Samuel koko"},
+        {label: "Next of kin phone number", value: "0903849449"},
+        {label: "Next of kin email address", value: "maria@gmail.com"},
+        {label: "Next of kin relation", value: "brother"},
+
+    ];
+    return (
+        <div
+            id={"loanRequestDetails"}
+            data-testid={"loanRequestDetails"}
+            className={`  md:px-8 w-full h-full  px-3 pt-4 md:pt-4 `}
+        >
+            <BackButton handleClick={backToLoanRequest} iconRight={true} text={"Back to loan request"}
+                        id={"loanRequestDetailsBackButton"} textColor={'#142854'}/>
+
+
+            <div
+                id={`ImageComponentOnLoanRequestDetails`}
+                data-testid={`ImageComponentOnLoanRequestDetails`}
+                className={`  mt-4 mb-4 grid md:flex gap-3 h-fit md:justify-between md:gap-6 md:w-full  md:h-fit   `}
+            >
+
+                <div>
+                    <Avatar id={'loaneeImageOnLoanRequestDetails'} data-testid={'loaneeImageOnLoanRequestDetails'}
+                            className={`h-[8rem] w-[8rem] md:w-[8rem] md:h-[8rem] `}>
+                        <AvatarImage src={`/234d70b3-ec71-4d68-8696-5f427a617fb7.jpeg`} alt="@shadcn"/>
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+
+                    <div
+                        className={`grid gap-2 mt-4`}
+                    >
+                        <div id={'loaneeNameOnLoanRequestDetails'} data-testid={'loaneeNameOnLoanRequestDetails'}
+                             className={`${cabinetGroteskRegular.className} text-black text-xl md:text-3xl  `}>Sarah
+                            Akinyemi
+                        </div>
+                        <div
+                            className={`flex gap-2  `}
+                        >
+                        <span id={'loaneeProgramOnLoanRequestDetails'} data-testid={'loaneeProgramOnLoanRequestDetails'}
+                              className={`${inter.className} text-sm text-black400`}>Product Design</span>
+                            <span
+                                className={`${inter.className} text-sm text-black400 mt-auto mb-auto md:mt-auto md:mb-auto `}>.</span>
+                            <span id={'loaneeCohortOnLoanRequestDetails'} data-testid={'loaneeCohortOnLoanRequestDetails'}
+                                  className={`${inter.className} text-sm text-black400`}>Luminary</span>
+                        </div>
+                        <Button
+                            id={'loaneeCheckCreditScoreOnLoanRequestDetails'}
+                            data-testid={'loaneeCheckCreditScoreOnLoanRequestDetails'}
+                            className={`${inter.className} w-fit px-4 md:mt-2 text-sm font-semibold text-meedlBlue border border-meedlBlue`}
+                        >
+                            Check credit score
+                        </Button>
+                    </div>
+                </div>
+                <div
+                    className={`  overflow-x-hidden overflow-y-auto md:w-fit mt-4   w-full md:h-fit border border-gray500 rounded-md md:px-4 md:py-4 py-3 grid gap-3 md:grid `}
+                >
+                    <div
+                        className={` ${styles.tabConnector} md:w-fit pl-1  h-fit md:h-fit  flex md:flex `}
+                    >
+                        <TabConnector tabNames={loanRequestDetailsTab} currentTab={currentTab}/>
+                    </div>
+                    <LoanDetailsCard dataList={dataList} id={"basicDetailsOnLoanDetails"} showNextButton={true}
+                                     backButtonId={"continueToLoan"} nextButtonTittle={'continue'}/>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LoanOfferDetails;
