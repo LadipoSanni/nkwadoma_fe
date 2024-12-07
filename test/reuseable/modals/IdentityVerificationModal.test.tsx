f1f7a3d2dfc66a89a309c1b749328872cd48b07a
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 <ned@workmail.com>
2024-12-07 17:38:59 +0100

    re worked the test

--- test/reuseable/modals/IdentityVerificationModal.test.tsx
+++ test/reuseable/modals/IdentityVerificationModal.test.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import IdentityVerificationModal from '@/reuseable/modals/IdentityVerificationModal'; // Adjust the import path accordingly
+import IdentityVerificationModal from '@/reuseable/modals/IdentityVerificationModal';
+import {Providers} from "@/app/provider"; // Adjust the import path accordingly
 
 describe('IdentityVerificationModal', () => {
+    const testId= "test-id";
+    const mockOnClose = jest.fn();
     it('renders modal when isOpen is true', () => {
-        render(<IdentityVerificationModal isOpen={true} onClose={jest.fn()}/>);
+        render(
+            <Providers>
+                <IdentityVerificationModal isOpen={true} onClose={mockOnClose} loanReferralId={testId} onThirdStepContinue={jest.fn()}/>
+            </Providers>
+        );
         expect(screen.getByText(/Provide your details/i)).toBeInTheDocument();
     });
 
     it('calls onClose when Cancel button is clicked', () => {
-        const mockOnClose = jest.fn();
-        render(<IdentityVerificationModal isOpen={true} onClose={mockOnClose}/>);
+        render(
+            <Providers>
+                <IdentityVerificationModal isOpen={true} onClose={mockOnClose} loanReferralId={testId} onThirdStepContinue={jest.fn()}/>
+            </Providers>
+        );
         fireEvent.click(screen.getByText(/Cancel/i));
         expect(mockOnClose).toHaveBeenCalled();
     });
 
     it('displays second modal upon successful form submission', async () => {
-        const mockOnClose = jest.fn();
-
-        render(<IdentityVerificationModal isOpen={true} onClose={mockOnClose}/>);
-
+        render(
+            <Providers>
+                <IdentityVerificationModal isOpen={true} onClose={mockOnClose} loanReferralId={testId} onThirdStepContinue={jest.fn()}/>
+            </Providers>
+        );
         fireEvent.input(screen.getByPlaceholderText(/Enter BVN/i), {
             target: { value: '12345678901' },
         });
@@ -37,14 +48,21 @@ describe('IdentityVerificationModal', () => {
     });
 
     it('collapses information when trigger clicked', () => {
-        render(<IdentityVerificationModal isOpen={true} onClose={jest.fn()}/>);
+        render(
+            <Providers>
+                <IdentityVerificationModal isOpen={true} onClose={mockOnClose} loanReferralId={testId} onThirdStepContinue={jest.fn()}/>
+            </Providers>
+        );
         const triggers = screen.getAllByText(/why do we need/i);
         fireEvent.click(triggers[0]);
         expect(screen.getByText(/We request for your BVN/i)).toBeInTheDocument();
     });
 
     it('does not render modal when isOpen is false', () => {
-        render(<IdentityVerificationModal isOpen={false} onClose={jest.fn()}/>);
-        expect(screen.queryByText(/Provide your details/i)).not.toBeInTheDocument();
+        render(
+            <Providers>
+                <IdentityVerificationModal isOpen={false} onClose={mockOnClose} loanReferralId={testId} onThirdStepContinue={jest.fn()}/>
+            </Providers>
+        );expect(screen.queryByText(/Provide your details/i)).not.toBeInTheDocument();
     });
 });
