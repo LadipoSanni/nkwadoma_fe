92c02eccef74ed7a0099d7ad65abeec2f982d196
a-mariam <mariiam22222@gmail.com>
2024-12-10 11:12:46 +0100

    commented middleware code

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,10 @@ import {jwtDecode} from "jwt-decode";
 
 export function middleware(req: NextRequest){
 
-    const encryptedToken =  req.cookies.get('access_token')?.value || ''
-    const decryptedToken =  jwtDecode(encryptedToken)
-
-    console.log("loan request: ", decryptedToken)
+    // const encryptedToken =  req.cookies.get('access_token')?.value || ''
+    // const decryptedToken =  jwtDecode(encryptedToken)
+    //
+    // console.log("loan request: ", decryptedToken)
     // function isTokenValid(token: string){
     //     const decodedToken = jwt.decode(token) as jwt.JwtPayload;
     //
