8f3eef7a05ae3383ac91e3ff6a0233952ce579a0
a-mariam <mariiam22222@gmail.com>
2024-12-12 11:50:24 +0100

    fixed logout on admin

--- src/app/(Admin)/layout.tsx
+++ src/app/(Admin)/layout.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import AdminLayout from "@/layout/admin-layout/index";
-import {getUserDetailsFromStorage} from "@/components/topBar/action";
-import {isTokenValid, isUserAdmin} from "@/utils/GlobalMethods";
-import {redirect} from "next/navigation";
-import {useToast} from "@/hooks/use-toast";
-import {clearData} from "@/utils/storage";
 
 
 type props = {
@@ -13,31 +8,7 @@ type props = {
 
 const Layout: React.FC<props> = ({ children }) => {
 
-    const cookie = getUserDetailsFromStorage('access_token')
-    const role = getUserDetailsFromStorage('user_role')
-    const {toast} = useToast()
-    if(cookie){
-       const response =  isTokenValid(cookie)
-        if (!response){
-            clearData()
-            toast({
-                description: "Session expired. Please login again",
-                status: "error",
-            });
-            redirect("/auth/login")
-        }
-    }
-    if (role){
-        if(!isUserAdmin(role)){
-            toast({
-                description: "You don't have the permission to perform this action," +
-                    "please login",
-                status: "error",
-            });
-            redirect("/auth/login")
-        }
 
-    }
 
 
 
