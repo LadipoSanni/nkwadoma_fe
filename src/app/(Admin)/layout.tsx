45949cc8e909a664321632079c19459be5475a35
a-mariam <mariiam22222@gmail.com>
2024-12-12 11:06:36 +0100

    worked on route restriction on the admin

--- src/app/(Admin)/layout.tsx
+++ src/app/(Admin)/layout.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import AdminLayout from "@/layout/admin-layout/index";
+import {getUserDetailsFromStorage} from "@/components/topBar/action";
+import {isTokenValid, isUserAdmin} from "@/utils/GlobalMethods";
+import {redirect} from "next/navigation";
+import {useToast} from "@/hooks/use-toast";
 
 
 type props = {
@@ -7,6 +11,34 @@ type props = {
 }
 
 const Layout: React.FC<props> = ({ children }) => {
+
+    const cookie = getUserDetailsFromStorage('access_token')
+    const role = getUserDetailsFromStorage('user_role')
+    const {toast} = useToast()
+    if(cookie){
+       const response =  isTokenValid(cookie)
+        if (response){
+            toast({
+                description: "Session expired. Please login again",
+                status: "error",
+            });
+            redirect("/auth/login")
+        }
+    }
+    if (role){
+        if(!isUserAdmin(role)){
+            toast({
+                description: "You don't have the permission to perform action," +
+                    "please login",
+                status: "error",
+            });
+            redirect("/auth/login")
+        }
+
+    }
+
+
+
     return (
         <AdminLayout>
             {children}
