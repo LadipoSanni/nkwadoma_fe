105826fad0bd0a54266e4023ce35054c21412be3
Moyinoluwa <olawamidemoyinoluwamary@gmail.com>
2024-12-16 11:41:55 +0100

    Loan product details page

---
+++ src/app/(Admin)/loan-product/loan-product-details/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {LoanProductDetails} from "@/features/portfolio-manager/loan-product/details/Index";
+
+const Page = () => {
+    return (
+        <>
+            <LoanProductDetails />
+        </>
+    );
+};
+
+export default Page;
