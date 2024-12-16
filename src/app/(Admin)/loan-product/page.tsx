8288d7b659a8dcb0361e79224183924fd5e6c165
Moyinoluwa <olawamidemoyinoluwamary@gmail.com>
2024-12-16 11:42:41 +0100

    Loan product details view

--- src/app/(Admin)/loan-product/page.tsx
+++ src/app/(Admin)/loan-product/page.tsx
@@ -1,4 +1,4 @@
-import LoanProductPage from '@/features/portfolio-manager/loan-product'
+import LoanProductPage from '@/features/portfolio-manager/loan-product/loan-product-view'
 import React from 'react'
 
 function page() {
