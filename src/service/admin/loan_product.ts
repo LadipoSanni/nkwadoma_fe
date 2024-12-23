562af43316c79bd91be2c6b80783d6bbae0e266c
Moyinoluwa <olawamidemoyinoluwamary@gmail.com>
2024-12-23 12:51:51 +0100

    param was renamed to params

--- src/service/admin/loan_product.ts
+++ src/service/admin/loan_product.ts
@@ -13,7 +13,7 @@ export const loanProductApi = createApi({
             }) => ({
                 url: `/loan/loan-product/all`,
                 method: "GET",
-                param: param,
+                params: param,
             }),
             providesTags: [`loanProduct`]
         }),
