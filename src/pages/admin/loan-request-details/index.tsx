5f2763998a05a25550067dcc6710840331f8b8e1
a-mariam <mariiam22222@gmail.com>
2024-12-25 16:30:49 +0100

    loan request

--- src/pages/admin/loan-request-details/index.tsx
+++ src/pages/admin/loan-request-details/index.tsx
@@ -12,34 +12,42 @@ import TabConnector from "@/reuseable/details/tab-connector";
 import styles from "./index.module.css"
 import {useViewLoanRequestDetailsQuery} from "@/service/admin/loan/loan-request-api";
 import dynamic from 'next/dynamic'
+import {Breakdown} from "@/reuseable/details/breakdown";
+import {NumericFormat} from "react-number-format";
+import dayjs from "dayjs";
+import {Button} from "@/components/ui/button";
+import {ChevronDownIcon, ChevronUpIcon} from "@radix-ui/react-icons";
+
 
 const LoanDetailsContent = dynamic(
     () => Promise.resolve(LoanDetails),
-    { ssr: false }
+    {ssr: false}
 )
 
-function LoanDetails () {
+function LoanDetails() {
     const router = useRouter()
-    const [currentTab] = useState(0)
     const searchParams = useSearchParams()
+    // const [breakdown] = useState<[]>([]);
+    const [currentTab, setCurrentTab] = useState(0);
+    const [arrowDown, setArrowDown] = useState(false);
 
 
-    const getId  = () => {
-        if (searchParams){
+
+    const getId = () => {
+        if (searchParams) {
             const pathVariable = searchParams.get("id")
-            if (pathVariable){
+            if (pathVariable) {
                 return pathVariable
-            }else {
+            } else {
                 return ""
             }
-        }else {
+        } else {
             return ""
         }
 
     }
-    const id : string =  getId()
+    const id: string = getId()
     const {data} = useViewLoanRequestDetailsQuery(id)
-    console.log("datssa: ", data)
 
     const backToLoanRequest = () => {
         router.push("/loan/loan-request")
@@ -49,6 +57,153 @@ function LoanDetails () {
         "Additional details",
         "Loan details"
     ]
+
+    const breakDown = [
+        {itemName: 'tuition', itemAmount: '$2000'},
+        {itemName: 'skincare', itemAmount: '$2000'},
+        {itemName: 'head', itemAmount: '$2000'},
+
+    ]
+    const toggleArrow = () => {
+        console.log('he dey enter')
+        if (arrowDown) {
+            setArrowDown(true)
+        } else {
+            setArrowDown(false)
+        }
+    };
+
+
+
+
+    const handleNext = () => {
+        if (currentTab < loanRequestDetailsTab.length - 1) {
+            setCurrentTab(currentTab + 1);
+        }
+    };
+
+    const handleBack = () => {
+        if (currentTab > 0) {
+            setCurrentTab(currentTab - 1);
+        }
+    };
+
+    const loanDetil = [
+        {
+            label: 'Tuition', value:
+                <NumericFormat
+                    id={'loanTuitionAmount'}
+                    name={'loanTuitionAmount'}
+                    type="text"
+                    thousandSeparator=","
+                    decimalScale={2}
+                    fixedDecimalScale={true}
+                    // value={'200000'}
+                    prefix={'₦'}
+
+                    className='bg-grey105 flex md:place-items-end '
+
+                    value={data?.data?.body?.data?.tuitionAmount}
+                    // placeholder={${detail.itemName}}
+                    // className="w-full p-3 h-[3.2rem] border rounded focus:outline-none"
+
+                />
+        },
+        {
+            label: 'Start date', value:
+            // dayjs(data?.data?.body?.data?.createdDate?.toString()).format('MMMM D, YYYY')
+                dayjs(data?.data?.body?.data?.createdDate?.toString()).format('MMMM D, YYYY')
+
+        },
+        {
+            label: 'Loan amount requested', value: <NumericFormat
+                id={'loanAmountRequested'}
+                name={'loanAmountRequested'}
+                type="text"
+                thousandSeparator=","
+                decimalScale={2}
+                fixedDecimalScale={true}
+                // value={'200000'}
+                className='bg-grey105 flex md:place-items-end '
+                prefix={'₦'}
+                value={data?.data?.body?.data?.loanAmountRequested}
+                // placeholder={${detail.itemName}}
+                // className="w-full p-3 h-[3.2rem] border rounded focus:outline-none"
+
+            />
+        },
+        {
+            label: 'Deposit', value: <NumericFormat
+                id={'depositOnLoanRequestDetails'}
+                name={'depositOnLoanRequestDetails'}
+                type="text"
+                thousandSeparator=","
+                decimalScale={2}
+                fixedDecimalScale={true}
+                // value={'200000'}
+                prefix={'₦'}
+                value={data?.data?.body?.data?.initialDeposit}
+                // placeholder={${detail.itemName}}
+                // className="w-full p-3 h-[3.2rem] border rounded focus:outline-none"
+                className='bg-grey105 flex md:place-items-end'
+
+            />
+        },
+        {
+            label: 'Credit score', value:
+            // data?.data?.body?.data
+                <div className={`flex gap-2 `}>
+                    <span>Good</span>
+                    <div
+                        className={` w-fit md:w-fit md:h-fit h-fit md:py-0 py-0 md:px-1 px-1 md:rounded-md rounded-md border md:border border-green650 md:border-green650`}>
+                        <span
+                            className={`md:bg-green150 bg-green150 md:px-0.5 px-0.5 md:rounded-md rounded-md md:py-0.5 py-0.5 md:text-xs text-xs text-green750 md:text-green750 `}>234</span>
+                    </div>
+                </div>
+        },
+
+    ]
+
+    const basic = [
+        {label: 'Gender', value: data?.data?.body?.data?.userIdentity?.gender},
+        {label: 'Email address', value: data?.data?.body?.data?.userIdentity?.email},
+        {label: 'Phone number', value: data?.data?.body?.data?.userIdentity?.phoneNumer},
+        {label: 'Date of birth', value: data?.data?.body?.data?.userIdentity?.dateOfBirth},
+        {label: 'Marital status', value: data?.data?.body?.data?.userIdentity?.maritalStatus},
+        {label: 'Nationality', value: data?.data?.body?.data?.userIdentity?.nationality},
+        {label: 'State of origin ', value: data?.data?.body?.data?.userIdentity?.stateOfOrigin},
+        {label: 'State of residence', value: data?.data?.body?.data?.userIdentity?.stateOfResidence},
+    ]
+
+    const additional = [
+        {label: 'Alternate email address', value: data?.data?.body?.data?.alternateEmail},
+        {label: 'Alternate phone number', value: data?.data?.body?.data?.alternatePhoneNumber},
+        {label: 'Alternate residential address', value: data?.data?.body?.data?.alternateContactAddress},
+        {
+            label: 'Next of kin name',
+            value: data?.data?.body?.data?.nextOfKin?.firstName + " " + data?.data?.body?.data?.nextOfKin?.lastName
+        },
+        {label: 'Next of kin email address', value: data?.data?.body?.data?.nextOfKin?.email},
+        {label: 'Next of kin phone number', value: data?.data?.body?.data?.nextOfKin?.phoneNumber},
+        {label: 'Next of kin relationship ', value: data?.data?.body?.data?.nextOfKin?.nextOfKinRelationship},
+
+    ]
+
+
+    const getCurrentDataList = () => {
+        switch (currentTab) {
+            case 0:
+                return basic;
+            case 1:
+                return additional;
+            case 2:
+                return loanDetil;
+
+            default:
+                return [];
+        }
+    };
+
 
     return (
         <div
@@ -85,7 +240,8 @@ function LoanDetails () {
                               className={`${inter.className} text-sm text-black400`}>Product Design</span>
                             <span
                                 className={`${inter.className} text-sm text-black400 mt-auto mb-auto md:mt-auto md:mb-auto `}>.</span>
-                            <span id={'loaneeCohortOnLoanRequestDetails'} data-testid={'loaneeCohortOnLoanRequestDetails'}
+                            <span id={'loaneeCohortOnLoanRequestDetails'}
+                                  data-testid={'loaneeCohortOnLoanRequestDetails'}
                                   className={`${inter.className} text-sm text-black400`}>Luminary</span>
                         </div>
                         {/*<Button*/}
@@ -105,11 +261,63 @@ function LoanDetails () {
                     >
                         <TabConnector tabNames={loanRequestDetailsTab} currentTab={currentTab}/>
                     </div>
+                    <div className={`px - 2`}>
+                        <div className={`bg-grey105 `}>
+                            {getCurrentDataList().map((item, index) => (
+                                <li key={"key" + index} className={'p-5  grid gap-9 rounded-md'}>
+                                    <div
+                                        className={'md:flex md:justify-between md:items-center md:gap-0 grid gap-3 '}>
+                                        <div
+                                            className={'text-black300 text-[14px] leading-[150%] font-normal'}>{item.label}</div>
+                                        <div
+                                            className={'text-black500 text-[14px] leading-[150%] font-normal'}> {item.value}</div>
+                                    </div>
+                                </li>
+                            ))
+                            }
+                            <div className={` px-3 pb-2`}>
+                                {currentTab == 2 &&
+                                    <Breakdown breakDown={breakDown}
+                                    />
+                                }
+                            </div>
+                        </div>
+                    </div>
+                <div  className="md:flex grid md:justify-end gap-5 mt-4">
+                    {currentTab !== 0 && (
+                        <Button
+                            className={'w-full md:w-fit md:px-6 md:py-4 h-fit py-4 text-meedlBlue border border-meedlBlue bg-meedlWhite hover:bg-meedlWhite'}
+                            onClick={handleBack} disabled={currentTab === 0}>Back</Button>
+                    )}
 
+                    <Button
+                        className={'w-full md:w-fit md:px-8 flex gap-2 h-fit py-4 bg-meedlBlue hover:bg-meedlBlue'}
+                        onClick={handleNext}
+                        disabled={currentTab === loanRequestDetailsTab.length - 1}>
+                        {currentTab === 2 ? 'Make decision ' : 'Continue'}
+                        {currentTab == 2 &&
+                            <div onClick={toggleArrow}>
+                                {arrowDown ?
+                                    <ChevronUpIcon
+                                        className={'h-5 cursor-pointer  w-4 stroke-2 text-white'}
+                                        onClick={toggleArrow}/>
+                                 :
+                                    <ChevronDownIcon
+                                        className={'h-5  cursor-pointer w-5 stroke-2 text-white'}
+                                        onClick={toggleArrow}/>
+
+                                }
+                            </div>
+                        }
+                    </Button>
                 </div>
+
+
             </div>
         </div>
-    );
+</div>
+)
+    ;
 }
 
 export default LoanDetailsContent;
