4b60163758379c3e55a1e4604d85c3f7e4887b7d
a-mariam <mariiam22222@gmail.com>
2025-01-02 18:07:37 +0100

    working on organization modal

--- src/components/loan/change-organization/index.tsx
+++ src/components/loan/change-organization/index.tsx
@@ -11,33 +11,37 @@ import React, {useState} from "react";
 //
 // } from "@radix-ui/react-dialog";
 import * as Dialog from "@radix-ui/react-dialog";
-import { Cross2Icon } from "@radix-ui/react-icons";
+import {Cross2Icon} from "@radix-ui/react-icons";
 // import "./styles.css";
 import {inter, cabinetGroteskRegular} from "@/app/fonts";
+import {MdSearch} from "react-icons/md";
+import {Input} from "@/components/ui/input";
 // interface props {
-    // open: boolean,
-    // onOpenChange: (state: boolean) => void
+// open: boolean,
+// onOpenChange: (state: boolean) => void
 // }
 
 const ChangeInstitutionModal = () => {
 
     const [open, setOpen] = React.useState(false)
-    if (open ){
+    if (open) {
         console.log('ajhbs')
     }
 
-    return(
+    return (
         <Dialog.Root>
             <Dialog.Trigger asChild>
-                         <button  id="changeOrganizationButton" data-testid={'changeOrganizationButton'}
-                             className={` ${inter.className} text-blue300 pt-0.5 underline w-fit h-fit md:text-xs md:font-light px-1 bg-blue500 rounded `}>Change
-                        </button>
+                <button id="changeOrganizationButton" data-testid={'changeOrganizationButton'}
+                        className={` ${inter.className} text-meedlBlue pt-0.5 underline w-fit h-fit md:font-size-0.875rem md:font-light px-1 bg-blue500 rounded `}>Change
+                </button>
             </Dialog.Trigger>
             <Dialog.Portal>
-                <Dialog.Overlay className="fixed inset-0 bg-[#344054B2] data-[state=open]:animate-overlayShow" />
-                <Dialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[450px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
-                    <Dialog.Title className={` flex justify-between bg-red-300 w-full `}>
-                        <div id={`organizationText`} data-testid={`organizationText`} className={`${cabinetGroteskRegular.className} text-2xl`}>
+                <Dialog.Overlay className="fixed inset-0 bg-[#344054B2] data-[state=open]:animate-overlayShow"/>
+                <Dialog.Content
+                    className="fixed left-1/2 top-1/2 md:h-[80vh] md:w-[30vw] h-[90vh] w-[90vw] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white py-6 px-5 md:py-6 md:px-5  ">
+                    <Dialog.Title className={` flex justify-between  w-full `}>
+                        <div id={`organizationText`} data-testid={`organizationText`}
+                             className={`${cabinetGroteskRegular.className} text-2xl`}>
                             Organization
                         </div>
                         <Dialog.Close asChild>
@@ -49,41 +53,19 @@ const ChangeInstitutionModal = () => {
                             </button>
                         </Dialog.Close>
                     </Dialog.Title>
-                    <Dialog.Description className="mb-5 mt-2.5 text-[15px] leading-normal text-mauve11">
-                        Make changes to your profile here. Click save when you're done.
+                    <Dialog.Description className="w-full md:w-full ">
+                        <div className='relative mt-7 '>
+                            <span className="absolute inset-y-0 left-0 flex items-center pr-4 pl-3">
+                               <MdSearch className="h-5 w-5 text-[#939CB0]"/>
+                            </span>
+                            <Input
+                                id='searchOrganizationOn'
+                                placeholder='Search'
+                                className='w-full md:w-full rounded h-11 md:h-11 focus-visible:ring-0 shadow-none  border-solid border border-neutral650  text-grey450 '
+                            />
+                        </div>
                     </Dialog.Description>
-                    <fieldset className="mb-[15px] flex items-center gap-5">
-                        <label
-                            className="w-[90px] text-right text-[15px] text-violet11"
-                            htmlFor="name"
-                        >
-                            Name
-                        </label>
-                        <input
-                            className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
-                            id="name"
-                            defaultValue="Pedro Duarte"
-                        />
-                    </fieldset>
-                    <fieldset className="mb-[15px] flex items-center gap-5">
-                        <label
-                            className="w-[90px] text-right text-[15px] text-violet11"
-                            htmlFor="username"
-                        >
-                            Username
-                        </label>
-                        <input
-                            className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
-                            id="username"
-                            defaultValue="@peduarte"
-                        />
-                    </fieldset>
                     <div className="mt-[25px] flex justify-end">
-                        <Dialog.Close asChild>
-                            <button className="inline-flex h-[35px] items-center justify-center rounded bg-green4 px-[15px] font-medium leading-none text-green11 hover:bg-green5 focus:shadow-[0_0_0_2px] focus:shadow-green7 focus:outline-none">
-                                Save changes
-                            </button>
-                        </Dialog.Close>
                     </div>
                 </Dialog.Content>
             </Dialog.Portal>
