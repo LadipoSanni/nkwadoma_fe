a750d30453a36a2b5c816fd6f5eda7edf92412e6
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-01-23 11:42:25 +0100

    dialog close was added

--- src/reuseable/buttons/AuthButton.tsx
+++ src/reuseable/buttons/AuthButton.tsx
@@ -1,26 +1,43 @@
 "use client"
 import React from 'react';
 import {AuthButtonPropsType} from "@/types/ButtonTypes";
-import { Button } from "@/components/ui/button"
+import {Button} from "@/components/ui/button"
 import Isloading from "@/reuseable/display/Isloading";
+import * as Dialog from "@radix-ui/react-dialog";
 
 
-
-
-
-const AuthButton = ({id, buttonText, width, handleClick, textColor, backgroundColor, disable,isLoading, height}: AuthButtonPropsType) => {
+const AuthButton = ({
+                        id,
+                        buttonText,
+                        width,
+                        handleClick,
+                        textColor,
+                        backgroundColor,
+                        disable,
+                        isLoading,
+                        height
+                    }: AuthButtonPropsType) => {
     return (
-        <Button
-            data-testid="auth-button"
-            disabled={disable}
-            id={id}
-            onClick={(event)=>{handleClick(event)}}
-            style={{height: `${height ? height : '3rem'}`,width: `${width}`, color: `${textColor}`, backgroundColor: `${disable ? "#D0D0D0": backgroundColor}`}}
-            className={`grid place-content-center rounded font-bold text-sm`}
-        >  
-        {isLoading? <Isloading /> : buttonText}  
-            
-        </Button>
+        <Dialog.Close asChild>
+            <Button
+                data-testid="auth-button"
+                disabled={disable}
+                id={id}
+                onClick={(event) => {
+                    handleClick(event)
+                }}
+                style={{
+                    height: `${height ? height : '3rem'}`,
+                    width: `${width}`,
+                    color: `${textColor}`,
+                    backgroundColor: `${disable ? "#D0D0D0" : backgroundColor}`
+                }}
+                className={`grid place-content-center rounded font-bold text-sm`}
+            >
+                {isLoading ? <Isloading/> : buttonText}
+
+            </Button>
+        </Dialog.Close>
     );
 };
 
