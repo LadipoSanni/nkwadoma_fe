fffaf014deac88346457a2e0216d28914619ee10
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-01-24 08:39:41 +0100

    Skeleton For Grid was formatted

--- src/reuseable/Skeleton-loading-state/Skeleton-for-grid.tsx
+++ src/reuseable/Skeleton-loading-state/Skeleton-for-grid.tsx
@@ -1,29 +1,28 @@
 import React from 'react'
-import { Skeleton } from '@/components/ui/skeleton'
-
+import {Skeleton} from '@/components/ui/skeleton'
 
 
 function SkeletonForGrid() {
 
     const skeletonList = [
-        { value: <Skeleton className="h-72 w-full bg-[#F9F9F9] " /> },
-        { value: <Skeleton  className="h-72 w-full bg-[#F9F9F9]  " /> },
-        { value: <Skeleton  className="h-72 w-full bg-[#F9F9F9]" /> },
-        {  value: <Skeleton className="h-72 w-full bg-[#F9F9F9]" /> },
-        { value: <Skeleton  className="h-72 w-full bg-[#F9F9F9]"/> },
-        { value: <Skeleton className="h-72 w-full bg-[#F9F9F9]" /> },
-        { value: <Skeleton className="h-72 w-full bg-[#F9F9F9]" /> },
-        { value: <Skeleton className="h-72 w-full bg-[#F9F9F9]"/> },
+        {value: <Skeleton className="h-72 w-full bg-[#F9F9F9] "/>},
+        {value: <Skeleton className="h-72 w-full bg-[#F9F9F9]  "/>},
+        {value: <Skeleton className="h-72 w-full bg-[#F9F9F9]"/>},
+        {value: <Skeleton className="h-72 w-full bg-[#F9F9F9]"/>},
+        {value: <Skeleton className="h-72 w-full bg-[#F9F9F9]"/>},
+        {value: <Skeleton className="h-72 w-full bg-[#F9F9F9]"/>},
+        {value: <Skeleton className="h-72 w-full bg-[#F9F9F9]"/>},
+        {value: <Skeleton className="h-72 w-full bg-[#F9F9F9]"/>},
     ]
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             {skeletonList.map((item, index) => (
-                <div key={index} >
+                <div key={index}>
                     {item.value}
                 </div>
             ))}
         </div>
-  )
+    )
 }
 
 export default SkeletonForGrid
