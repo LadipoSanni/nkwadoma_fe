498990809a336dbeb42dd92a50ec7bd6e0011752
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-01-24 08:40:27 +0100

    Skeleton For table was formatted

--- src/reuseable/Skeleton-loading-state/Skeleton-for-table.tsx
+++ src/reuseable/Skeleton-loading-state/Skeleton-for-table.tsx
@@ -1,30 +1,30 @@
 import React from 'react'
-import { Skeleton } from '@/components/ui/skeleton'
+import {Skeleton} from '@/components/ui/skeleton'
 
 function SkeletonForTable() {
-    
-  return (
-    <div className='h-52 w-full border-solid border-black'>
-       <Skeleton className="h-16 w-full bg-[#F6F6F6]" /> 
-       <Skeleton className="h-16 w-full  bg-[#F6F6F6] mt-2" />
-       <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2" /> 
-       <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2" /> 
-       <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2" /> 
-       <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2" /> 
-       <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2" />
-       <div className='flex justify-between  items-center'>
-          <Skeleton className="h-16 w-32 bg-[#F6F6F6] mt-2 " />
-          <div className='md:flex items-center justify-center gap-3 hidden '>
-          <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2" />
-          <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2" />
-          <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2" />
-          <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2" />
-          <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2" />
-          </div>
-          <Skeleton className="h-16 w-32 bg-[#F6F6F6] mt-2" /> 
-       </div>
-    </div>
-  )
+
+    return (
+        <div className='h-52 w-full border-solid border-black'>
+            <Skeleton className="h-16 w-full bg-[#F6F6F6]"/>
+            <Skeleton className="h-16 w-full  bg-[#F6F6F6] mt-2"/>
+            <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2"/>
+            <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2"/>
+            <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2"/>
+            <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2"/>
+            <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2"/>
+            <div className='flex justify-between  items-center'>
+                <Skeleton className="h-16 w-32 bg-[#F6F6F6] mt-2 "/>
+                <div className='md:flex items-center justify-center gap-3 hidden '>
+                    <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2"/>
+                    <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2"/>
+                    <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2"/>
+                    <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2"/>
+                    <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2"/>
+                </div>
+                <Skeleton className="h-16 w-32 bg-[#F6F6F6] mt-2"/>
+            </div>
+        </div>
+    )
 }
 
 export default SkeletonForTable
