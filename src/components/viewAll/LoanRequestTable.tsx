7bda5abf26ef3271cd465e0f0cbbb49d1ec6719f
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-01-26 17:51:09 +0100

    Updated disbursal loan response

--- src/components/viewAll/LoanRequestTable.tsx
+++ src/components/viewAll/LoanRequestTable.tsx
@@ -12,73 +12,98 @@ import {capitalizeFirstLetters} from "@/utils/GlobalMethods";
 import SkeletonForTable from "@/reuseable/Skeleton-loading-state/Skeleton-for-table";
 
 
-
 interface TableRowData {
     [key: string]: string | number | null | React.ReactNode;
 }
 
 const Index = () => {
     const router = useRouter();
-    const request ={
+    const request = {
         pageSize: 10,
         pageNumber: 0
     }
-    const { data, isLoading} = useViewAllLoanRequestQuery(request)
+    const {data, isLoading} = useViewAllLoanRequestQuery(request)
 
     const loanRequestHeader = [
-        { title: 'Loanee', sortable: true, id: 'firstName', selector: (row: TableRowData) =><div className='flex gap-2 '>{capitalizeFirstLetters(row.firstName?.toString())} <div className={``}></div>{row.lastName}</div>  },
-        { title: 'Program', sortable: true, id: 'program', selector: (row: TableRowData) =>row.programName },
-        { title: 'Cohort', sortable: true, id: 'cohort', selector: (row: TableRowData) => row.cohortName },
-        { title: 'Start date', sortable: true, id: 'startDate', selector: (row: TableRowData) => <div>{dayjs(row.cohortStartDate?.toString()).format('MMM D, YYYY')}</div> },
-        { title: 'Request date', sortable: true, id: 'requestDate', selector: (row: TableRowData) =><div>{dayjs(row.requestDate?.toString()).format('MMM D, YYYY')}</div> },
-        { title: 'Initial deposit', sortable: true, id: 'initialDeposit', selector: (row: TableRowData) => <div className=''>{formatAmount(row.initialDeposit)}</div>},
-        { title: 'Amount Requested', sortable: true, id: 'amountRequested', selector: (row: TableRowData) => <div className=''>{formatAmount(row.loanAmountRequested)}</div>}
+        {
+            title: 'Loanee',
+            sortable: true,
+            id: 'firstName',
+            selector: (row: TableRowData) => <div
+                className='flex gap-2 '>{capitalizeFirstLetters(row.firstName?.toString())} {row.lastName}</div>
+        },
+        {title: 'Program', sortable: true, id: 'program', selector: (row: TableRowData) => row.programName},
+        {
+            title: 'Cohort',
+            sortable: true,
+            id: 'cohort',
+            selector: (row: TableRowData) => row.cohortName
+        },
+        {
+            title: 'Start date',
+            sortable: true,
+            id: 'startDate',
+            selector: (row: TableRowData) => <div>{dayjs(row.cohortStartDate?.toString()).format('MMM D, YYYY')}</div>
+        },
+        {
+            title: 'Request date',
+            sortable: true,
+            id: 'requestDate',
+            selector: (row: TableRowData) => <div>{dayjs(row.requestDate?.toString()).format('MMM D, YYYY')}</div>
+        },
+        {
+            title: 'Initial deposit',
+            sortable: true,
+            id: 'initialDeposit',
+            selector: (row: TableRowData) => <div className=''>{formatAmount(row.initialDeposit)}</div>
+        },
+        {
+            title: 'Amount Requested',
+            sortable: true,
+            id: 'amountRequested',
+            selector: (row: TableRowData) => <div className=''>{formatAmount(row.loanAmountRequested)}</div>
+        }
     ];
-
-
 
     const handleRowClick = (ID: string | object | React.ReactNode) => {
         router.push(`/loan-request-details?id=${ID}`);
     };
 
-
     return (
         <div data-testid={'mainDivContainer'} id={`mainDivContainer`}
-             className={`grid md:px-3 md:overflow-hidden  md:pb-3 place-items-center w-full md:w-full md:h-full md:grid md:place-items-center  h-full `}
+             className={`grid md:px-3 md:overflow-hidden md:pb-3 place-items-center w-full md:w-full md:h-full md:grid md:place-items-center h-full `}
         >
             {isLoading ? (
-                    <div className={`w-full h-fit md:w-full md:h-full`}>
-                        <SkeletonForTable />
-                    </div>
-                ):
-                data?.data?.body?.length > 0 ?
-                    <div className={`md:w-full  w-full h-full md:h-full `}>
-                        <Tables
-                            tableData={data?.data?.body}
-                            isLoading={isLoading}
-                            handleRowClick={handleRowClick}
-                            tableHeader={loanRequestHeader}
-                            tableHeight={52}
-                            sx='cursor-pointer'
-                            staticColunm='firstName'
-                            staticHeader='Loanee'
-                            showKirkBabel={false}
-                            icon={MdOutlinePeople}
-                            sideBarTabName='Cohort'
-                            optionalFilterName='graduate'
-                        />
-                    </div>
-                    :
-                    <LoanEmptyState
-                        id={'LoanRequestEmptyState'}
-                        icon={<Icon icon="material-symbols:money-bag-outline"
-                                    height={"2rem"}
-                                    width={"2em"}
-                                    color={'#142854'}
-                        ></Icon >} iconBg={'#D9EAFF'} title={'Loan request will show here'} description={`There are no loan requests available yet`} />
-
-
-            }
+                <div className={`w-full h-fit md:w-full md:h-full`}>
+                    <SkeletonForTable/>
+                </div>
+            ) : data?.data?.body?.length > 0 ? (
+                <div className={`md:w-full w-full h-full md:h-full `}>
+                    <Tables
+                        tableData={data?.data?.body}
+                        isLoading={isLoading}
+                        handleRowClick={handleRowClick}
+                        tableHeader={loanRequestHeader}
+                        tableHeight={52}
+                        sx='cursor-pointer'
+                        staticColunm='firstName'
+                        staticHeader='Loanee'
+                        showKirkBabel={false}
+                        icon={MdOutlinePeople}
+                        sideBarTabName='Cohort'
+                        optionalFilterName='graduate'
+                    />
+                </div>
+            ) : (
+                <LoanEmptyState
+                    id={'LoanRequestEmptyState'}
+                    icon={<Icon icon="material-symbols:money-bag-outline" height={"2rem"} width={"2em"}
+                                color={'#142854'}/>}
+                    iconBg={'#D9EAFF'}
+                    title={'Loan request will show here'}
+                    description={`There are no loan requests available yet`}
+                />
+            )}
         </div>
     );
 }
