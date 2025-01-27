5cb2942150dcce686f0de5ac0ebcd0071ce4250c
ikjames5454 <ikennajames03@gmail.com>
2025-01-27 18:07:37 +0100

    Bug fixes

--- src/components/program/create-program/index.tsx
+++ src/components/program/create-program/index.tsx
@@ -57,7 +57,7 @@ function CreateProgram({setIsOpen}:Props) {
       .max(200, "Program name cannot be more than 200 characters.")
       .test(
         "valid-name",
-        "Program name cannot be only numbers or special character hyphen.",
+        "Program name can include letters, numbers, hyphens, and underscores, but cannot be solely numbers or special characters.",
         (value = "") => {
         const regex = /^[a-zA-Z0-9\s-_]*$/;
         const onlyNumbersOrSpecials = /^[^a-zA-Z]*$/;
