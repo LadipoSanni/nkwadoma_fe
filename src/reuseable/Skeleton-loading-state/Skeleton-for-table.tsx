11006b11a4481e0977a1d58863860c73403c7412
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-02-06 18:31:41 +0100

    isLoading and skeleton on tables

--- src/reuseable/Skeleton-loading-state/Skeleton-for-table.tsx
+++ src/reuseable/Skeleton-loading-state/Skeleton-for-table.tsx
@@ -4,24 +4,24 @@ import {Skeleton} from '@/components/ui/skeleton'
 function SkeletonForTable() {
 
     return (
-        <div className='h-52 w-full border-solid border-black'>
-            <Skeleton className="h-16 w-full bg-[#F6F6F6]"/>
-            <Skeleton className="h-16 w-full  bg-[#F6F6F6] mt-2"/>
-            <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2"/>
-            <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2"/>
-            <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2"/>
-            <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2"/>
-            <Skeleton className="h-16 w-full bg-[#F6F6F6] mt-2"/>
+        <div className='h-96 w-full border-solid border-black'>
+            <Skeleton className="h-12 w-full bg-[#F6F6F6]"/>
+            <Skeleton className="h-12 w-full  bg-[#F6F6F6] mt-2"/>
+            <Skeleton className="h-12 w-full bg-[#F6F6F6] mt-2"/>
+            <Skeleton className="h-12 w-full bg-[#F6F6F6] mt-2"/>
+            <Skeleton className="h-12 w-full bg-[#F6F6F6] mt-2"/>
+            <Skeleton className="h-12 w-full bg-[#F6F6F6] mt-2"/>
+            <Skeleton className="h-12 w-full bg-[#F6F6F6] mt-2"/>
             <div className='flex justify-between  items-center'>
-                <Skeleton className="h-16 w-32 bg-[#F6F6F6] mt-2 "/>
+                <Skeleton className="h-12 w-32 bg-[#F6F6F6] mt-2 "/>
                 <div className='md:flex items-center justify-center gap-3 hidden '>
-                    <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2"/>
-                    <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2"/>
-                    <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2"/>
-                    <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2"/>
-                    <Skeleton className="h-10 w-10 rounded-full bg-[#F6F6F6] mt-2"/>
+                    <Skeleton className="h-8 w-8 rounded-full bg-[#F6F6F6] mt-2"/>
+                    <Skeleton className="h-8 w-8 rounded-full bg-[#F6F6F6] mt-2"/>
+                    <Skeleton className="h-8 w-8 rounded-full bg-[#F6F6F6] mt-2"/>
+                    <Skeleton className="h-8 w-8 rounded-full bg-[#F6F6F6] mt-2"/>
+                    <Skeleton className="h-8 w-8 rounded-full bg-[#F6F6F6] mt-2"/>
                 </div>
-                <Skeleton className="h-16 w-32 bg-[#F6F6F6] mt-2"/>
+                <Skeleton className="h-12 w-32 bg-[#F6F6F6] mt-2"/>
             </div>
         </div>
     )
