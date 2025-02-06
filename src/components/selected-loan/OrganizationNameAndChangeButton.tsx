baf654379ab1d2904cd7ed658d733fc81a48163c
a-mariam <mariiam22222@gmail.com>
2025-02-06 19:57:46 +0100

    worked on change organization

--- src/components/selected-loan/OrganizationNameAndChangeButton.tsx
+++ src/components/selected-loan/OrganizationNameAndChangeButton.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import OrganizationImage from "@/reuseable/profile/Organization-image";
-import {cabinetGroteskRegular} from "@/app/fonts";
+import {cabinetGroteskRegular, inter} from "@/app/fonts";
 import ChangeInstitutionModal from "@/components/loan/change-organization/index";
 import {useAppSelector} from "@/redux/store";
 import InitialsAvatar from '../../reuseable/avater/InitialAvater';
+import {Cross2Icon} from "@radix-ui/react-icons";
+import CreateInvestmentVehicle from "@/components/portfolio-manager/fund/Create-investment-vehicle";
+import TableModal from "@/reuseable/modals/TableModal";
+import {Button} from "@/components/ui/button";
 
 const OrganizationNameAndChangeButton = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const getInitials = (name: string): string => {
         const nameArray = name.split(' ');
         let initials = nameArray.map(word => word[0]).join('');
@@ -37,7 +42,25 @@ const OrganizationNameAndChangeButton = () => {
                  className={`${cabinetGroteskRegular.className} text-2xl`}>
                 {name || "All organizations"}
             </div>
-            <ChangeInstitutionModal />
+            <Button id="changeOrganizationButton" data-testid={'changeOrganizationButton'} size={"lg"}
+                    variant={"secondary"}
+                    className={` ${inter.className} text-meedlBlue pt-0.5 underline w-fit h-fit md:font-size-0.875rem md:font-light px-1 bg-blue500 rounded `}
+                    onClick={()=> setIsModalOpen(true)}
+            >Change
+            </Button>
+            {
+                <TableModal
+                    isOpen={isModalOpen}
+                    closeModal={()=> setIsModalOpen(false)}
+                    className='pb-1'
+                    headerTitle='Organization'
+                    closeOnOverlayClick={true}
+                    icon={Cross2Icon}
+                >
+                    <ChangeInstitutionModal />
+                </TableModal>
+            }
+            {/*<ChangeInstitutionModal />*/}
         </div>
     );
 };
