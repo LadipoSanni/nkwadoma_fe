65a67233a53507f630a1858e6ef41af621b0e3e6
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-05 17:12:15 +0100

    update draft button

---
+++ src/reuseable/buttons/UpdateDraftButton.tsx
@@ -0,0 +1,41 @@
+"use client"
+import React from 'react';
+import {AuthButtonPropsType} from "@/types/ButtonTypes";
+import {Button} from "@/components/ui/button"
+import Isloading from "@/reuseable/display/Isloading";
+
+
+const UpdateDraftButton = ({
+                          id,
+                          buttonText,
+                          width,
+                          handleClick,
+                          textColor,
+                          backgroundColor,
+                          disable,
+                          isLoading,
+                          height
+                      }: AuthButtonPropsType) => {
+    return (
+            <Button
+                data-testid="auth-button"
+                disabled={disable}
+                id={id}
+                onClick={(event) => {
+                    handleClick(event)
+                }}
+                style={{
+                    height: `${height ? height : '3rem'}`,
+                    width: `${width}`,
+                    color: `${textColor}`,
+                    backgroundColor: `${disable ? "#D0D0D0" : backgroundColor}`
+                }}
+                className={`grid place-content-center rounded font-bold text-sm`}
+            >
+                {isLoading ? <Isloading/> : buttonText}
+
+            </Button>
+    );
+};
+
+export default UpdateDraftButton;
