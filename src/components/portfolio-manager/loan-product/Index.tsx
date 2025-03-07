a674ac8b69ec24d91a636cbfa7ca02ae81824015
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-07 13:29:40 +0100

    fund product end point was changed from investment vehicle (draft and published) all to all published investment vehicle.

--- src/components/portfolio-manager/loan-product/Index.tsx
+++ src/components/portfolio-manager/loan-product/Index.tsx
@@ -9,7 +9,9 @@ import CurrencySelectInput from "@/reuseable/Input/CurrencySelectInput";
 import {useCreateLoanProductMutation} from "@/service/admin/loan_product";
 import Isloading from "@/reuseable/display/Isloading";
 import ToastPopUp from "@/reuseable/notification/ToastPopUp";
-import {useGetAllInvestmentmentVehicleQuery} from "@/service/admin/fund_query";
+import {
+    useGetPublishedInvestmentVehicleByNameQuery
+} from "@/service/admin/fund_query";
 import CustomInputField from "@/reuseable/Input/CustomNumberFormat"
 import 'react-quill-new/dist/quill.snow.css'
 import FormikCustomQuillField from "@/reuseable/textArea/FormikCustomQuillField";
@@ -40,12 +42,8 @@ const CreateLoanProduct = ({setIsOpen}: CreateLoanProductProps) => {
     // const [loanProductTermsAndConditionError, setLoanProductTermsAndConditionError] = useState('');
     // const [step, setStep] = useState(1);
     const [createLoanProduct, {isLoading}] = useCreateLoanProductMutation();
-    const dataElement = {
-        pageNumber: 0,
-        pageSize: 200
-    }
-    const {data: investmentVehicleData} = useGetAllInvestmentmentVehicleQuery(dataElement);
 
+    const {data: investmentVehicleData} = useGetPublishedInvestmentVehicleByNameQuery({status: "PUBLISHED"});
     useEffect(() => {
         if (investmentVehicleData) {
             const obj: { [key: string]: string } = {};
