babf6f4714b63af14bf38f5cb4e4c39c793a7423
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-08 02:52:50 +0100

    Investment Vehicles By Type And Status was added to query for integration

--- src/service/admin/fund_query.ts
+++ src/service/admin/fund_query.ts
@@ -1,59 +1,105 @@
 import {createApi} from '@reduxjs/toolkit/query/react'
 import {customFetchBaseQuery} from "@/service/customFetchBaseQuery"
 
+interface InvestmentVehicle {
+    id: string;
+    name: string;
+    investmentVehicleType: 'COMMERCIAL' | 'ENDOWMENT';
+    mandate: string;
+    sponsors: string;
+    tenure: number;
+    size: number;
+    rate: number;
+    trustee: string;
+    custodian: string;
+    bankPartner: string;
+    fundManager: string;
+    minimumInvestmentAmount: number;
+    status: string;
+    startDate: string;
+    totalAmountInInvestmentVehicle:number;
+}
+interface InvestmentVehicleResponse {
+    data: InvestmentVehicle[];
+    total: number;
+    pageSize: number;
+    pageNumber: number;
+}
 export const fundApi = createApi({
     reducerPath: 'fundApi',
     baseQuery: customFetchBaseQuery,
     tagTypes: ['vehicle'],
     endpoints: (builder) => ({
-      getAllInvestmentmentVehicle: builder.query({
-        query: (param: {
-            pageSize?: number;
-            pageNumber?: number;
-        }) => ({
-          url: '/view-all-investment-vehicle',
-          method: "GET",
-          params: param,
+        getAllInvestmentmentVehicle: builder.query({
+            query: (param: {
+                pageSize?: number;
+                pageNumber?: number;
+            }) => ({
+                url: '/view-all-investment-vehicle',
+                method: "GET",
+                params: param,
+            }),
+            providesTags: ['vehicle'],
         }),
-        providesTags: ['vehicle'],
-      }),
-      getInvestmentVehicleDetail: builder.query({
-        query:({id}) => ({
-          url: `/investment-vehicle-details/${id}`,
-          method: "GET",
+        getInvestmentVehicleDetail: builder.query({
+            query: ({id}) => ({
+                url: `/investment-vehicle-details/${id}`,
+                method: "GET",
+            }),
+            // providesTags: (result, error, arg) => [{ type: 'vehicle', id: arg.id }],
+            providesTags: ['vehicle'],
         }),
-        // providesTags: (result, error, arg) => [{ type: 'vehicle', id: arg.id }],
-        providesTags: ['vehicle'],
-      }),
-       createInvestmentVehicle: builder.mutation({
-        query: (data:{
-        name:  string,
-        investmentVehicleType: string | undefined,
-        mandate: string,
-       tenure: string,
-       size: string,
-       rate: string,
-       trustee: string,
-       custodian: string,
-       bankPartner: string,
-       fundManager: string,
-       sponsors: string,
-       minimumInvestmentAmount: string
+        createInvestmentVehicle: builder.mutation({
+            query: (data: {
+                name: string,
+                investmentVehicleType: string | undefined,
+                mandate: string,
+                tenure: string,
+                size: string,
+                rate: string,
+                trustee: string,
+                custodian: string,
+                bankPartner: string,
+                fundManager: string,
+                sponsors: string,
+                minimumInvestmentAmount: string
 
-        }) => ({
+            }) => ({
                 url: `/investment-vehicle`,
                 method: 'POST',
                 body: data
+            }),
+            invalidatesTags: ['vehicle']
         }),
-        invalidatesTags: ['vehicle']
-       }),
-       searchInvestmentVehicleByName: builder.query({
-        query: (investmentVehicleName) => ({
-            url: `/investmentvehicle/search/${investmentVehicleName}`,
-            method: 'GET'
+        searchInvestmentVehicleByName: builder.query({
+            query: (investmentVehicleName) => ({
+                url: `/investmentvehicle/search/${investmentVehicleName}`,
+                method: 'GET'
+            }),
+        }),
+        getInvestmentVehiclesByTypeAndStatus: builder.query<
+            InvestmentVehicleResponse,
+            { pageSize: number; pageNumber: number; type: string; status: string }
+        >({
+            query: ({ pageSize, pageNumber, type, status }) => ({
+                url: '/view-all-investment-vehicle-by-type-and-status',
+                method: 'GET',
+                params: {
+                    pageSize,
+                    pageNumber,
+                    type,
+                    status,
+                },
+            }),
+            providesTags: ['vehicle'],
         }),
     }),
-    })
 })
 
-export const { useGetAllInvestmentmentVehicleQuery,useGetInvestmentVehicleDetailQuery,useCreateInvestmentVehicleMutation,useSearchInvestmentVehicleByNameQuery} = fundApi;
+export const {
+    useGetAllInvestmentmentVehicleQuery,
+    useGetInvestmentVehicleDetailQuery,
+    useCreateInvestmentVehicleMutation,
+    useSearchInvestmentVehicleByNameQuery,
+    useGetInvestmentVehiclesByTypeAndStatusQuery,
+} = fundApi;
