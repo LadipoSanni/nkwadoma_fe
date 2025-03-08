368acb02d63f8bb2a73d63ac7acc0646fb277708
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-08 02:54:52 +0100

    view all draft integration slice

--- src/redux/slice/vehicle/vehicle.ts
+++ src/redux/slice/vehicle/vehicle.ts
@@ -1,12 +1,32 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
+export interface Draft {
+    id: string;
+    name: string;
+    investmentVehicleType: string;
+    mandate: string;
+    sponsors: string;
+    tenure: number;
+    size: number;
+    rate: number;
+    trustee: string;
+    custodian: string;
+    bankPartner: string;
+    fundManager: string;
+    minimumInvestmentAmount: number;
+    status: string;
+    startDate: string;
+    totalAmountInInvestmentVehicle: number;
+}
 
 interface vehicleState {
     currentVehicleId: string;
+    saveClickedDraft: Draft | null;
 }
 
 const initialState : vehicleState = {
     currentVehicleId: '',
+    saveClickedDraft: null,
 }
 
 export const vehicleSlice = createSlice({
@@ -15,10 +35,15 @@ export const vehicleSlice = createSlice({
     reducers: {
         setCurrentVehicleId: (state, action: PayloadAction<string>) => {
             state.currentVehicleId = action.payload;
-        }
+        },
+        setSaveClickedDraft: (state, action: PayloadAction<Draft>) => {
+            state.saveClickedDraft = action.payload;
+        },
+        clearSaveClickedDraft: (state) => {
+            state.saveClickedDraft = null;
+        },
     }
 })
 
-export const {setCurrentVehicleId} = vehicleSlice.actions;
-
+export const { setCurrentVehicleId, setSaveClickedDraft, clearSaveClickedDraft } = vehicleSlice.actions;
 export default vehicleSlice.reducer;
