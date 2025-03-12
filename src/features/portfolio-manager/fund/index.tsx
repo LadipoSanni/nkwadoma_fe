6920f9deecc50b2b1fcf16387ee10457a71677f7
ikjames5454 <ikennajames03@gmail.com>
2025-03-12 17:25:37 +0100

    notification_query

--- src/features/portfolio-manager/fund/index.tsx
+++ src/features/portfolio-manager/fund/index.tsx
@@ -282,7 +282,7 @@ const InvestmentVehicle = () => {
                         isOpen={isModalOpen}
                         closeModal={() => setIsModalOpen(false)}
                         className='pb-1'
-                        headerTitle='Create Investment'
+                        headerTitle={modalType === 'commercial' ? "Commerciial fund" : "Endowment fund"}
                         closeOnOverlayClick={true}
                         icon={Cross2Icon}
                         width={"36%"}
