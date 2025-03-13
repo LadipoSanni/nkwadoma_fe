3db67f6028d45649892e6446fc7def0946e3ea27
a-mariam <mariiam22222@gmail.com>
2025-03-13 14:50:57 +0100

    adding authenticated routes to the authenticated layout

--- src/features/auth/Authentication/index.tsx
+++ src/features/auth/Authentication/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {getUserDetailsFromStorage} from "@/components/topBar/action";
 import {redirect} from "next/navigation";
+import {useToast} from "@/hooks/use-toast";
+import {isTokenExpired} from "@/utils/GlobalMethods";
+import {clearData} from "@/utils/storage";
 
 interface AuthProps {
 children: React.ReactNode;
@@ -8,17 +11,28 @@ children: React.ReactNode;
 const Index : React.FC<AuthProps> = ({children}) => {
     // const user_role = getUserDetailsFromStorage('user_role')
     const token = getUserDetailsFromStorage('access_token' )
+    const refreshToken = getUserDetailsFromStorage('refresh_token' )
 
+    const {toast} = useToast()
+    const response = isTokenExpired(token ? token : '')
+    const response2 = isTokenExpired(refreshToken ? refreshToken : '')
+    useEffect(() => {
+        checkUserToken(response, response2)
+    }, [response, response2]);
 
+    const checkUserToken = (isTokenExpired: boolean, isRefreshTokenExpired: boolean) => {
+        if (isTokenExpired && isRefreshTokenExpired) {
+            clearData()
+            toast({
+                description: "Session expired. Please login again",
+                status: "error",
+            });
+            redirect("/auth/login")
+        }
+    }
 
-    // useEffect(() => {
-    //     if (!user_role) {
-    //         redirect("/auth/login")
-    //     }
-    //     // else {
-    //     //     setRole(user_role)
-    //     // }
-    // }, [user_role]);
+    
+
 
     return token ? (
         <div>
