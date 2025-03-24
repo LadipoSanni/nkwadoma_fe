5935e6c5664d097c1e5620387f50ea67223f9e3e
a-mariam <mariiam22222@gmail.com>
2025-03-24 12:53:07 +0100

    creating financier side bar items

--- src/utils/sideBarItems/index.tsx
+++ src/utils/sideBarItems/index.tsx
@@ -1,4 +1,4 @@
-import { navbarRouterItemsProps} from "@/types/Component.type";
+import {navbarItemsProps, navbarRouterItemsProps} from "@/types/Component.type";
 import {Icon} from "@iconify/react";
 import {
     MdOutlineAccountBalance,
@@ -7,15 +7,17 @@ import {
     MdOutlineBusinessCenter,
     MdOutlinePersonOutline,
     MdOutlineHome,
-    MdOutlineAccountBalanceWallet
+    MdOutlineAccountBalanceWallet, MdOutlinePeopleAlt
 } from "react-icons/md";
 import React from "react";
+import {GearIcon} from "@radix-ui/react-icons";
+import {LuLogOut} from "react-icons/lu";
 
 const currentTextLiterals = `text-meedlBlue md:text-meedlBlue`;
 const textLiterals = `text-[#626F8C] md:text-[#626F8C] `;
 
 
-export const portfolioManagerSideBarItems =  (currentItem: string) => {
+export const getPortfolioManagerSideBarItems =  (currentItem: string) => {
     const items: navbarRouterItemsProps[] = [
         { icon: <MdOutlineHome color={'#d7d7d7'} className={` h-[1.2rem] w-[1.2em] `}/>, id: 'Overview', name: 'Overview', isActive: false,},
         {id: 'loan', name: 'Loan', route: '/loan/loan-request', isActive: true, icon: <Icon icon="material-symbols:money-bag-outline" height={"1.2rem"} width={"1.2rem"} color={currentItem === 'Loan' ? '#142854' : '#939cb0'}></Icon>},
@@ -32,13 +34,53 @@ export const portfolioManagerSideBarItems =  (currentItem: string) => {
 
 
 
-export const loaneeSideBarItems = ( current: string) => {
+export const getLoaneeSideBarItems = (currentItem: string) => {
 
     const items : navbarRouterItemsProps[] = [
-        {icon: <MdOutlineHome className={` h-[1.2rem] w-[1.2rem] ${current === 'Overview' ? currentTextLiterals : textLiterals} `}/>, id: 'overview', isActive: true, name: "Overview", route: '/overview'},
+        {icon: <MdOutlineHome className={` h-[1.2rem] w-[1.2rem] ${currentItem === 'Overview' ? currentTextLiterals : textLiterals} `}/>, id: 'overview', isActive: true, name: "Overview", route: '/overview'},
         {icon: <MdOutlineAccountBalanceWallet className={` h-[1.2rem] w-[1.2rem] text-[#d7d7d7] md:text-[#d7d7d7`}/>, id: 'wallet', name: "Wallet", isActive: false, route: '/wallet'},
         {icon: <Icon icon='iconoir:hand-cash' color={'#d7d7d7'} height={"1.2rem"} width={"1.3rem"}/>, id: 'repayment', isActive: false, name: "Repayment", route: '/repayment'},
     ]
     return items;
 }
 
+export const getInstituteAdminSideBarItems = (currentItem: string) => {
+    const items: navbarRouterItemsProps[] = [
+        {icon: <MdOutlineHome color={'#d7d7d7'} className={`h-[1.3rem] w-[1.3rem]`}/>,id: 'Overview', name: 'Overview', isActive: false},
+        {id: 'program', name: 'Program', route: '/program', isActive: true, icon: <Icon icon="mynaui:book" color={currentItem === 'Program' ? '#142854' : '#667085'} height={"1.3rem"} width={"1.3rem"}>         </Icon>},
+        {id: 'cohort', name: 'Cohort', route: '/cohort', isActive: true, icon: <MdOutlinePeopleAlt className={` h-[1.3rem] w-[1.3rem] ${currentItem === 'Cohort' ? currentTextLiterals : textLiterals} `}/>},
+        {id: 'loan', name: 'Loan', isActive: false, icon: <Icon icon="material-symbols:money-bag-outline" height={"1.2rem"} width={"1.3rem"} color={'#d7d7d7'} className={`h-[1.3rem] w-[1.3rem]`}></Icon>},
+        {id: 'loanee', name: 'Loanee', isActive: false, icon: <MdOutlinePersonOutline color={'#d7d7d7'} className={`h-[1.3rem] w-[1.3rem]`}/>},
+    ]
+    return items;
+}
+
+export const getSettingItem = ( currentNavBottomItem: string,handleClick:()=> void , userRole?: string | undefined) => {
+    const item: navbarItemsProps[] = [];
+
+    if (userRole === "PORTFOLIO_MANAGER") {
+        item.push(
+            {id: 'settings', name: 'Settings', icon: <GearIcon color={currentNavBottomItem === 'Settings' ? '#142854' : '#939CB0'} className={`text-navbarIconColor h-[1.3rem] w-[1.3rem] `}/>, handleClick: handleClick}
+        );
+    }
+    return item;
+}
+
+export const getLogoutItem = (currentNavBottomItem: string, handleClick: ()=> void)=> {
+    const item: navbarItemsProps =
+        {id: 'logout', name: 'Logout', icon: <LuLogOut color={currentNavBottomItem === 'Logout' ? '#142854' : '#939CB0'} className={` h-[1.2rem] w-[1.2rem] `}/>, handleClick: handleClick
+    };
+    return item;
+}
+
+
+export const getFinancierSideBarItems = (currentNavItem: string)=> {
+    const item : navbarRouterItemsProps[] = [
+        {icon: <MdOutlineHome className={` h-[1.2rem] w-[1.2rem] ${currentNavItem === 'Overview' ? currentTextLiterals : textLiterals} `}/>, id: 'overview', isActive: true, name: "Overview", route: '/overview'},
+        {id: 'marketplace', name: 'Marketplace', route: '/marketplace', isActive: true, icon: <MdOutlineAccountBalance className={` h-[1.2rem] w-[1.2em]  `} color={currentNavItem === 'Marketplace' ? '#142854' : '#939CB0'}></MdOutlineAccountBalance>},
+        {icon: <Icon icon='iconoir:hand-cash' color={'#d7d7d7'} height={"1.2rem"} width={"1.3rem"}/>, id: 'repayment', isActive: false, name: "Repayment", route: '/repayment'},
+    ]
+    console.log(currentNavItem)
+    return item;
+
+}
