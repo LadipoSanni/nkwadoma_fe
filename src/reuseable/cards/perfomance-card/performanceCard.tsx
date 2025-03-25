316843bc897971d0ceaea5a3b88eecd309c97606
a-mariam <mariiam22222@gmail.com>
2025-03-25 20:59:16 +0100

    working on performance card styles and props

--- src/reuseable/cards/perfomance-card/performanceCard.tsx
+++ src/reuseable/cards/perfomance-card/performanceCard.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import {cabinetGroteskBold, inter} from "@/app/fonts";
+import { MdArrowUpward, MdOutlineArrowDownward } from "react-icons/md";
 
-const PerformanceCard = () => {
+interface PerformanceCardProps {
+
+}
+
+const PerformanceCard = ({}: PerformanceCardProps) => {
     return (
-        <div className={` w-[18rem] py-3 px-3 h-[10rem] border border-blue550 rounded-md `}>
-            <div className={` w-full h-full bg-grey105 px-4 py-8`}>
+        <div className={` w-[20rem] py-3 px-3 h-[10rem] border border-blue550 rounded-md `}>
+            <div className={` w-full h-full bg-grey105 px-4 py-4 grid content-between`}>
 
-                diufhi
+                <p className={`text-[16px] ${inter.className} text-black300`}>Portfolio percentage</p>
+                <div className={`flex justify-between w-full  `}>
+                    <p className={` ${cabinetGroteskBold.className} text-[36px] text-meedlBlue `}>30%</p>
+                    <div className={` flex rounded-full w-fit py-0.5 gap-1 px-1 h-fit mt-auto mb-auto bg-[#E7F5EC] text-[#085322]  `}>
+                        <MdArrowUpward className={` mt-auto mb-auto  `} />
+                        <p className={` ${inter.className} text-[13px]  `}>12.6%</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
