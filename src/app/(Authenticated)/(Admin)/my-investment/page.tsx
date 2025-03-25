6a2e9402c19b8ea5f84006c1386934d6177b396c
a-mariam <mariiam22222@gmail.com>
2025-03-25 16:28:51 +0100

    customizing bar chart to fit meedl bar chart

--- src/app/(Authenticated)/(Admin)/my-investment/page.tsx
+++ src/app/(Authenticated)/(Admin)/my-investment/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Index from "@/reuseable/bar-chart";
 
 const Page = () => {
     return (
         <div>
-            
+            <Index maxHeight={'20rem'} maxWidth={'30rem'} componentId={'e'} />
         </div>
     );
 };
---
+++ test/reuseable/bar-chart/index.test.tsx
@@ -0,0 +1,16 @@
+import Index from '@/reuseable/bar-chart/index'
+import {render, screen} from "@testing-library/react";
+
+describe('test bar chart component', () => {
+
+    test('that bar chart exists', ()=> {
+        render(<Index componentId={'testingBarChart'} />);
+        expect(screen.getByTestId('testingBarChart')).toBeInTheDocument();
+    })
+
+    test('that bar chart display number of bar passed to it', ()=> {
+        render(<Index componentId={'testingBarChart'} />);
+
+    })
+
+})
