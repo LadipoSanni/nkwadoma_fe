1502df72bae541643baf61dcd94eb98760a8da59
koleajeolayinka <koleajeolayinka@gmail.com>
2025-03-27 11:19:14 +0100

    feat: make BalanceCard component dynamic and update overview pages

--- src/pages/financier/overview/Index.tsx
+++ src/pages/financier/overview/Index.tsx
@@ -5,19 +5,29 @@ import BalanceCard from "@/reuseable/cards/BalanceCard/Index";
 import MyInvestments from "@/features/financier/summary/myInvestments/Index"
 import InvestmentMarketplace from '@/features/financier/summary/investmentMarketplace/Index'
 
+const financialCardData = [
+    {
+        title: "Number of investments", amount: "0", linkText: "View"
+    },
+    {
+        title: "Amount invested", amount: "0", linkText: "View"
+    },
+    {
+        title: "Portfolio value", amount: "0", linkText: "View"
+    }
+];
+
 const FinancierOverview = () => {
     return (
         <main className={'px-5 h-[88vh] overflow-y-auto pb-8'}>
-            <section>
 
-            </section>
             <div
                 className={`${inter500.className} h-16 w-full mt-[38px] p-5 flex gap-2 bg-warning150 border-[0.5px] border-warning650 rounded-[6px]`}>
                 <MdOutlineErrorOutline className={'h-[22px] w-[22px] text-warning650'}/>
                 <p className={'text-warning650 leading-[150%] text-[16px]'}>Click here to complete your KYC</p>
             </div>
             <section className={'mt-8'}>
-                <BalanceCard/>
+                <BalanceCard cardData={financialCardData} />
             </section>
             <div className={'mt-8'}>
                 <MyInvestments />
