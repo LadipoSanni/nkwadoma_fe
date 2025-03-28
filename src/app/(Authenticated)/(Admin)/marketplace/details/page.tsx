c7bdd99da9dfdfd855141961ca5a0a75f7814744
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-28 21:08:15 +0100

    market place layoutils page

---
+++ src/app/(Authenticated)/(Admin)/marketplace/details/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MarketPlaceDetails from "@/features/market-place/details/Index";
+
+const Page = () => {
+    return (
+        <MarketPlaceDetails/>
+    );
+}
+
+export default Page;
