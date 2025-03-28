6ab45bb870c747634759c01569936a74cc05e9e6
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-28 16:30:48 +0100

    Transfer page

---
+++ src/app/(Authenticated)/(Admin)/marketplace/(invest)/transfer/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react';
+import Transfer from "@/features/market-place/Invest/transfer/Index";
+
+const Page = () => {
+    return (
+        <>
+            <Transfer/>
+        </>
+    );
+}
+
+export default Page;
