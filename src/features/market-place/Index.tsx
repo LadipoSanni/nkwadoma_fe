03a2353439f648563ac9486049c9b6aab0e3681a
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-28 15:20:09 +0100

    style

--- src/features/market-place/Index.tsx
+++ src/features/market-place/Index.tsx
@@ -62,17 +62,17 @@ const MarketPlaceView = () => {
                         selectContent={['Commercial Investment', 'Endowment Investment']}
                         placeHolder="Type"
                         triggerId="marketplaceTrigger"
-                        className={`h-11 md:w-sm w-full mt-0 bg-[#D0D5DD]`}
+                        className={`h-11 md:w-sm w-full mt-0 bg-[#F7F7F7] border border-[#D0D5DD]`}
                     />
             </div>
 
             <div id={"card-segmentId"}
-                 className="grid grid-cols-1 px-4 md:grid-cols-2 lg:grid-cols-4 max:md:h-96 h-96 overflow-x-hidden overflow-y-auto gap-y-10 gap-x-5">
+                 className="grid grid-cols-1 px-4 md:grid-cols-2 lg:grid-cols-4 h-[70vh] overflow-x-hidden overflow-y-auto gap-y-10 gap-x-5">
             {dummyInvestments.map((investment) => {
                     const backgroundColor = investment.type === "Commercial" ? "#D9EAFF" : "#E6F2EA";
                     const imageSrc = investment.type === "Commercial"
-                        ? "/asset/image/Circles.svg"
-                        : "/asset/image/Asset.svg";
+                        ? "/asset/image/Asset.svg"
+                        :"/asset/image/Circles.svg";
                     const statusClass = investment.status === "Open"
                         ? "bg-green-100 text-[#0D9B48] border-[#B4E5C8} "
                         : "bg-red-100 text-red-600 border-[#F2BCBA]";
