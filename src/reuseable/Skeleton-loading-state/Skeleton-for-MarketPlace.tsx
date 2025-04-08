53d584b0a67afadb2ee20092dcd3973b33209726
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-04-08 15:51:27 +0100

    skeleton

--- src/reuseable/Skeleton-loading-state/Skeleton-for-MarketPlace.tsx
+++ src/reuseable/Skeleton-loading-state/Skeleton-for-MarketPlace.tsx
@@ -5,23 +5,35 @@ import {Skeleton} from '@/components/ui/skeleton'
 function MarketPlaceInvestmentGrid() {
 
     const skeletonList = [
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-2xl bg-[#F9F9F9] "/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-2xl bg-[#F9F9F9]  "/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-2xl bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-2xl bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-2xl bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-2xl bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-2xl bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-2xl bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-2xl bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-2xl bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-2xl bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-2xl bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-2xl bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-2xl bg-[#F9F9F9]"/>},
+        {value: <Skeleton className="h-[328px] w-36  bg-[#F9F9F9] "/>},
+        {value: <Skeleton className="h-[328px] w-36  bg-[#F9F9F9] "/>},
+        {value: <Skeleton className="h-[328px] w-36  bg-[#F9F9F9] "/>},
+        {value: <Skeleton className="h-[328px] w-36  bg-[#F9F9F9] "/>},
+        {value: <Skeleton className="h-[328px] w-36  bg-[#F9F9F9] "/>},
+        {value: <Skeleton className="h-[328px] w-36  bg-[#F9F9F9] "/>},
+        {value: <Skeleton className="h-[328px] w-36  bg-[#F9F9F9] "/>},
+        {value: <Skeleton className="h-[328px] w-36 bg-[#F9F9F9] "/>},
+        {value: <Skeleton className="h-[328px] w-36  bg-[#F9F9F9] "/>},
+        {value: <Skeleton className="h-[328px] w-36  bg-[#F9F9F9] "/>},
+        {value: <Skeleton className="h-[328px] w-36  bg-[#F9F9F9] "/>},
+        {value: <Skeleton className="h-[328px] w-36  bg-[#F9F9F9] "/>},
+        {value: <Skeleton className="h-[328px] w-36  bg-[#F9F9F9] "/>},
+        // {value: <Skeleton className=" w-40 bg-[#F9F9F9]  "/>},
+        // {value: <Skeleton className=" w-40 bg-[#F9F9F9]"/>},
+        // {value: <Skeleton className=" w-40  bg-[#F9F9F9]"/>},
+        // {value: <Skeleton className="bg-[#F9F9F9]"/>},
+        // {value: <Skeleton className="bg-[#F9F9F9]"/>},
+        // {value: <Skeleton className="bg-[#F9F9F9]"/>},
+        // {value: <Skeleton className=" bg-[#F9F9F9]"/>},
+        // {value: <Skeleton className=" bg-[#F9F9F9]"/>},
+        // {value: <Skeleton className=" bg-[#F9F9F9]"/>},
+        // {value: <Skeleton className=" bg-[#F9F9F9]"/>},
+        // {value: <Skeleton className=" bg-[#F9F9F9]"/>},
+        // {value: <Skeleton className=" bg-[#F9F9F9]"/>},
+        // {value: <Skeleton className=" bg-[#F9F9F9]"/>},
     ]
     return (
-        <div className="grid md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
             {skeletonList.map((item, index) => (
                 <div key={index}>
                     {item.value}
