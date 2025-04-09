82a4a7bef40a4769eb9bd9f74daf56bab9876cfe
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-04-09 17:35:30 +0100

    height was added to skeleton

--- src/reuseable/Skeleton-loading-state/Skeleton-for-MarketPlace.tsx
+++ src/reuseable/Skeleton-loading-state/Skeleton-for-MarketPlace.tsx
@@ -5,14 +5,14 @@ import {Skeleton} from '@/components/ui/skeleton'
 function MarketPlaceInvestmentGrid() {
 
     const skeletonList = [
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-lg bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-lg bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-lg bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-lg bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-lg bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-lg bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-lg bg-[#F9F9F9]"/>},
-        {value: <Skeleton className="h-[13.8125rem] md:max-w-lg bg-[#F9F9F9]"/>},
+        {value: <Skeleton className="md:h-80 h-72 md:max-w-lg bg-[#F9F9F9]"/>},
+        {value: <Skeleton className="md:h-80 h-72 md:max-w-lg bg-[#F9F9F9]"/>},
+        {value: <Skeleton className="md:h-80 h-72 md:max-w-lg bg-[#F9F9F9]"/>},
+        {value: <Skeleton className="md:h-80 h-72 md:max-w-lg bg-[#F9F9F9]"/>},
+        {value: <Skeleton className="md:h-80 h-72 md:max-w-lg bg-[#F9F9F9]"/>},
+        {value: <Skeleton className="md:h-80 h-72 md:max-w-lg bg-[#F9F9F9]"/>},
+        {value: <Skeleton className="md:h-80 h-72 md:max-w-lg bg-[#F9F9F9]"/>},
+        {value: <Skeleton className="md:h-80 h-72 md:max-w-lg bg-[#F9F9F9]"/>},
     ]
     return (
         <div className="grid grid-cols-1 px-3 md:grid-cols-3 w-full sm:grid-cols-2 lg:grid-cols-4 gap-y-10 gap-x-5">
