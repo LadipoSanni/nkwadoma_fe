f586207860cfd2dc767ebd0455bb13f4d6ba30d4
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-04-15 16:19:53 +0100

    Status for disabling invest button was added

--- src/redux/slice/investors/MarketPlaceSlice.ts
+++ src/redux/slice/investors/MarketPlaceSlice.ts
@@ -4,7 +4,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 export interface MarketData {
     marketInvestmentVehicleId: string;
     vehicleType: string;
-    status: string | null;
+    status: string;
 }
 
 interface ProgramSliceState {
