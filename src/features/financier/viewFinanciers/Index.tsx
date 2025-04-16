f849697ed82a8886ca5cf970057e595610400ca3
ikjames5454 <ikennajames03@gmail.com>
2025-04-16 17:00:33 +0100

    refcatore the website link on organization details

--- src/features/financier/viewFinanciers/Index.tsx
+++ src/features/financier/viewFinanciers/Index.tsx
@@ -118,20 +118,6 @@ const ViewFinanciers = () => {
         setIsDropdownOpen(false);
     };
 
-    // const financierView = [
-    //     { id: '1', name: 'Financier 1', type: 'Individual', investments: 5, amountInvested: 10000, amountEarned: 2000, payout: 500, portfolioValue: 12000 },
-    //     { id: '2', name: 'Financier 2', type: 'Corporate', investments: 10, amountInvested: 50000, amountEarned: 10000, payout: 2000, portfolioValue: 60000 },
-    //     { id: '3', name: 'Financier 3', type: 'Individual', investments: 7, amountInvested: 15000, amountEarned: 3000, payout: 700, portfolioValue: 18000 },
-    //     { id: '4', name: 'Financier 4', type: 'Corporate', investments: 12, amountInvested: 60000, amountEarned: 12000, payout: 2500, portfolioValue: 72000 },
-    //     { id: '5', name: 'Financier 5', type: 'Individual', investments: 3, amountInvested: 8000, amountEarned: 1600, payout: 400, portfolioValue: 9600 },
-    //     { id: '6', name: 'Financier 6', type: 'Corporate', investments: 15, amountInvested: 75000, amountEarned: 15000, payout: 3000, portfolioValue: 90000 },
-    //     { id: '7', name: 'Financier 7', type: 'Individual', investments: 4, amountInvested: 9000, amountEarned: 1800, payout: 450, portfolioValue: 10800 },
-    //     { id: '8', name: 'Financier 8', type: 'Corporate', investments: 20, amountInvested: 100000, amountEarned: 20000, payout: 4000, portfolioValue: 120000 },
-    //     { id: '9', name: 'Financier 9', type: 'Individual', investments: 6, amountInvested: 12000, amountEarned: 2400, payout: 600, portfolioValue: 14400 },
-    //     { id: '10', name: 'Financier 10', type: 'Corporate', investments: 25, amountInvested: 125000, amountEarned: 25000, payout: 5000, portfolioValue: 150000 },
-    //     { id: '11', name: 'Financier 11', type: 'Individual', investments: 8, amountInvested: 16000, amountEarned: 3200, payout: 800, portfolioValue: 19200 },
-    //     { id: '12', name: 'Financier 12', type: 'Corporate', investments: 30, amountInvested: 150000, amountEarned: 30000, payout: 6000, portfolioValue: 180000 },
-    // ];
 
     const financierHeader = [
         { title: 'Name', id: 'name', selector: (row: viewAllfinancier) => row?.financierType === "INDIVIDUAL"? row.userIdentity?.firstName + " " + row.userIdentity?.lastName : row?.organizationName},
@@ -172,6 +158,7 @@ const ViewFinanciers = () => {
                         isDropdownOpen={isDropdownOpen}
                         toggleDropdown={toggleDropdown}
                         isLoading={isLoading}
+                        disabled={true}
                     />
                 </div>
                 <Button
@@ -179,7 +166,7 @@ const ViewFinanciers = () => {
                     size={"lg"}
                     className={`${inter.className} bg-meedlBlue text-meedlWhite h-[2.8125rem] w-full md:w-[7.8125rem] flex justify-center items-center`}
                     id='createProgramModal'
-                    onClick={() => setIsModalOpen(true)} // Open modal on button click
+                    onClick={() => setIsModalOpen(true)}
                 >
                     Invite financier
                 </Button>
