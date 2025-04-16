b6893ff98cd84675eb948762d31e88d1dc751350
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-04-16 17:10:00 +0100

    Details implementation

--- src/features/market-place/details/Index.tsx
+++ src/features/market-place/details/Index.tsx
@@ -3,51 +3,11 @@ import React from 'react';
 import BackButton from "@/components/back-button";
 import {useRouter} from "next/navigation";
 import Image from "next/image";
-import {cabinetGrotesk, cabinetGroteskMediumBold600, inter, inter500} from "@/app/fonts";
+import {cabinetGroteskMediumBold600, inter, inter500} from "@/app/fonts";
 import {Button} from "@/components/ui/button";
 import styles from "../../market-place/Index.module.css";
 import {useSelector} from "react-redux";
 import {RootState} from "@/redux/store";
-
-
-interface SectionProps {
-    index: number;
-    title: string;
-    content: React.ReactNode;
-    subtitle?: string;
-    isList?: boolean;
-    parentIndex?: number;
-}
-
-const Section: React.FC<SectionProps> = ({index, title, content, subtitle, isList = false, parentIndex}) => {
-
-    const number = parentIndex !== undefined ? `${parentIndex + 1}.${index + 1}` : `${index + 1}.`;
-
-
-    return (
-        <div className="space-y-3">
-            {title && (
-                <h1 className={`${cabinetGrotesk.className} text-2xl font-extralight flex items-start gap-2 mb-4`}>
-                    <span>{number}</span>
-                    <span>{title}</span>
-                </h1>
-            )}
-            {subtitle && (
-                <h3 className="font-medium text-lg flex items-start gap-2 md-[15px]">
-                    {parentIndex !== undefined && <span>{number}</span>}
-                    <span>{subtitle}</span>
-                </h3>
-            )}
-            {isList ? (
-                <ul className={`${inter.className} list-disc pl-5 text-gray-700 font-normal text-base space-y-4`}>
-                    {content}
-                </ul>
-            ) : (
-                <p className={`${inter.className} font-normal text-base`}>{content}</p>
-            )}
-        </div>
-    );
-};
 
 const MarketPlaceDetails = () => {
     const router = useRouter();
@@ -60,7 +20,6 @@ const MarketPlaceDetails = () => {
         marketInvestmentVehicleId,
         vehicleType,
     } = useSelector((state: RootState) => state.marketPlace.savedMarketplaceData) || {};
-    // console.log(marketInvestmentVehicleId, vehicleType);
 
     const HandleInvest = () => {
         // vehicleType
@@ -89,42 +48,12 @@ const MarketPlaceDetails = () => {
         },
     ];
 
-    const purposeAndObjectiveData = [
-        {
-            title: "Purpose & Objectives",
-            content: "The Tech Innovators Fund aims to generate high returns by investing in early-stage and growth-stage software engineering companies. The fund prioritizes companies developing innovative software solutions with scalable business models and strong market potential.",
-            isList: false,
-        },
-        {
-            title: "Investment Strategy",
-            subsections: [
-                {
-                    subtitle: "Target Sectors",
-                    content: (
-                        <>
-                            <li>Artificial Intelligence & Machine Learning</li>
-                            <li>Cloud Computing & SaaS Platforms</li>
-                            <li>Cybersecurity</li>
-                            <li>Fintech & Blockchain</li>
-                            <li>Enterprise Software & Automation</li>
-                        </>
-                    ),
-                    isList: true,
-                },
-                {
-                    subtitle: "Investment Stages",
-                    content: <li>Pre-seed, Seed, Series A, and Growth-stage startups</li>,
-                    isList: true,
-                },
-            ],
-        },
-    ];
-    // const bgColor = vehicleType === "commercial" ? "#D9EAFF" : "#E6F2EA";
-    const bgColor = vehicleType === "Commercial" ? "bg-[#D9EAFF]" : "bg-[#E6F2EA]";
+    const bgColor = vehicleType === "COMMERCIAL" ? "bg-[#D9EAFF]" : "bg-[#E6F2EA]";
     const imageSrc =
-        vehicleType === "Commercial"
+        vehicleType === "COMMERCIAL"
             ? "/BlueCircles.svg"
             : "/GreenCircles.svg";
+    const typeTextColor = vehicleType === "COMMERCIAL" ? "text-[#142854]" : "text-[#045620]";
 
     return (
         <main id="mainDiv" className="md:px-10 py-6 px-3 w-full md:gap-10 gap-8">
@@ -140,19 +69,21 @@ const MarketPlaceDetails = () => {
 
             <div
                 id="detailsPurposeAndObjectiveDiv"
-                className={`w-full md:w-full grid md:flex md:justify-between gap-3.5 gap- md:pt-4 pt-4`}
+                className={`flex items-center justify-center md:pt-4 pt-4`}
             >
                 <div
                     id="purpposeDiv"
-                    className={`${styles.container} w-full md:w-1/3 grid  md:h-[70vh] md:max-h-none `}
+                    className={`${styles.container} w-full grid md:w-2/5 md:h-[70vh] md:max-h-none `}
                 >
                     <div id="backgroundId" className={`w-full md:w-full rounded-md md:rounded-md ${bgColor}`}>
                         <div id="type" data-testid="type" className="py-5 px-4 flex flex-col">
                             <div
                                 id="investmentTypeId"
-                                className="bg-white text-black text-sm font-medium rounded-[32px] px-3 py-1 w-[104px] h-[29px] flex items-center justify-center"
+                                className={`bg-white ${typeTextColor} text-sm font-medium rounded-[32px] px-3 py-1 w-[104px] h-[29px] flex items-center justify-center`}
                             >
-                                {vehicleType}
+                                {vehicleType
+                                    ? vehicleType.charAt(0).toUpperCase() + vehicleType.slice(1).toLowerCase()
+                                    : ""}
                             </div>
                         </div>
                         <div id="imageId" className="object-right-bottom justify-end flex">
@@ -169,10 +100,30 @@ const MarketPlaceDetails = () => {
                     </div>
 
                     <p
-                        className={`id="keyValuePairId" ${cabinetGroteskMediumBold600.className} md:text-[32px] text-[24px] md:text-[#212221]`}
+                        className={`id="keyValuePairId" ${cabinetGroteskMediumBold600.className} md:text-[32px] text-[24px] pt-5 md:text-[#212221]`}
                     >
                         Software Engineering Fund
                     </p>
+
+                    <div className={`${inter.className} flex md:flex-row gap-4`}>
+                        <div className={`${bgColor} rounded-full h-12 w-12`}>
+                            <Image
+                                src={`BlueCircles.svg`}
+                                alt="logo"
+                                width={104}
+                                height={29}
+                                className="p-3 h-4 w-4 object-cover"
+                                data-testid="circle-image"
+                                loading="lazy"
+                            />
+                        </div>
+                        <div className={`flex flex-col gap-1`}>
+                            <div className={`text-[#6A6B6A] text-sm font-normal`}>Fund manager</div>
+                            <div className={`text-sm font-medium text-[#212221]`}>Cowrywise</div>
+                        </div>
+                    </div>
+
+
                     <div
                         className="bg-[#F9F9F9] h-fit md:grid px-5 w-full">
                         {investmentBasicDetails?.map((item, index) => (
@@ -191,50 +142,19 @@ const MarketPlaceDetails = () => {
                         ))}
                     </div>
 
-                    <div className={`pt-4`}>
+                    <div className={`pt-3`}>
                         <Button type="button" id={`invest-button`} size="lg" variant="secondary"
                                 className={`${inter.className} bg-meedlBlue w-full text-meedlWhite`}
                                 onClick={HandleInvest}
                                 // disabled={color === 'black'}
                                 // disabled={status === 'CLOSE'}
                                 // className={`${inter.className} ${status === 'CLOSE'? " bg-black  cursor-not-allowed" : "bg-meedlBlue text-meedlWhite"}  w-full `}
-
                         >
                             Invest
                         </Button>
                     </div>
                 </div>
 
-                <div
-                    id="purposeAndObjectiveDiv"
-                    className={`${styles.container} text-[#212221] w-full md:w-3/5 md:h-[75vh] md:max-h-none rounded-[20px] border border-[#D7D7D7] bg-grey105 md:p-10 py-2 px-5 space-y-6`}
-                >
-                    {purposeAndObjectiveData.map((section, index) => (
-                        <div key={index}>
-                            <Section
-                                index={index}
-                                title={section.title}
-                                content={section.content || ""}
-                                isList={section.isList}
-                            />
-                            {section.subsections && (
-                                <div className="space-y-4">
-                                    {section.subsections.map((subsection, subIndex) => (
-                                        <Section
-                                            key={`${index}-${subIndex}`}
-                                            index={subIndex}
-                                            parentIndex={index}
-                                            title=""
-                                            subtitle={subsection.subtitle}
-                                            content={subsection.content}
-                                            isList={subsection.isList}
-                                        />
-                                    ))}
-                                </div>
-                            )}
-                        </div>
-                    ))}
-                </div>
             </div>
         </main>
     );
