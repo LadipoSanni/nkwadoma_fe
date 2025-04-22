776b4d53ff6ab5de0650491c96e7049e0ee17aca
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-04-22 16:16:11 +0100

    Importation was corrected

--- src/app/(Authenticated)/(Admin)/financier/(financialDetails)/layout.tsx
+++ src/app/(Authenticated)/(Admin)/financier/(financialDetails)/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TabSwitch from "@/layout/tabLayout";
-import {financialTabData} from "@/types/tabDataTypes";
+import {financierTabData} from "@/types/tabDataTypes";
 import CustomAuthorization from "@/features/auth/authorization";
 
 type props = {
@@ -11,7 +11,7 @@ const FinancialTabs: React.FC<props> = ({ children }) => {
 
     return (
         <CustomAuthorization authorizedRoles={['PORTFOLIO_MANAGER']}>
-            <TabSwitch tabData={financialTabData} defaultTab="/financier/details" backClickRoutePath="/financier" backClickName="financiers"  condition={true}>
+            <TabSwitch tabData={financierTabData} defaultTab="/financier/details" backClickRoutePath="/financier" backClickName="financiers"  condition={true}>
                 {children}
             </TabSwitch>
         </CustomAuthorization>
