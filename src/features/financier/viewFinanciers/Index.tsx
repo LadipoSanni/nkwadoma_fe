5d31bf487b078310360cfc6b8466f7531c3c0f4a
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-05-05 14:13:27 +0100

    space between drop down text and arrow was corrected

--- src/features/financier/viewFinanciers/Index.tsx
+++ src/features/financier/viewFinanciers/Index.tsx
@@ -170,7 +170,7 @@ const ViewFinanciers = () => {
                         selectContent={["Individual", "Cooperate", "Reset"]}
                         placeHolder="Select type"
                         triggerId="financierSelectId"
-                        className="h-11 md:w-sm w-fit  mt-0 bg-[#F7F7F7] border border-[#D0D5DD]"
+                        className="h-11 md:w-32 w-full  mt-0 bg-[#F7F7F7] border border-[#D0D5DD]"
                     />
                 </div>
                 <Button
