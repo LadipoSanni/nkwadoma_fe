02e2b09eb8f8c366e2e32d6d36e51546a9e14568
a-mariam <mariiam22222@gmail.com>
2025-05-11 20:13:14 +0100

    fixed double import

--- src/components/portfolio-manager/fund/investmentVehicle-multistep/Choose-visibility.tsx
+++ src/components/portfolio-manager/fund/investmentVehicle-multistep/Choose-visibility.tsx
@@ -6,8 +6,7 @@ import { Formik, Form, FormikErrors } from "formik";
 import * as Yup from "yup";
 import { Label } from '@/components/ui/label';
 import Isloading from "@/reuseable/display/Isloading";
-import {store} from "@/redux/store";
-import {useAppSelector} from "@/redux/store";
+import {store,useAppSelector} from "@/redux/store";
 import {clearVisibilityType } from '@/redux/slice/multiselect/vehicle-multiselect';
 import { useRouter } from "next/navigation";
 import CustomSelectId from '@/reuseable/Input/custom-select-id';
@@ -23,8 +22,7 @@ import {
     useFinancierInvestmentVehicleQuery
 } from '@/service/admin/fund_query';
 import { useToast } from "@/hooks/use-toast";
-import { clearDraftId,clearPublicVehicleUrl} from '@/redux/slice/vehicle/vehicle';
-import { clearSaveCreateInvestmentField,clearSaveInvestmentStatus } from '@/redux/slice/vehicle/vehicle'
+import { clearDraftId,clearPublicVehicleUrl, clearSaveCreateInvestmentField,clearSaveInvestmentStatus} from '@/redux/slice/vehicle/vehicle';
 import DisplayFinancierInvehicle
     from "@/components/portfolio-manager/fund/investmentVehicle-multistep/DisplayFinancierInvehicle";
 import {FinancierInInvestmentVehicle} from "@/types/Component.type";
