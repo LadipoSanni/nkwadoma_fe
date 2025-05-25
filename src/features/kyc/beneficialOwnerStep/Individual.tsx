c9d1502756981caef22e51e9a9312f811b40fcdf
a-mariam <mariiam22222@gmail.com>
2025-05-25 22:31:54 +0100

    working on individual component

--- src/features/kyc/beneficialOwnerStep/Individual.tsx
+++ src/features/kyc/beneficialOwnerStep/Individual.tsx
@@ -7,62 +7,93 @@ import {MdKeyboardArrowDown, MdKeyboardArrowUp, MdOutlineDateRange} from "react-
 import {Calendar} from "@/components/ui/calendar";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
 import FileUpload from "../../../reuseable/Input/FileUpload";
+import {validateEntityOwnership, validateName} from "@/utils/GlobalMethods";
 
-interface EntityData {
-    name: string,
-    country: string,
-    rcNumber: string,
+interface IndividualData {
+    firstName: string,
+    lastName: string,
+    dateOfBirth: string,
+    relationShip: string,
     ownership: string,
     errorMessage: string,
     entityError: string,
 
 }
 const Individual = () => {
-    const initialEntityDate = {
-        name: '',
-        country: '',
-        rcNumber: '',
+    const initialIndividualData = {
+        firstName: '',
+        lastName: '',
+        dateOfBirth: '',
+        relationShip: '',
         ownership: '',
+        errorMessage: '',
         entityError: '',
-        errorMessage: ''
 
     }
 
-    const [entityData, setEntityData] = React.useState<EntityData>(initialEntityDate)
+    const [individualData, setIndividualData] = React.useState<IndividualData>(initialIndividualData)
+
+    const handleInputChange = ( field: string, value: string) => {
+        setIndividualData((prevState) => (
+            { ...prevState, [field]: value }
+        ))
+    }
+    const handleDateSelect = (field: string, date: Date | undefined) => {
+        console.log('date: ', date)
+        setIndividualData((prev) =>(
+                { ...prev, [field]: date ? date.toISOString() : undefined}
+
+        ));
+    };
+
     return (
         <div className="grid gap-5">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div className="space-y-2">
-                    <Label htmlFor={`firstName-${section.id}`}>First
+                    <Label htmlFor={`firstName-`}>First
                         name</Label>
                     <Input
-                        id={`firstName-${section.id}`}
+                        id={`firstName`}
                         name="firstName"
                         placeholder="Enter first name"
-                        value={section.firstName || ""}
-                        onChange={(e) =>
-                            handleInputChange(section.id, "firstName", e.target.value)
-                        }
+                        value={individualData.firstName || ""}
+                        onChange={(e) => {
+                            const isInputValid = validateName(e.target.value)
+                            if (typeof  isInputValid === "string") {
+                                setIndividualData((prevState) => (
+                                    { ...prevState, ['entityError']: 'firstName' }
+                                ))
+                                setIndividualData((prevState) => (
+                                    { ...prevState, ['errorMessage']: isInputValid }
+                                ))
+
+                            }else {
+                                setIndividualData((prevState) => (
+                                    { ...prevState, ['errorMessage']: '' }
+                                ))
+                                handleInputChange('firstName', e.target.value)
+                            }
+                        }}
                         className="p-4 focus-visible:outline-0 shadow-none focus-visible:ring-transparent rounded-md h-[3.375rem] font-normal leading-[21px] text-[14px] placeholder:text-grey250 text-black500 border border-solid border-neutral650"
                     />
-                    {section.errors?.firstName && (
-                        <p className="text-red-500 text-sm">{section.errors.firstName}</p>
+                    {individualData.entityError === 'firstName' && (
+                        <p className="text-red-500 text-sm">{individualData.errorMessage}</p>
                     )}
                 </div>
                 <div className="space-y-2">
-                    <Label htmlFor={`lastName-${section.id}`}>Last name</Label>
+                    <Label htmlFor={`lastName-`}>Last name</Label>
                     <Input
-                        id={`lastName-${section.id}`}
+                        // id={`lastName-${section.id}`}
                         name="lastName"
                         placeholder="Enter last name"
-                        value={section.lastName || ""}
+                        value={individualData.lastName || ""}
                         onChange={(e) =>
-                            handleInputChange(section.id, "lastName", e.target.value)
+                            handleInputChange( "lastName", e.target.value)
                         }
                         className="p-4 focus-visible:outline-0 shadow-none focus-visible:ring-transparent rounded-md h-[3.375rem] font-normal leading-[21px] text-[14px] placeholder:text-grey250 text-black500 border border-solid border-neutral650"
                     />
-                    {section.errors?.lastName && (
-                        <p className="text-red-500 text-sm">{section.errors.lastName}</p>
+                    {individualData.entityError === 'lastName' && (
+                        <p className="text-red-500 text-sm">{individualData.errorMessage}</p>
                     )}
                 </div>
             </div>
@@ -76,22 +107,19 @@ const Individual = () => {
                                 variant="outline"
                                 className="w-full h-[3.375rem] border border-solid border-neutral650 flex justify-between items-center font-normal focus:outline-none focus:ring-0"
                             >
-                                                                            <span>
-                                                                                {section.dob ? new Date(section.dob).toLocaleDateString() : "Select date"}
-                                                                            </span>
-                                <MdOutlineDateRange
-                                    className="h-5 w-5 text-neutral950"/>
+                                  {individualData.dateOfBirth ? new Date(initialIndividualData.dateOfBirth).toLocaleDateString() : "Select date"}
+                                <MdOutlineDateRange className="h-5 w-5 text-neutral950"/>
                             </Button>
                         </PopoverTrigger>
                         <PopoverContent className="w-auto p-0 shadow-none"
                                         align="start">
                             <Calendar
                                 mode="single"
-                                selected={section.dob ? new Date(section.dob) : undefined}
-                                onSelect={(date) => handleDateSelect(section.id, date)}
+                                selected={individualData.dateOfBirth ? new Date(individualData.dateOfBirth) : undefined}
+                                onSelect={(date) => handleDateSelect('dateOfBirth', date)}
                                 disabled={(date) => date > new Date()}
                                 initialFocus
-                                defaultMonth={section.dob ? new Date(section.dob) : new Date()}
+                                defaultMonth={individualData.dateOfBirth ? new Date(individualData.dateOfBirth) : new Date()}
                             />
                         </PopoverContent>
                     </Popover>
@@ -99,24 +127,24 @@ const Individual = () => {
                 <div className="space-y-2">
                     <Label>Relationship</Label>
                     <Select
-                        onValueChange={(value) => handleRelationshipSelect(section.id, value)}
-                        value={section.relationship || ""}
-                        onOpenChange={(open) => setIsOpen({
-                            ...isOpen,
-                            [section.id]: open
-                        })}
+                        onValueChange={(value) => handleInputChange('relationShip', value)}
+                        value={individualData.relationShip || ""}
+                        // onOpenChange={(open) => setIsOpen({
+                        //     ...isOpen,
+                        //     [section.id]: open
+                        // })}
                     >
                         <SelectTrigger
                             className="p-4 focus-visible:outline-0 shadow-none focus-visible:ring-transparent rounded-md h-[3.375rem] font-normal leading-[21px] text-[14px] placeholder:text-grey250 text-black500 border border-solid border-neutral650 flex justify-between items-center"
                         >
                             <SelectValue placeholder="Select relationship"/>
-                            {isOpen[section.id] ? (
-                                <MdKeyboardArrowUp
-                                    className="h-5 w-5 text-neutral950"/>
-                            ) : (
-                                <MdKeyboardArrowDown
-                                    className="h-5 w-5 text-neutral950"/>
-                            )}
+                            {/*{isOpen[section.id] ? (*/}
+                            {/*    <MdKeyboardArrowUp*/}
+                            {/*        className="h-5 w-5 text-neutral950"/>*/}
+                            {/*) : (*/}
+                            {/*    <MdKeyboardArrowDown*/}
+                            {/*        className="h-5 w-5 text-neutral950"/>*/}
+                            {/*)}*/}
                         </SelectTrigger>
                         <SelectContent>
                             <SelectItem value="father">Father</SelectItem>
@@ -129,52 +157,71 @@ const Individual = () => {
                 </div>
             </div>
             <div className="space-y-2">
-                <Label htmlFor={`ownership-${section.id}`}>Ownership / Share
+                <Label
+                    // htmlFor={`ownership-${section.id}`}
+                >Ownership / Share
                     (%)</Label>
                 <Input
-                    id={`ownership-${section.id}`}
+                    // id={`ownership-${section.id}`}
                     name="ownership"
                     type="number"
                     max="100"
                     placeholder="0"
-                    value={section.individualOwnership || ""}
-                    onChange={(e) => handleInputChange(section.id, "individualOwnership", e.target.value)}
+                    value={individualData.ownership || ""}
+                    onChange={(e) => {
+                        const value = e.target.value.replace(/^rc/i, 'RC');
+                        const isInputValid = validateEntityOwnership(e.target.value)
+                        if (typeof  isInputValid === "string") {
+                            setIndividualData((prevState) => (
+                                { ...prevState, ['entityError']: 'ownership' }
+                            ))
+                            setIndividualData((prevState) => (
+                                { ...prevState, ['errorMessage']: isInputValid }
+                            ))
+                        }
+                        else{
+                            setIndividualData((prevState) => (
+                                { ...prevState, ['errorMessage']: '' }
+                            ))
+                            handleInputChange('ownership', value)
+                        }
+                    }}
                     className="p-4 focus-visible:outline-0 shadow-none focus-visible:ring-transparent rounded-md w-full md:w-[47%] h-[3.375rem] font-normal leading-[21px] text-[14px] placeholder:text-grey250 text-black500 border border-solid border-neutral650"
                 />
-                {section.errors?.ownership && (
-                    <p className="text-red-500 text-sm">{section.errors.ownership}</p>
+                {individualData.entityError ==='ownership' && (
+                    <p className="text-red-500 text-sm">{individualData.errorMessage}</p>
                 )}
             </div>
-            <div className="space-y-4">
-                <Label>Proof of beneficial ownership</Label>
-                <div className="flex gap-3">
-                    {proofOptions.map((option) => (
-                        <label
-                            key={option.id}
-                            className={`rounded-[20px] px-3 py-2 text-[14px] leading-[150%] font-medium bg-blue50 hover:bg-blue50 cursor-pointer ${
-                                section.proofType === option.id
-                                    ? "border border-meedlBlue bg-blue50 text-meedlBlue"
-                                    : "text-black300"
-                            }`}
-                        >
-                            <input
-                                type="radio"
-                                name={`proofType-${section.id}`}
-                                value={option.id}
-                                checked={section.proofType === option.id}
-                                onChange={(e) => handleProofTypeChange(section.id, e.target.value)}
-                                className="hidden"
-                            />
-                            <span className="text-sm">{option.label}</span>
-                        </label>
-                    ))}
-                </div>
-            </div>
-            <FileUpload
-                handleDrop={(e) => handleDrop(section.id, e)}
-                handleDragOver={(e) => e.preventDefault()}
-                setUploadedImageUrl={(url) => handleSetUploadedImageUrl(section.id, url)}
-            />
+            {/*<div className="space-y-4">*/}
+            {/*    <Label>Proof of beneficial ownership</Label>*/}
+            {/*    <div className="flex gap-3">*/}
+            {/*        {proofOptions.map((option) => (*/}
+            {/*            <label*/}
+            {/*                key={option.id}*/}
+            {/*                className={`rounded-[20px] px-3 py-2 text-[14px] leading-[150%] font-medium bg-blue50 hover:bg-blue50 cursor-pointer ${*/}
+            {/*                    section.proofType === option.id*/}
+            {/*                        ? "border border-meedlBlue bg-blue50 text-meedlBlue"*/}
+            {/*                        : "text-black300"*/}
+            {/*                }`}*/}
+            {/*            >*/}
+            {/*                <input*/}
+            {/*                    type="radio"*/}
+            {/*                    name={`proofType-${section.id}`}*/}
+            {/*                    value={option.id}*/}
+            {/*                    checked={section.proofType === option.id}*/}
+            {/*                    onChange={(e) => handleProofTypeChange(section.id, e.target.value)}*/}
+            {/*                    className="hidden"*/}
+            {/*                />*/}
+            {/*                <span className="text-sm">{option.label}</span>*/}
+            {/*            </label>*/}
+            {/*        ))}*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            {/*<FileUpload*/}
+            {/*    handleDrop={(e) => handleDrop(section.id, e)}*/}
+            {/*    handleDragOver={(e) => e.preventDefault()}*/}
+            {/*    setUploadedImageUrl={(url) => handleSetUploadedImageUrl(section.id, url)}*/}
+            {/*/>*/}
         </div>
 
     );
