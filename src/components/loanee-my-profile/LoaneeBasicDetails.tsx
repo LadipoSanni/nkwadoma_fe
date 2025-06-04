78765b0be669f53d103cd8c959e2189bd638c860
a-mariam <mariiam22222@gmail.com>
2025-06-04 11:54:43 +0100

    working on pagr

--- src/components/loanee-my-profile/LoaneeBasicDetails.tsx
+++ src/components/loanee-my-profile/LoaneeBasicDetails.tsx
@@ -10,103 +10,54 @@ interface props{
 
 
 const LoaneeBasicDetails = ({data}: props) => {
-    const basicData = data?.userIdentity
 
-    const basicDetails = [
-        {label: 'Gender', value: `${basicData?.gender ? basicData?.gender : 'Not provided'}`},
-        {label: 'Date of birth', value: `${basicData?.dateOfBirth ? basicData?.dateOfBirth : 'Not provided'}`},
-        {label: 'Marital status', value: `${basicData?.maritalStatus ? basicData?.maritalStatus : 'Not provided'}`},
-        {label: 'Nationality', value: `${basicData?.nationality ? basicData?.nationality : 'Not provided'}`},
-        {label: 'State of origin ', value: `${basicData?.stateOfOrigin ? basicData?.stateOfOrigin : 'Not provided'}`},
-        {label: 'State of residence', value: `${basicData?.stateOfResidence ? basicData?.stateOfResidence : 'Not provided'}`},
-        {label: 'Residential address', value: `${basicData?.residentialAddress ? basicData?.residentialAddress : 'Not provided'}`},
-        {label: 'Phone number', value: `${basicData?.phoneNumber ? basicData?.phoneNumber : 'Not provided'}`},
-        {label: 'Alternate residential address', value: `${basicData?.residentialAddress ? basicData?.residentialAddress : 'Not provided'}`},
-        // {label: 'Alternate email address', value: ''},
-        {label: 'Alternate email address', value: `${basicData?.alternateEmail? basicData?.alternateEmail : 'Not provided'}`},
-        {label: 'Next of kin full name', value: `${basicData?.nextOfKinResponse}`},
-        {label: 'Next of kin phone number', value: ''},
-        {label: 'Next of kin residential address', value: ''},
-        {label: 'Next of kin relationship ', value: ''},
-        {label: 'Highest level of education ', value: ''},
-        {label: 'Institution name ', value: data?.institutionName},
-        {label: 'Program of study ', value: data?.programOfStudy},
-
-
-    ]
 
 
     return (
         <div
             id={'loaneeBasicDetails'}
             data-testid={'loaneeBasicDetails'}
-            className={`md:max-h-fit hidden  md:w-[45%] pb-6 pt-6 md:flex lg:flex sm:hidden    sm:w-full w-full  `}
+            className={`md:max-h-fit hidden bg-red-500  md:w-[45%]  md:grid lg:grid px-4  sm:hidden    sm:w-full w-full  `}
         >
-            <Tabs defaultValue={'bioDetails'} className={` px-4 w-full  `}>
-                <TabsList className="grid w-fit px-0 pb-2  grid-cols-2">
-                    <TabsTrigger id={'bioDetails'} className={`w-fit ${inter.className} text-[14px] text-[#6A6B6A] data-[state=active]:text-[#212221] data-[state=active]:border data-[state=active]:border-grey-200 data-[state=active]:${inter500.className} `} value="bioDetails">Bio details</TabsTrigger>
-                    <TabsTrigger id={'document'} className={`w-fit ${inter.className} text-[14px] text-[#6A6B6A] data-[state=active]:text-[#212221] data-[state=active]:border data-[state=active]:border-grey-200 data-[state=active]:${inter500.className} `} value="documents">Document</TabsTrigger>
-                </TabsList>
+                <div id={'document'} className={`w-fit ${inter500.className} text-[18px]  py-4 h-fit  text-[#212221] ${inter500.className} `} >Document</div>
                 <div
-                    className={` md:max-h-[65vh] w-full  ${styles.container}`}
+                    className={` md:max-h-[30vh] w-full bg-purple-100 ${styles.container}`}
                 >
-                    <TabsContent value={'bioDetails'} className={` md:w-full pt-3 w-full   `}>
-                        <div className={` bg-grey105  w-full rounded-md  `}>
-                            {basicDetails?.map((item, index) => (
-                                <li key={"key" + index} className={'p-4  grid gap-9 rounded-md'}>
-                                    <div
-                                        className={'md:flex md:justify-between md:items-center md:gap-0 grid gap-3 '}>
-                                        <div
-                                            id={'name:'+item.label}
-                                            className={`  ${inter.className} break-all md:max-w-[40%] text-black300 text-[14px] `}>{item.label}</div>
-                                        <div
-                                            id={'name:'+item.value}
-                                            className={` ${inter.className} break-all md:max-w-[50%]   text-black500 text-[14px] `}> {item.value ? item.value : 'Not provided'}</div>
-                                    </div>
-                                </li>
-                            ))
-                            }
-                        </div>
+                     {/*<div className={` h-fit  w-full rounded-md px-3 pt-5 pb-3  bg-[#F6F6F6] `}>*/}
+                     {/*       <div className={` h-[8rem] bg-white  w-full rounded-md `}>*/}
 
-                    </TabsContent>
-                    <TabsContent value={'documents'} className={` grid md:flex pt-3 gap-5  md:flex-col-2  `}>
-                        <div className={` h-fit  w-full rounded-md px-3 pt-5 pb-3  bg-[#F6F6F6] `}>
-                            <div className={` h-[8rem] bg-white  w-full rounded-md `}>
+                     {/*       </div>*/}
 
-                            </div>
+                     {/*       <div className={`flex py-4 gap-2 `}>*/}
+                     {/*           <Image*/}
+                     {/*               src={'/MyMandateLogo.png'}*/}
+                     {/*               alt="circle"*/}
+                     {/*               width={20}*/}
+                     {/*               height={29}*/}
+                     {/*               data-testid="circle-image"*/}
+                     {/*               loading="lazy"*/}
+                     {/*           />*/}
+                     {/*           <span className={` ${inter.className} text-[#212221] text-[14px] `}>mymandate25.pdf</span>*/}
+                     {/*       </div>*/}
+                     {/*   </div>*/}
+                     {/*<div className={` h-fit  w-full rounded-md px-3 pt-5 pb-3   bg-[#F6F6F6] `}>*/}
+                     {/*       <div className={` h-[8rem] bg-white  w-full rounded-md `}>*/}
 
-                            <div className={`flex py-4 gap-2 `}>
-                                <Image
-                                    src={'/MyMandateLogo.png'}
-                                    alt="circle"
-                                    width={20}
-                                    height={29}
-                                    data-testid="circle-image"
-                                    loading="lazy"
-                                />
-                                <span className={` ${inter.className} text-[#212221] text-[14px] `}>mymandate25.pdf</span>
-                            </div>
-                        </div>
-                        <div className={` h-fit  w-full rounded-md px-3 pt-5 pb-3   bg-[#F6F6F6] `}>
-                            <div className={` h-[8rem] bg-white  w-full rounded-md `}>
-
-                            </div>
-                            <div className={`flex py-4  gap-2 `}>
-                                <Image
-                                    src={'/MyMandateLogo.png'}
-                                    alt="circle"
-                                    width={20}
-                                    height={29}
-                                    // className="object-right-bottom flex  "
-                                    data-testid="circle-image"
-                                    loading="lazy"
-                                />
-                                <span className={` ${inter.className} text-[#212221] text-[14px] `}>mymandate25.pdf</span>
-                            </div>
-                        </div>
-                    </TabsContent>
+                     {/*       </div>*/}
+                     {/*       <div className={`flex py-4  gap-2 `}>*/}
+                     {/*           <Image*/}
+                     {/*               src={'/MyMandateLogo.png'}*/}
+                     {/*               alt="circle"*/}
+                     {/*               width={20}*/}
+                     {/*               height={29}*/}
+                     {/*               // className="object-right-bottom flex  "*/}
+                     {/*               data-testid="circle-image"*/}
+                     {/*               loading="lazy"*/}
+                     {/*           />*/}
+                     {/*           <span className={` ${inter.className} text-[#212221] text-[14px] `}>mymandate25.pdf</span>*/}
+                     {/*       </div>*/}
+                     {/*   </div>*/}
                 </div>
-            </Tabs>
         </div>
     );
 };
--- src/components/loanee-my-profile/LoaneeLoanDetails.tsx
+++ src/components/loanee-my-profile/LoaneeLoanDetails.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
-import {inter500, inter} from '@/app/fonts'
+import {inter500, inter,} from '@/app/fonts'
 import Details from './Details';
 import styles from './index.module.css'
 // import BasicDetails from "@/components/loanee-my-profile/BasicDetails";
@@ -17,14 +16,9 @@ const LoaneeLoanDetails = ({data}:props ) => {
     return (
         <div id={'loaneeLoanDetails'}
              data-testid={'loaneeLoanDetails'}
-             className={`md:max-h-fit md:w-[55%] sm:w-[100%] w-[100%] pt-6 pb-6 md:border-r md:border-r-grey-200] `}>
-            <Tabs defaultValue={'loanInformation'} className={` pl-3 `}>
-                <TabsList className={`grid  ${styles.details} max-w-[100%] w-[100%] sm:w-full lg:w-fit md:w-fit px-0 pb-2 lg:grid-cols-2 grid-cols-4   md:grid-cols-2 `}>
-                    <TabsTrigger id={'loanInfo'} className={`w-fit ${inter.className} text-[14px] text-[#6A6B6A] data-[state=active]:text-[#212221] data-[state=active]:border data-[state=active]:border-grey-200 data-[state=active]:${inter500.className} `} value="loanInformation">Loan information</TabsTrigger>
-                    <TabsTrigger id={'document'} className={`w-fit ${inter.className} text-[14px] text-[#6A6B6A]  md:hidden lg:hidden  flex   data-[state=active]:text-[#212221] data-[state=active]:border data-[state=active]:border-grey-200 data-[state=active]:${inter500.className} `} value="document">Document</TabsTrigger>
-                </TabsList>
-                <div className={` md:max-h-[65vh] w-full  ${styles.container}`}>
-                    <TabsContent className={` pr-6 pt-3 grid gap-4`} value="loanInformation">
+             className={`md:max-h-fit md:w-[55%] sm:w-[100%] w-[100%] px-4 pb-6 md:border-r md:border-r-grey-200] `}>
+                <div id={'loanInfo'} className={`w-fit text-[18px] py-4 h-fit   text-[#212221] ${inter500.className} `}>Loan information</div>
+                <div className={` md:max-h-[65vh] grid gap-4 w-full  ${styles.container}`}>
                         <Details id={'loanAmount'}    maxWidth={'100%'} name={'Loan amount'} value={data?.loanAmount} valueType={'currency'} />
                         <div className={` md:flex md:gap-4  grid gap-4  w-full  `}>
                             <Details id={'amountOutstanding'}    maxWidth={'100%'} name={'Amount outstanding'} value={0} valueType={'currency'} />
@@ -38,14 +32,10 @@ const LoaneeLoanDetails = ({data}:props ) => {
                             <Details id={'deptPercentage'}    maxWidth={'100%'} name={'Dept percentage'} value={0} valueType={'percentage'} />
                             <Details id={'repaymentPercentage'}    maxWidth={'100%'} name={'Repayment percentage'} value={0} valueType={'percentage'}  />
                         </div>
-                    </TabsContent>
-
-                    <TabsContent value={'document'} className={` pr-6 pt-3 grid gap-4`} >
+                    <div className={` pr-6 flex  pt-3 md:hidden lg:hidden grid gap-4`} >
                         <Document/>
-                    </TabsContent>
+                    </div>
                 </div>
-            </Tabs>
-
         </div>
     );
 };
