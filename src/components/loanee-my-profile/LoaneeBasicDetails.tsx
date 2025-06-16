cbd137330683fa37d8474674da4018ed415d02ec
a-mariam <mariiam22222@gmail.com>
2025-06-16 20:38:35 +0100

    added repayment to loanee my loan tab

--- src/components/loanee-my-profile/LoaneeBasicDetails.tsx
+++ src/components/loanee-my-profile/LoaneeBasicDetails.tsx
@@ -49,29 +49,31 @@ const LoaneeBasicDetails = ({data, isLoading}: props) => {
         >
             {userRole === 'LOANEE'?
                 <div>
-                  <span id={'document'} className={`w-fit ${inter500.className} text-[18px]  py-4 h-fit  text-[#212221]  `} >Document</span>
-                <div
+                  <div id={'document'} className={`w-fit ${inter500.className} text-[18px]  py-4 h-fit   text-[#212221]  `} >Document</div>
+                  <div
                     className={` md:max-h-fit grid  grid-cols-2 w-full   `}
-                >
+                   >
 
-                        <div className={` h-fit  w-full rounded-md px-3 pt-5 pb-3   bg-[#F6F6F6] `}>
-                        <div className={` h-[8rem] w-full bg-white   rounded-md `}>
+                    <p>No Document available</p>
 
-                        </div>
-                        <div className={`flex py-4  gap-2 `}>
-                            <Image
-                                src={'/MyMandateLogo.png'}
-                                alt="circle"
-                                width={20}
-                                height={29}
-                                // className="object-right-bottom flex  "
-                                data-testid="circle-image"
-                                loading="lazy"
-                            />
-                            <span className={` ${inter.className} text-[#212221] text-[14px] `}>mymandate25.pdf</span>
-                        </div>
-                       </div>
-                </div>
+                    {/* <div className={` h-fit  w-full rounded-md px-3 pt-5 pb-3   bg-[#F6F6F6] `}>*/}
+                       {/* <div className={` h-[8rem] w-full bg-white   rounded-md `}>*/}
+
+                       {/* </div>*/}
+                       {/* <div className={`flex py-4  gap-2 `}>*/}
+                       {/*     <Image*/}
+                       {/*         src={'/MyMandateLogo.png'}*/}
+                       {/*         alt="circle"*/}
+                       {/*         width={20}*/}
+                       {/*         height={29}*/}
+                       {/*         // className="object-right-bottom flex  "*/}
+                       {/*         data-testid="circle-image"*/}
+                       {/*         loading="lazy"*/}
+                       {/*     />*/}
+                       {/*     <span className={` ${inter.className} text-[#212221] text-[14px] `}>mymandate25.pdf</span>*/}
+                       {/* </div>*/}
+                       {/*</div>*/}
+                   </div>
                 </div>
                 :
                 <Tabs defaultValue={'bioDetails'} className={` px-4 py-4  w-full  `}>
