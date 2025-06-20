5f49544dd5d04e8f7b62e4a5e77baf241d4caaac
a-mariam <mariiam22222@gmail.com>
2025-06-20 10:03:08 +0100

    removed unuse code

--- src/service/action.ts
+++ src/service/action.ts
@@ -4,33 +4,20 @@ import axios from 'axios';
 
 
 export async  function refreshTokenAction (token?: string) {
-    // const {storedRefreshToken} = getUserDetails();
-    // console.log('stored refreshToken', storedRefreshToken);
-    // I think you can use the base url here;
     const url = `https://api-systest.meedl.africa/api/v1/auth/refresh-token`;
-    // method: "POST",
-    //     body: JSON.stringify({refreshToken: token}),
-    //     headers: {
-    //     "Content-Type": "application/json",
-    // },
-    // try{
          await axios.post(url, {refreshToken: token})
              .then((response ) => {
                  const json =  response;
-                 // console.log('json', json);
-                 setItemSessionStorage("access_token", json?.data?.access_token);
-                 setItemSessionStorage("refresh_token",json?.data?.refresh_token)
+                 setItemSessionStorage("access_token", json?.data?.data?.access_token);
+                 setItemSessionStorage("refresh_token",json?.data?.data?.refresh_token)
              }).catch((error) => {
              console.log('error', error);
          })
 
-    // }catch (error){
-    //     console.log('error', error)
-    // }
+
 }
 
 export async function getToken () {
-    // const { storedAccessToken } = getUserDetails();
     const storedRefreshToken = getItemSessionStorage("refresh_token")
     const  storedAccessToken  = getItemSessionStorage('access_token');
 
