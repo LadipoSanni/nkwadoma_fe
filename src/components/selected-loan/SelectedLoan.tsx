ec067985bffdfac673edba35f47d9ce67d82e194
ikjames5454 <ikennajames03@gmail.com>
2025-07-11 08:11:52 +0100

    i updated the upload file component to collect drag and drop

--- src/components/selected-loan/SelectedLoan.tsx
+++ src/components/selected-loan/SelectedLoan.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React,{useEffect} from 'react';
 import SelectedLoanHeader from "@/components/selected-loan/SelectedLoanHeader";
 import styles from "./SelectedLoan.module.css"
@@ -15,7 +16,7 @@ const SelectedLoan = ({children}: props) => {
         store.dispatch(resetAll())
         store.dispatch(clearSaveCreateInvestmentField())
     },[])
-    
+
     return (
         <div>
             <div id={'selectedLoanLayout'} className={` ${styles.layout}  grid md:grid  absolute gap-1 h-[100%] w-[100%] md:h-[100%] md:w-[100%] pr-2 md:pr-0`}>
