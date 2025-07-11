e709aa83bd1f3d9149de3966ea9c28b17969fff3
ikjames5454 <ikennajames03@gmail.com>
2025-07-11 08:06:55 +0100

    i updated the upload file component to collect drag and drop

--- src/components/selected-loan/SelectedLoan.tsx
+++ src/components/selected-loan/SelectedLoan.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import SelectedLoanHeader from "@/components/selected-loan/SelectedLoanHeader";
 import styles from "./SelectedLoan.module.css"
+import { resetAll,clearSaveCreateInvestmentField} from '@/redux/slice/vehicle/vehicle';
+import {store} from "@/redux/store";
 
 interface props  {
     children: React.ReactNode;
 }
 
+
 const SelectedLoan = ({children}: props) => {
+
+    useEffect(() => {
+        store.dispatch(resetAll())
+        store.dispatch(clearSaveCreateInvestmentField())
+    },[])
+    
     return (
         <div>
             <div id={'selectedLoanLayout'} className={` ${styles.layout}  grid md:grid  absolute gap-1 h-[100%] w-[100%] md:h-[100%] md:w-[100%] pr-2 md:pr-0`}>
