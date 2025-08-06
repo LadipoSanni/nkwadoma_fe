653a691f415de5613e929c92c5a4d9cafa0e33dc
Mariam Ambali <mariiam22222@gmail.com>
2025-08-06 10:45:44 +0100

    added test case for the total cards

--- src/components/loanee-my-profile/Details.tsx
+++ src/components/loanee-my-profile/Details.tsx
@@ -6,17 +6,17 @@ interface Props {
     id: string;
     name: string;
     value?: string| number;
-    maxWidth: string;
+    maxWidth?: string;
     valueType:'percentage'| 'digit'| 'currency' | 'tenor',
     showAsWholeNumber?: boolean,
     isLoading?: boolean,
     sx?:string,
 }
 
-const Details = ({id, name, value,sx, maxWidth, valueType,showAsWholeNumber, isLoading}: Props) => {
+const Details = ({id, name, value,sx, valueType,showAsWholeNumber, isLoading}: Props) => {
     return (
-        <div id={id} data-testid={id} className={`md:border bg-white grid max-w-[${maxWidth}] rounded-md px-3 py-4 w-[${maxWidth}] md:border-grey-200 border border-grey-200 `}>
-           <div className={`  ${isLoading ? 'animate-pulse bg-[#f4f4f5] h-[6rem]  ' : 'bg-[#f9f9f9]'} px-4 py-4 grid gap-4  ${sx ? `${sx}` : `w-full `}`}>
+        <div id={id} data-testid={id} className={`md:border bg-white grid w-fit   rounded-md px-3 py-4  md:border-grey-200 border border-grey-200 `}>
+           <div className={`  ${isLoading ? 'animate-pulse bg-[#f4f4f5] h-[6rem]  ' : 'bg-[#f9f9f9]'} ${styles.detailsWidth}    px-4 py-4 grid gap-4  ${sx ? `${sx}` : `w-full `}`}>
                <span id={'detailName:'+ name} className={` ${inter.className} text-[#6A6B6A] text-[14px] ${isLoading ? 'hidden ' : ''} `}>{name}</span>
                <span id={'detailsValue:' + value} className={` ${cabinetGroteskBold.className} ${isLoading ? 'hidden ' : ''}  text-[32px] ${styles.details} max-w-[100%]  text-meedlBlue  `}>
                    {valueType === 'percentage' ? (`${value}%`) : valueType === 'digit' ? (`${formateDigits(Number(value))}`) : valueType === 'tenor' ? (`${Number(value) > 1 ? `${value} months` : `${value} month` } `) : (`${formatAmount(Number(value),showAsWholeNumber)}`)  }
--- src/features/admin-loanee-view/view-all-loanee-overview.tsx
+++ src/features/admin-loanee-view/view-all-loanee-overview.tsx
@@ -7,17 +7,16 @@ const ViewAllLoaneeOverview = () => {
         <div
             id={'viewAllLoaneeOverviewContainer'}
             data-testid={'viewAllLoaneeOverviewContainer'}
-            className={`w-full h-full px-6 py-6   `}
+            className={`w-full h-full grid gap-4 px-4 py-6   `}
         >
             <div
                 id={'viewAllLoaneeTotalOverviewContainer'}
                 data-testid={'viewAllLoaneeTotalOverviewContainer'}
                 className={` w-full h-full flex gap-4  ${styles.overviewCard}   `}
             >
-                <Details isLoading={false} sx={`  w-[20em] md:w-full  `} name={'No. of loanees'} valueType={'digit'}  id={'totalNumberOfLoanees'} showAsWholeNumber={false}   maxWidth={'100%'}  value={'10000'}/>
-                <Details isLoading={false} sx={` w-[20em] md:w-full `} id={'loaneeTotalLoaneOutstanding'} showAsWholeNumber={false}   maxWidth={'100%'} name={'Total amount outstanding '} value={''} valueType={'currency'}  />
-                <Details isLoading={false} sx={` w-[20em] md:w-full `} id={'loaneeTotalAmountRepaid'} showAsWholeNumber={false}   maxWidth={'100%'} name={'Total amount repaid '} value={''} valueType={'currency'}  />
-
+                <Details isLoading={false} sx={`  w-[20em] md:w-full  `} name={'No. of loanees'} valueType={'digit'}  id={'totalNumberOfLoanees'} showAsWholeNumber={false}  value={'0'}/>
+                <Details isLoading={false} sx={` w-[20em] md:w-full `} id={'historicalDept'} showAsWholeNumber={false}    name={'Historical debt'} value={''} valueType={'currency'}  />
+                <Details isLoading={false} sx={` w-[20em] md:w-full `} id={'totalOutstanding'} showAsWholeNumber={false}    name={'Total outstanding'} value={''} valueType={'currency'}  />
             </div>
         </div>
     );
