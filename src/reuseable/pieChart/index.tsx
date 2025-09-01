fd89343400085b762e6f18b1b8b25a07bb7f389a
a-mariam <mariiam22222@gmail.com>
2025-09-01 11:44:45 +0100

    working on pie responsive ness

--- src/reuseable/pieChart/index.tsx
+++ src/reuseable/pieChart/index.tsx
@@ -1,11 +1,6 @@
 "use client"
 
 import { Pie, PieChart } from "recharts"
-
-import {
-    Card,
-    CardContent,
-} from "@/components/ui/card"
 import {
     ChartConfig,
     ChartContainer,
@@ -47,32 +42,25 @@ export function PieCharts({title, chartData, dataKey}: PieChartProps) {
        <div className={`grid border max-h-fit h-full overflow-y-hidden py-3 px-2 rounded-md  border-[#D7D7D7] bg-white w-full `}>
                <div className={` w-full rounded-md overflow-y-hidden py-2 px-2  bg-[#F9F9F9] `}>
                    <div id={'chartDescription'} data-testid={'chartDescription'} className={`  py-3  md:text-[24px] text-black ${cabinetGroteskMediumBold.className} `}>{title}</div>
-                   <div className={` flex  w-full h-full   `}>
-                       <Card
-                           id={'pieChart'+ dataKey}
-                           data-testid={'pieChart'+ dataKey}
-                           className="flex w-fit bg-[#F9F9F9] flex-col">
-                           <CardContent className="flex-1 flex w-fit   items-start pb-0">
-                               <ChartContainer
-                                   config={chartConfig}
-                                   className="mx-auto aspect-square   min-h-[300px] md:min-h-[300px]"
-                               >
-                                   <PieChart id={'pieChart'} className={' '} data-testid={'pieChart'}>
-                                       <Pie data={chartData} dataKey="visitors" />
-                                   </PieChart>
-                               </ChartContainer>
-                           </CardContent>
-                       </Card>
-                       <div id={'chartDataDescription'} data-testid={'chartDataDescription'} className={` bg-red-100  grid gap-2  w-full  mt-auto mb-auto `}>
+                   <div className={` flex justify-between   w-full h-full   `}>
+                       <ChartContainer
+                           config={chartConfig}
+                           className="mx-auto aspect-square flex justify-start w-fit    max-h-[300px] md:max-h-[300px]"
+                       >
+                           <PieChart id={'pieChart'} className={' self-start  '} data-testid={'pieChart'}>
+                              <Pie data={chartData} dataKey="visitors" />
+                           </PieChart>
+                       </ChartContainer>
+                       <div id={'chartDataDescription'} data-testid={'chartDataDescription'} className={` bg-red-100  grid gap-2 self-end w-[50%]  mt-auto mb-auto `}>
                            {chartData?.map((item, index) => (
-                               <div className={`  w-full  flex justify-between `} key={index}>
-                                   <div className={` flex  gap-2 `}>
+                               <div className={`  w-full  flex justify-between `} key={'key'+index}>
+                                   <div className={` flex w-fit gap-2 `}>
                                        <div
                                            style={{backgroundColor: item?.fill}}
-                                           className={` aspect-square rounded-full mt-auto mb-auto w-2 h-2  md:w-2.5 md:h-2.5  `}></div>
-                                       <p className={` text-[12px] text-[#6A6B6A]  ${inter.className} `}>{item?.browser}</p>
+                                           className={` aspect-square rounded-full bg-red-500 mt-auto mb-auto w-2 h-2  md:w-2.5 md:h-2.5  `}></div>
+                                       <p className={` text-[12px]  text-[#6A6B6A]  ${inter.className} `}>{item?.browser}</p>
                                    </div>
-                                   <span className={`text-black text-[12px] ${inter500.className}`}>{item?.visitors} %</span>
+                                   <span className={` text-black w-fit text-[12px] ${inter500.className}`}>{item?.visitors} %</span>
                                </div>
                            ))}
                        </div>
