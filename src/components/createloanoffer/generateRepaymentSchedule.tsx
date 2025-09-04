e1a883c6e47ade505dbc86a9069c89c3c467c4a6
a-mariam <mariiam22222@gmail.com>
2025-09-04 22:11:23 +0100

    started working on generate loan schedule summation ui

--- src/components/createloanoffer/generateRepaymentSchedule.tsx
+++ src/components/createloanoffer/generateRepaymentSchedule.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import {cabinetGroteskMediumBold600, inter700} from "@/app/fonts";
+import Details from "@/components/loanee-my-profile/Details";
 
 const GenerateRepaymentSchedule = () => {
     return (
         <div
+            id={'generateRepaymentScheduleComponent'}
+            data-testid={'generateRepaymentScheduleComponent'}
+            className={` w-full grid gap-6  h-full px-2 py-2 `}
         >
-            
+            <div className={`w-full h-fit flex justify-between `}>
+                <h4 className={` ${cabinetGroteskMediumBold600.className} text-[#101828] text-[28px] text-  `}>Generate repayment schedule</h4>
+                <button id={'confirmRepaymentButton'} data-tesid={'confirmRepaymentButton'} className={` rounded-md text-[14px] ${inter700.className}  w-fit h-fit py-1 px-2 bg-meedlBlue text-white  `}>Confirm repayment schedule</button>
+            </div>
+            <div className={` flex `}>
+                <div className={` w-[50%] `}>
+                    <Details showIcon={true} isLoading={false} sx={` w-full md:w-[100%] `} id={'total'} showAsWholeNumber={false}    name={'Sum total'} value={'200000'} valueType={'currency'}  />
+                </div>
+                <div className={`w-[50%] `}>
+                    <Details showIcon={true} isLoading={false} sx={` w-full md:w-[100%] `} id={'totalamountEarned'} showAsWholeNumber={false}    name={'Tenor'} value={'200000'} valueType={'tenor'}  />
+                    <Details showIcon={true} isLoading={false} sx={` w-full md:w-[100%] `} id={'totalamountEarned'} showAsWholeNumber={false}    name={'Tenor'} value={'200000'} valueType={'tenor'}  />
+                </div>
+            </div>
         </div>
     );
 };
