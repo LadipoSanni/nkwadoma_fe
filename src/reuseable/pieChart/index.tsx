4384379f3ea728f025bc4bcc5acc79a457e716f6
a-mariam <mariiam22222@gmail.com>
2025-09-05 14:49:33 +0100

    worked on pie chart web responsiveness still working small phone responsiveness

--- src/reuseable/pieChart/index.tsx
+++ src/reuseable/pieChart/index.tsx
@@ -6,7 +6,7 @@ import {
     ChartContainer,
 } from "@/components/ui/chart"
 import {cabinetGroteskMediumBold, inter, inter500} from "@/app/fonts";
-
+import styles from './index.module.css'
 
 interface PieChartProps {
     title: string,
@@ -47,31 +47,34 @@ export function PieCharts({title, chartData, dataKey, isLoading}: PieChartProps)
     const chartConfig = generateChartConfig(chartData) satisfies  ChartConfig;
 
 
+    console.log('total percentage', totalPercentage);
 
     return (
        <div className={`grid border max-h-fit h-full overflow-y-hidden py-3 px-2 rounded-md  border-[#D7D7D7] bg-white w-full `}>
                <div className={` w-full rounded-md overflow-y-hidden py-2 px-2  bg-[#F9F9F9] `}>
                    <div id={'chartDescription'} data-testid={'chartDescription'} className={`  py-3  md:text-[24px] text-black ${cabinetGroteskMediumBold.className} `}>{title}</div>
-                   <div className={` flex flex-wrap bg-red-500 py-  ${totalPercentage === 0 || !totalPercentage ? ' h-[15rem] md:h-full  ' : 'h-fit '}  w-full    `}>
+                   <div className={` ${styles.pieChartContainer}  flex  justify-between py-6  ${totalPercentage === 0 || !totalPercentage ? ' h-fit  md:h-fit   ' : 'h-fit '}  w-full    `}>
                        {totalPercentage === 0 || !totalPercentage  || isLoading ?
-                            <div className={` bg-red-200    aspect-square `}>
+                            <div className={` ${styles.pieChartChart}  mr-2    aspect-square `}>
                                 <div
-                                    className={` bg-blue550 ${isLoading ? 'animate-pulse' : ''}  aspect-square rounded-full   max-h-[300px] md:max-h-[300px]  `}
+                                    id={'emptyStateCircle'}
+                                    data-testid={'emptyStateCircle'}
+                                    className={` bg-[#ECECEC] ${isLoading ? 'animate-pulse' : ''}  h-full w-full   rounded-full    `}
                                 >
                                 </div>
-                            </div>
+                             </div>
                            :
                            <ChartContainer
                                config={chartConfig}
-                               className="mx-auto aspect-square flex justify-start w-fit    max-h-[300px] md:max-h-[300px]"
+                               className="mx-auto flex justify-start w-full h-full  aspect-square "
                            >
-                               <PieChart id={'pieChart'} className={' self-start  '} data-testid={'pieChart'}>
-                                   <Pie data={chartData} dataKey="visitors" />
+                               <PieChart id={'pieChart'} data-testid={'pieChart'} >
+                                   <Pie data={chartData} dataKey="visitors" nameKey="browser" />
                                </PieChart>
                            </ChartContainer>
 
                        }
-                       <div id={'chartDataDescription'} data-testid={'chartDataDescription'} className={`   grid gap-2 self-end w-[50%]  mt-auto mb-auto `}>
+                       <div id={'chartDataDescription'} data-testid={'chartDataDescription'} className={` ${styles.pieChartChart}  grid gap-2 self-end   mt-auto mb-auto `}>
                            {chartData?.map((item, index) => (
                                <div className={`  w-full   flex justify-between `} key={'key'+index}>
                                    {isLoading ?
@@ -79,7 +82,7 @@ export function PieCharts({title, chartData, dataKey, isLoading}: PieChartProps)
                                    </div>
                                    :
                                        <div className={` w-full  flex justify-between  `}>
-                                           <div className={` flex w-fit gap-2 `}>
+                                           <div className={` flex w-fit gap-1 `}>
                                                <div
                                                    style={{backgroundColor: item?.fill}}
                                                    className={` aspect-square rounded-full  mt-auto mb-auto w-2 h-2  md:w-2.5 md:h-2.5  `}></div>
