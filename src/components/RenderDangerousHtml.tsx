f004054b354cc447bfc7c6ad8d11347911a733e1
a-mariam <mariiam22222@gmail.com>
2025-10-12 22:17:40 +0100

    worked on cohort details fonts

--- src/components/RenderDangerousHtml.tsx
+++ src/components/RenderDangerousHtml.tsx
@@ -2,12 +2,12 @@
 import styles from './index.module.css'
 import DOMPurify from "dompurify";
 
-export default function QuillRenderer({ html }: { html: string }) {
+export default function QuillRenderer({ html,sx }: { html: string, sx?:string }) {
     // Clean + allow only safe tags
     const cleanHtml = DOMPurify.sanitize(html, {
         ALLOWED_TAGS: ["p", "ol", "ul", "li", "strong", "em", "u", "br", "span"],
         ALLOWED_ATTR: ["class"],
     });
 
-    return <div className={`${styles.quillContent}`} dangerouslySetInnerHTML={{ __html: cleanHtml }} />;
+    return <div className={`${styles.quillContent} ${sx}`} dangerouslySetInnerHTML={{ __html: cleanHtml }} />;
 }
