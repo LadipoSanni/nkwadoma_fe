55f7ce486fb40fc96a044b88309157a128b47dbb
a-mariam <mariiam22222@gmail.com>
2025-11-02 11:45:14 +0100

    fixed back button

--- src/components/loanee-my-profile/OrganizationViewLoaneeProfile.tsx
+++ src/components/loanee-my-profile/OrganizationViewLoaneeProfile.tsx
@@ -1,8 +1,14 @@
 'use client'
-import React from 'react';
+import React, { useEffect } from 'react';
 import Index from "@/pages/loanee/MyProfile/index";
+import { store } from '@/redux/store';
+import { setOrganizationFrom } from '@/redux/slice/layout/adminLayout';
 
 const OrganizationViewLoaneeProfile = () => {
+
+    useEffect(()=>{
+        store.dispatch(setOrganizationFrom('cohort'))
+    } ,[])
 
     return (
 
