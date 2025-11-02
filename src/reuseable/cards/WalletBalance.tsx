90c9a247fcd98332d07a3c2666bdb09f656a232b
a-mariam <mariiam22222@gmail.com>
2025-11-02 14:24:51 +0100

    working on card ui

--- src/reuseable/cards/WalletBalance.tsx
+++ src/reuseable/cards/WalletBalance.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Image from "next/image";
 
 const WalletBalance = () => {
     return (
         <div
-            className={` w-full h-[12rem] rounded-md bg-gradient-to-r from-[#142854] to-[#093086] `}
+            className={` w-full h-[12rem] flex justify-between rounded-md bg-gradient-to-r from-[#142854] to-[#093086] `}
         >
-
+            <div></div>
+            <div className={` mb-0 h-[6rem] bg-red-300 `}>
+                <Image src={`/chart-02.svg`}
+                       alt={''} width={130} height={160}
+                       className={` mb-0 `}
+                />
+            </div>
         </div>
     );
 };
