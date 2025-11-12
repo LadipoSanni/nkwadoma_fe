6592b73e5add8115ff6b7206439913ad852be7eb
samibyrone <you@example.com>
2025-11-12 15:55:38 +0100

    loanee profile UI updated changes

--- src/features/portfolio-manager/settings/loanee-profile-settings/LoaneeProfileSetting.tsx
+++ src/features/portfolio-manager/settings/loanee-profile-settings/LoaneeProfileSetting.tsx
@@ -29,14 +29,14 @@ const LoaneeProfileSetting = ({ whoseProfile, companyUrl }: ProfileProps) => {
     whoseProfile === "company" ? companyUrl : profileData?.data?.avatar;
 
   return (
-    <div className='py-1 px-1 md:py-4 md:px-14 w-auto'>
+    <div className='py-1 px-1 md:py-4 md:px-10 w-auto'>
       <div className='w-full border border-[#ECECEC] rounded-lg py-1 md:py-5'>
-        <div className=' w-full flex justify-between md:px-6 px-1 py-4'>
+        <div className=' w-full flex justify-between md:px-6 px-2 py-4'>
           <div>
-            <h2 className={` text-md md:text-[18px] md:font-semibold py-1 ${inter.className} font-bold text-[#212221] md:px-1 px-1 `} >
+            <h2 className={` text-[14px] md:text-[18px] md:font-semibold py-1 ${inter.className} font-bold text-[#212221] md:px-1 px-1 `} >
               Profile
             </h2>
-            <p className={` text-xs md:text-sm font-normal ${inter.className} text-gray-500 md:px-1 px-1 `} >
+            <p className={` text-[12px] md:text-[14px] font-normal ${inter.className} text-[#4D4E4D] md:px-1 px-1 `} >
               Manage and update your personal details
             </p>
           </div>
@@ -48,38 +48,38 @@ const LoaneeProfileSetting = ({ whoseProfile, companyUrl }: ProfileProps) => {
           />
         </div>
 
-        <div className='space-y-6 md:space-y-6 w-full'>
+        <div className='space-y-4 md:space-y-4 w-full'>
           <ProfileSection title='Basic details'>
-            <div className='grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6'>
+            <div className='grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-2'>
               <div>
-                <p className='text-xs sm:text-sm font-normal text-[#4D4E4D] px-2 py-1'>
+                <p className={` text-[10px] md:text-[12px] font-normal ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   Age
                 </p>
-                <p className='text-sm sm:text-base font-medium text-[#4D4E4D] px-2 py-1'>
+                <p className={` text-[12px] md:text-[14px] font-medium ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   {calculateAge(profileData?.data?.dateOfBirth)}
                 </p>
               </div>
               <div>
-                <p className='text-xs sm:text-sm font-normal text-[#4D4E4D] px-2 py-1'>
+               <p className={`text-[10px] md:text-[12px] font-normal ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   Gender
                 </p>
-                <p className='text-sm sm:text-base font-medium text-[#4D4E4D] px-2 py-1'>
+                <p className={` text-[12px] md:text-[14px] font-medium ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   {toSentenceCase(profileData?.data?.gender)}
                 </p>
               </div>
               <div>
-                <p className='text-xs sm:text-sm font-normal text-[#4D4E4D] px-2 py-1'>
+              <p className={` text-[10px] md:text-[12px] font-normal ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   State of origin
                 </p>
-                <p className='text-sm sm:text-base font-medium text-[#4D4E4D] px-2 py-1'>
+                <p className={` text-[12px] md:text-[14px] font-medium ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   {toSentenceCase(profileData?.data?.stateOfOrigin)}
                 </p>
               </div>
               <div>
-                <p className='text-xs sm:text-sm font-normal text-[#4D4E4D] px-2 py-1'>
+              <p className={` text-[10px] md:text-[12px] font-normal ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   State of residence
                 </p>
-                <p className='text-sm sm:text-base font-medium text-[#4D4E4D] px-2 py-1'>
+                <p className={` text-[12px] md:text-[14px] font-medium ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   {toSentenceCase(profileData?.data?.stateOfResidence)}
                 </p>
               </div>
@@ -87,28 +87,28 @@ const LoaneeProfileSetting = ({ whoseProfile, companyUrl }: ProfileProps) => {
           </ProfileSection>
 
           <ProfileSection title='Contact'>
-            <div className='w-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6'>
+            <div className='w-auto grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-4 sm:gap-4'>
               <div>
-                <p className='text-xs sm:text-sm font-normal text-[#4D4E4D] px-2 py-1'>
+              <p className={` text-[10px] md:text-[12px] font-normal ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   Email
                 </p>
-                <p className='text-sm sm:text-base font-medium text-[#4D4E4D] px-2 py-1'>
+                <p className={` text-[12px] md:text-[14px] font-medium ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   {toSentenceCase(profileData?.data?.email)}
                 </p>
               </div>
               <div>
-                <p className='text-xs sm:text-sm font-normal text-[#4D4E4D] px-2 py-1'>
+              <p className={` text-[10px] md:text-[12px] font-normal ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   Phone
                 </p>
-                <p className='text-sm sm:text-base font-medium text-[#4D4E4D] px-2 py-1'>
+                <p className={` text-[12px] md:text-[14px] font-medium ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   {profileData?.data?.phoneNumber}
                 </p>
               </div>
               <div>
-                <p className='text-xs sm:text-sm font-normal text-[#4D4E4D] px-2 py-1'>
+              <p className={` text-[10px] md:text-[12px] font-normal ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   Address
                 </p>
-                <p className='text-sm sm:text-base font-medium text-[#4D4E4D] px-2 py-1'>
+                <p className={` text-[12px] md:text-[14px] font-medium ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   {toSentenceCase(profileData?.data?.residentialAddress)}
                 </p>
               </div>
@@ -116,12 +116,12 @@ const LoaneeProfileSetting = ({ whoseProfile, companyUrl }: ProfileProps) => {
           </ProfileSection>
 
           <ProfileSection title='Education'>
-            <div className='grid grid-cols-2 sm:grid-cols-2 gap-4 sm:gap-6'>
+            <div className='grid grid-cols-2 sm:grid-cols-2 gap-4 sm:gap-4'>
               <div>
-                <p className='text-xs sm:text-sm font-normal text-[#4D4E4D] px-2 py-1'>
+              <p className={` text-[10px] md:text-[12px] font-normal ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   Level of education
                 </p>
-                <p className='text-sm sm:text-base font-medium text-[#4D4E4D] px-2 py-1'>
+                <p className={` text-[12px] md:text-[14px] font-medium ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   {toSentenceCase(profileData?.data?.levelOfEduction)}
                 </p>
               </div>
@@ -129,49 +129,49 @@ const LoaneeProfileSetting = ({ whoseProfile, companyUrl }: ProfileProps) => {
           </ProfileSection>
 
           <ProfileSection title='Next of kin'>
-            <div className='w-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6'>
+            <div className='w-auto grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-4 sm:gap-4'>
               <div>
-                <p className='text-xs sm:text-sm font-normal text-[#4D4E4D] px-2 py-1'>
+              <p className={` text-[10px] md:text-[12px] font-normal ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   Full name
                 </p>
-                <p className='text-sm sm:text-base font-medium text-[#4D4E4D] px-2 py-1'>
+                <p className={` text-[12px] md:text-[14px] font-medium ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   {toSentenceCase(profileData?.data?.nextOfKinResponse?.firstName)}  {profileData?.data?.nextOfKinResponse?.lastName}
                 </p>
               </div>
               <div>
-                <p className='text-xs sm:text-sm font-normal text-[#4D4E4D] px-2 py-1'>
+              <p className={` text-[10px] md:text-[12px] font-normal  ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   Relationship
                 </p>
-                <p className='text-sm sm:text-base font-medium text-[#4D4E4D] px-2 py-1'>
+                <p className={` text-[12px] md:text-[14px] font-medium ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   {toSentenceCase(profileData?.data?.nextOfKinResponse?.nextOfKinRelationship)}
                 </p>
               </div>
               <div>
-                <p className='text-xs sm:text-sm font-normal text-[#4D4E4D] px-2 py-1'>
+              <p className={` text-[10px] md:text-[12px] font-normal ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   Email
                 </p>
-                <p className='text-sm sm:text-base font-medium text-[#4D4E4D] px-2 py-1'>
+                <p className={` text-[12px] md:text-[14px] font-medium ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   {toSentenceCase(profileData?.data?.nextOfKinResponse?.email)}
                 </p>
               </div>
-              <div>
-                <p className='text-xs sm:text-sm font-normal text-[#4D4E4D] px-2 py-1'>
+            </div>
+              <div className="py-4">
+              <p className={` text-[10px] md:text-[12px] font-normal ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   Address
                 </p>
-                <p className='text-sm sm:text-base font-medium text-[#4D4E4D] px-2 py-1'>
+                <p className={` text-[12px] md:text-[14px] font-medium ${inter.className} text-[#4D4E4D] px-2 py-1 `}>
                   {toSentenceCase(profileData?.data?.nextOfKinResponse?.contactAddress)}
                 </p>
               </div>
-            </div>
           </ProfileSection>
         </div>
 
-        <div className=' w-full flex md:justify-end justify-center mt-8 px-6'>
+        <div className=' w-full flex md:justify-end justify-center md:mt-8 mb-2 px-6'>
           <button
             id={"update-profile"}
             data-testid={"update-profile"}
             onClick={() => setIsModalOpen(true)}
-            className='bg-[#142854] text-white md:px-5 md:py-2 px-20 py-2 text-sm rounded-[6px] font-bold'
+            className={`bg-[#142854] text-white md:px-5 md:py-2 px-20 py-2 ${inter.className} text-[14px] rounded-[6px] font-bold `}
           >
             Update profile
           </button>
--- src/reuseable/profile/LoaneeProfileSection.tsx
+++ src/reuseable/profile/LoaneeProfileSection.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import type React from "react"
-import type { ReactNode } from "react"
+import { inter } from "@/app/fonts";
+import type { ReactNode } from "react";
 
 interface ProfileSectionProps {
   title: string
@@ -11,10 +12,10 @@ interface ProfileSectionProps {
 const ProfileSection = ({ title, children }: ProfileSectionProps) => {
   return (
     <div className="rounded-lg overflow-hidden">
-      <div className="px-4 sm:px-6 py-3 sm:py-4">
-        <h2 className="text-sm sm:text-base py-3 px-2 rounded-sm font-medium text-[#4D4E4D] bg-[#F2F2F2]">{title}</h2>
+      <div className="px-4 sm:px-6 py-3 md:py-2">
+        <h2 className={` text-[12px] md:text-[14px] py-3 px-2 rounded-sm font-medium ${inter.className} text-[#4D4E4D] bg-[#F2F2F2] `}>{title}</h2>
       </div>
-      <div className="px-4 sm:px-6 py-4 sm:py-6">{children}</div>
+      <div className={` px-4 md:px-6 py-4 md:py-2 text-[#4D4E4D] ${inter.className} font-normal `}>{children}</div>
     </div>
   )
 }
